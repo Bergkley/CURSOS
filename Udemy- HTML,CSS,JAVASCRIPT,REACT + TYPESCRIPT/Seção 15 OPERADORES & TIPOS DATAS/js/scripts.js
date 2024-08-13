0f69ab97ebf7ea44ef44653c808c85ea5e1ef2ff
Bergkley <bergkley@gmail.com>
2024-08-13 19:25:35 -0300

    feat: // 2- Operação aritmética

--- Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 15 OPERADORES & TIPOS DATAS/js/scripts.js
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 15 OPERADORES & TIPOS DATAS/js/scripts.js
@@ -2,3 +2,9 @@
 console.log(typeof 2)
 console.log(typeof 5.14)
 console.log(typeof -127)
+// 2- Operação aritmética
+console.log(2 + 2)
+console.log(10 - 2)
+console.log(5 * 2)
+console.log(10 / 2)
+console.log(10+(5+4))
