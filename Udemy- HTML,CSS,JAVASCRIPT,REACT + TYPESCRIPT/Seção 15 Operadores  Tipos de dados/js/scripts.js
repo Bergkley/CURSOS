78bb454a109a59dc5412c2c1d850e8caa7c8cff1
Bergkley <bergkley@gmail.com>
2024-08-13 19:41:41 -0300

    feat: Operadores && tipos de dados

---
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 15 Operadores  Tipos de dados/js/scripts.js
@@ -0,0 +1,39 @@
+// 1 - Number
+console.log(typeof 2);
+console.log(typeof 2.2);
+console.log(typeof -4);
+
+// 2 - Ops. aritméticas
+
+console.log(2 + 4);
+console.log(10 - 5);
+console.log(5 * 4);
+console.log(10 / 2);
+
+// 3 - Special Numbers
+console.log(typeof Infinity);
+
+console.log(12 * "asd");
+
+console.log(typeof NaN);
+
+// 4 - Strings
+console.log("Um texto");
+console.log(`Mais um texto`);
+
+console.log(typeof "Opa");
+
+// 5 - Símbolos especiais em string
+console.log("Testando \n quebra de linha");
+
+console.log("Espaçamento \t de tab");
+
+// 6 - Concatenação
+console.log("Oi, " + " tudo " + "bem?");
+
+console.log(`Testando ` + `Concatenação`);
+
+// 7 - Interpolação
+console.log(`A soma de 2 + 2 é: ${2 + 2}`);
+
+console.log(`Podemos executar qualquer coisa ${console.log("teste")}`);
