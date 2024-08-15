6147ada118209f2d3ece71d789b73b1a684aee7c
Bergkley <bergkley@gmail.com>
2024-08-14 21:13:53 -0300

    feat: Seção 16 13- identação

--- Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 16 Estruturas/js/scripts.js
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 16 Estruturas/js/scripts.js
@@ -57,3 +57,13 @@ do {
 for(let t = 0; t < 5; t++){
     console.log(`Repetindo algo : ${t}`)
 }
+// 13- identação
+for(let u = 0; u < 5; u++){
+    if(u * 2 >10){
+        console.log(`Maior que 10! ${u}`)
+    }else{
+        if(u / 2 === 0){
+            console.log(`Par: ${u}`)
+        }
+    }
+}
