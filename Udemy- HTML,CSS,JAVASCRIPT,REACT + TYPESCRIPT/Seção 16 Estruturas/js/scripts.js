f566990089625e2179f591180a8a2fcb5cab519d
Bergkley <bergkley@gmail.com>
2024-08-14 21:20:38 -0300

    feat: Seção 16 Estruturas

--- Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 16 Estruturas/js/scripts.js
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 16 Estruturas/js/scripts.js
@@ -2,15 +2,17 @@
 let nome = "Bergkley";
 const idade = 31;
 // 2- mais sobre variaveis
-let a = 10,b=20,c=30;
-let _teste= "ok";
+let a = 10,
+  b = 20,
+  c = 30;
+let _teste = "ok";
 let $teste2 = "ok2";
 // 3- Prompt
 let age = prompt("Qual seu idade?");
 // 4- alert
 alert("Seu nome e: " + nome);
 // 5- Math
-console.log(Math.max(10,20,30));
+console.log(Math.max(10, 20, 30));
 console.log(Math.floor(5.14));
 console.log(Math.ceil(5.5));
 // 6- console
@@ -19,83 +21,83 @@ console.error("erro");
 console.warn("aviso");
 // 7- if
 const m = 10;
-if(m > 5){
-    console.log(" M Maior que 5");
+if (m > 5) {
+  console.log(" M Maior que 5");
 }
 const user = "João";
-if(user === "João"){
-    console.log("Seu nome é João");
+if (user === "João") {
+  console.log("Seu nome é João");
 }
 // 8- else
 const loggedIn = false;
-if(loggedIn){
-    console.log('Está autenticado');
-}else{
-    console.log('Não está autenticado');
+if (loggedIn) {
+  console.log("Está autenticado");
+} else {
+  console.log("Não está autenticado");
 }
 // 9- else if
-if(1 > 2){
-    console.log("1 maior que 2");
-}else if(2>3){
-    console.log("2 maior que 3");
-}else{
-    console.log("3 maior que 1");
+if (1 > 2) {
+  console.log("1 maior que 2");
+} else if (2 > 3) {
+  console.log("2 maior que 3");
+} else {
+  console.log("3 maior que 1");
 }
 // 10- While
 let i = 0;
-while(i < 5){
-console.log(`Repetindo: ${i}` )
-i++;
+while (i < 5) {
+  console.log(`Repetindo: ${i}`);
+  i++;
 }
 // 11- do while
-let o = 10 ;
+let o = 10;
 do {
-    console.log(`Valor do o: ${o}`);
-    o--;
-}while(0 >1)
+  console.log(`Valor do o: ${o}`);
+  o--;
+} while (0 > 1);
 // 12- for
-for(let t = 0; t < 5; t++){
-    console.log(`Repetindo algo : ${t}`)
+for (let t = 0; t < 5; t++) {
+  console.log(`Repetindo algo : ${t}`);
 }
 // 13- identação
-for(let u = 0; u < 5; u++){
-    if(u * 2 >10){
-        console.log(`Maior que 10! ${u}`)
-    }else{
-        if(u / 2 === 0){
-            console.log(`Par: ${u}`)
-        }
+for (let u = 0; u < 5; u++) {
+  if (u * 2 > 10) {
+    console.log(`Maior que 10! ${u}`);
+  } else {
+    if (u / 2 === 0) {
+      console.log(`Par: ${u}`);
     }
+  }
 }
 // 14 - break
-for(let g = 0; g < 5; g++){
-    console.log(`valor de g é: ${g}`)
-    if(g === 2){
-        break;
-    }
+for (let g = 0; g < 5; g++) {
+  console.log(`valor de g é: ${g}`);
+  if (g === 2) {
+    break;
+  }
 }
 // 15 - continue
-for(let s = 0; s < 5; s++){
-    if(s % 2 === 0){
-        console.log("Numero par")
-        continue;
-    }
-    console.log(`valor de s é: ${s}`)
+for (let s = 0; s < 5; s++) {
+  if (s % 2 === 0) {
+    console.log("Numero par");
+    continue;
+  }
+  console.log(`valor de s é: ${s}`);
 }
 
 // 16 - switch
 const job = "Advogado";
 
-switch(job){
-    case "Advogado":
-        console.log("Seu cargo é Advogado");
-        break;
-    case "Programador":
-        console.log("Seu cargo é Programador");
-        break;
-    case "Designer":
-        console.log("Seu cargo é Designer");
-        break;
-    default:
-        console.log("Seu cargo não foi encontrado");
+switch (job) {
+  case "Advogado":
+    console.log("Seu cargo é Advogado");
+    break;
+  case "Programador":
+    console.log("Seu cargo é Programador");
+    break;
+  case "Designer":
+    console.log("Seu cargo é Designer");
+    break;
+  default:
+    console.log("Seu cargo não foi encontrado");
 }
