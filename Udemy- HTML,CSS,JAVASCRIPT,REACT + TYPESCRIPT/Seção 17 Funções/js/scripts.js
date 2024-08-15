111e5bb0976021c5740386c10de30c0d25cb5c33
Bergkley <bergkley@gmail.com>
2024-08-15 20:18:07 -0300

    feat: Seção 17 // 1- Criando uma função

---
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 17 Funções/js/scripts.js
@@ -0,0 +1,15 @@
+// 1- Criando uma função
+function minhaFunção() {
+    console.log('Ola, mundo!')
+}
+minhaFunção();
+const minhaFunçãoEmVariavel = function (){
+    console.log('Ola, mundo!')
+}
+minhaFunçãoEmVariavel();
+
+function funcaoComParametro(txt){
+    console.log(txt)
+}
+
+funcaoComParametro('Ola, mundo 3!')
