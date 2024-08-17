f726a94cdff6ac1915e639425ba546500a763d85
Bergkley <bergkley@gmail.com>
2024-08-16 22:07:18 -0300

    feat:Seção 15 Operadores  Tipos de dados

--- Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 15 Operadores  Tipos de dados/js/scripts.js
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 15 Operadores  Tipos de dados/js/scripts.js
@@ -41,7 +41,7 @@ console.log(`Podemos executar qualquer coisa ${console.log("teste")}`);
 //8- Boolean
 console.log(typeof true);
 console.log(typeof false);
-console.log(20>5)
+console.log(20 > 5);
 //9- Comparações
 console.log(2 == 2);
 console.log(2 == "2");
@@ -68,16 +68,16 @@ console.log(false || true);
 console.log(false || false);
 console.log(!true);
 console.log(!false);
-console.log(5> 2 && 10 > 20);
-console.log(5> 2 || 10 > 20);
+console.log(5 > 2 && 10 > 20);
+console.log(5 > 2 || 10 > 20);
 // 12- Empaty values
-console.log(typeof null, typeof undefined)
-console.log(null === undefined)
-console.log(null == undefined)
-console.log(null == false)
-console.log(undefined == false)
+console.log(typeof null, typeof undefined);
+console.log(null === undefined);
+console.log(null == undefined);
+console.log(null == false);
+console.log(undefined == false);
 // 13- Conversão de tipo automatica
-console.log(5* null);
+console.log(5 * null);
 console.log("teste" * "opa");
 console.log("10" + 1);
 console.log("10" - 1);
