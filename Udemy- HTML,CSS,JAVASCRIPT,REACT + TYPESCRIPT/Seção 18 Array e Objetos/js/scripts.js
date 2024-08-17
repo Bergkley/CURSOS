d4b4ad46c53c45c05fcb0c8f9d4d785b8d1ce745
Bergkley <bergkley@gmail.com>
2024-08-17 20:10:59 -0300

    feat:Seção 18 Array e Objetos 4 - métodos

--- Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 18 Array e Objetos/js/scripts.js
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 18 Array e Objetos/js/scripts.js
@@ -10,3 +10,14 @@ console.log(arr[0]);
 const numbers = [1, 2, 3, 4, 5];
 
 console.log(numbers.length)
+
+// 4 - métodos
+const otherNumbers = [1, 2, 3, 4, 5];
+const allNumbers = numbers.concat(otherNumbers);
+
+console.log(allNumbers);
+const text = "algum texto"
+
+console.log(text.toUpperCase())
+
+console.log(text.indexOf('g'))
