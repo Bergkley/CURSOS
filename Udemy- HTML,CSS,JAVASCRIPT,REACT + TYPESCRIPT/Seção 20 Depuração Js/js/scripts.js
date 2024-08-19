b7c227752557656475adfe049677d07a8d4ed591
Bergkley <bergkley@gmail.com>
2024-08-19 20:04:12 -0300

    feat: Seção 20 Depuração Js 2 - console.log

--- Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 20 Depuração Js/js/scripts.js
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 20 Depuração Js/js/scripts.js
@@ -2,3 +2,16 @@
 "use strict";
 
 // const undefined = 10;
+
+// 2 - console.log
+let a = 1
+let b = 2;
+
+if( a == 1){
+    a = b + 2;
+}
+
+for(let i = 0; i < b; i++){
+    a = a + 2;
+    console.log(a);
+}
