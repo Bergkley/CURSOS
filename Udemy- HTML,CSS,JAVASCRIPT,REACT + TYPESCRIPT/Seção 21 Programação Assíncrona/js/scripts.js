a52c32729672c562cecb5c423582b8c62491976b
Bergkley <bergkley@gmail.com>
2024-08-20 19:18:17 -0300

    feat: Seção 21 Programação Assíncrona  1 - setTimeout

--- Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 21 Programação Assíncrona/js/scripts.js
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 21 Programação Assíncrona/js/scripts.js
@@ -1,6 +1,7 @@
 // 1 - setTimeout
 console.log("Ainda não executou");
 setTimeout(() => {
-    console.log("Executou depois de 2 segundos");
+    console.log("Executou depois de 2 segundos SETTIMEOUT");
 }, 2000);
 console.log("Depois de executar");
+
