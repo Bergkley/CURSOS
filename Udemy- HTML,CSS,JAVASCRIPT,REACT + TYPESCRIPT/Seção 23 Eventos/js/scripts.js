7f7e5f733542a5d79141acd3ada75ea02c065520
Bergkley <bergkley@gmail.com>
2024-08-22 08:55:49 -0300

    feat:Seção 23 Eventos  2 - removendo eventos

--- Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 23 Eventos/js/scripts.js
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 23 Eventos/js/scripts.js
@@ -4,3 +4,19 @@ const  btn = document.querySelector('#my-button');
 btn.addEventListener('click', function(){
     console.log('Clicou');
 })
+
+ // 2 - removendo eventos
+ const secondBtn = document.querySelector('#btn');
+
+ function imprimirMensagem(){
+     console.log('Teste');
+ }
+
+ secondBtn.addEventListener('click', imprimirMensagem);
+
+ const thirdBtn = document.querySelector('#other-btn');
+
+ thirdBtn.addEventListener('click',()=>{
+    console.log('Evento removido')
+     secondBtn.removeEventListener('click',imprimirMensagem)
+ })
