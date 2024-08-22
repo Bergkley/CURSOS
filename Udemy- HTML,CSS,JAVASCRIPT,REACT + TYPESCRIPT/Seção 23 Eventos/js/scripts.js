6a783b6dbef81611c2ac88b80f21a57cd14eb1b1
Bergkley <bergkley@gmail.com>
2024-08-22 09:04:12 -0300

    feat:Seção 23 Eventos  // 4 - propagação

--- Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 23 Eventos/js/scripts.js
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 23 Eventos/js/scripts.js
@@ -32,3 +32,16 @@ myTitle.addEventListener("click",(event)=>{
     console.log(event.target);
 })
 
+// 4 - propagação
+
+const containerBtn = document.querySelector('#btn-container');
+const btnInsideContainer = document.querySelector('#div-btn');
+
+containerBtn.addEventListener('click', ()=>{
+    console.log("Evento 1")
+})
+
+btnInsideContainer.addEventListener('click', (e)=>{
+    e.stopPropagation();
+    console.log("Evento 1")
+})
