a3d02c8e46d9cce82ac5d25f995c6ccfcd379af0
Bergkley <bergkley@gmail.com>
2024-08-22 10:12:27 -0300

    feat:Seção 23 Eventos  10 - eventos de focus

--- Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 23 Eventos/js/scripts.js
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 23 Eventos/js/scripts.js
@@ -90,3 +90,14 @@ window.addEventListener('scroll', ()=>{
         console.log('Passou do 200')
     }
 })
+
+// 10 - eventos de focus
+const input = document.querySelector("#my-input");
+
+input.addEventListener('focus', (e)=>{
+    console.log('Entrou no input');
+});
+
+input.addEventListener('blur', (e)=>{
+    console.log('Saiu do input');
+});
