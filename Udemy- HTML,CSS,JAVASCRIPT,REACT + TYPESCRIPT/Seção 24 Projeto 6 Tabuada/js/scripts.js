bcd804a5238b4e20412128a97b3921bac0ebe96c
Bergkley <bergkley@gmail.com>
2024-08-23 20:05:59 -0300

    feat: Seção 24 Projeto 6 Tabuada JS // Eventos

--- Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 24 Projeto 6 Tabuada/js/scripts.js
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 24 Projeto 6 Tabuada/js/scripts.js
@@ -1,5 +1,14 @@
 // Seleção de elementos
 const multiplicationForm = document.querySelector("#multiplication-form");
-const NumberForm = document.querySelector("#number");
+const numberInput = document.querySelector("#number");
 const multiplicationInput = document.querySelector("#multiplicator");
 
+// Eventos
+multiplicationForm.addEventListener("submit",(e)=> {
+    e.preventDefault();
+    const multiplicationNumber = +numberInput.value;
+    const multiplicatorNumber = +multiplicationInput.value;
+
+    if(!multiplicationNumber || !multiplicatorNumber)return;
+    
+});
