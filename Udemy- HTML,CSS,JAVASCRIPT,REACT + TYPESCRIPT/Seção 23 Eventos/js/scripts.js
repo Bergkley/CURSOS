8236428cad56e758fbc6fe18f0d727b5db7289c2
Bergkley <bergkley@gmail.com>
2024-08-23 13:18:43 -0300

    feat:Seção 23 Eventos 12 - debounce

--- Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 23 Eventos/js/scripts.js
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 23 Eventos/js/scripts.js
@@ -111,3 +111,19 @@ window.addEventListener('beforeunload',(e)=>{
     e.preventDefault();
     e.returnValue = 'teste';
 })
+
+// 12 - debounce
+const debounce = (f, delay)=> {
+    let timeout;
+    return (...arguments)=>{
+        if(timeout){
+            clearTimeout(timeout);
+        }
+        timeout = setTimeout(()=>{
+            f.apply(arguments)
+    },delay)
+    }
+}
+window.addEventListener("mousemove", debounce(()=>{
+    console.log('Executando a cada 400ms')
+}, 400))
