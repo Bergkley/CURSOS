d05843aabf8b0547824305193aa18201314c4885
Bergkley <bergkley@gmail.com>
2024-08-23 20:00:53 -0300

    feat: Seção 24 Projeto 6 Tabuada JS // Seleção de elementos

---
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 24 Projeto 6 Tabuada/js/scripts.js
@@ -0,0 +1,5 @@
+// Seleção de elementos
+const multiplicationForm = document.querySelector("#multiplication-form");
+const NumberForm = document.querySelector("#number");
+const multiplicationInput = document.querySelector("#multiplicator");
+
