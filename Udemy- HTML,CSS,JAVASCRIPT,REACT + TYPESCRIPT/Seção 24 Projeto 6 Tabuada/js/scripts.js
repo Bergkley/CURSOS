360cbb5139d5bdf94d897cb53098af3f5fcb2835
Bergkley <bergkley@gmail.com>
2024-08-23 20:23:15 -0300

    feat: Seção 24 Projeto 6 Tabuada JS

--- Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 24 Projeto 6 Tabuada/js/scripts.js
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 24 Projeto 6 Tabuada/js/scripts.js
@@ -3,12 +3,44 @@ const multiplicationForm = document.querySelector("#multiplication-form");
 const numberInput = document.querySelector("#number");
 const multiplicationInput = document.querySelector("#multiplicator");
 
-// Eventos
-multiplicationForm.addEventListener("submit",(e)=> {
-    e.preventDefault();
-    const multiplicationNumber = +numberInput.value;
-    const multiplicatorNumber = +multiplicationInput.value;
+const multiplicationTitle = document.querySelector(
+  "#multiplication-title span"
+);
+const multiplicationTable = document.querySelector(
+  "#multiplication-operations"
+);
 
-    if(!multiplicationNumber || !multiplicatorNumber)return;
-    
+// Funções
+const createTable = (number, multiplicatorNumber) => {
+  multiplicationTable.innerHTML = "";
+
+  for (i = 1; i <= multiplicatorNumber; i++) {
+    const result = number * i;
+
+    const template = `<div class="row">
+            <div class="operation">${number} x ${i} = </div>
+            <div class="result">${result}</div>
+        </div>`;
+
+    const parser = new DOMParser();
+    const htmlTemplate = parser.parseFromString(template, "text/html");
+    const row = htmlTemplate.querySelector(".row");
+
+    multiplicationTable.appendChild(row);
+  }
+
+  multiplicationTitle.innerText = number;
+};
+
+// Eventos
+multiplicationForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const multiplicationNumber = numberInput.value;
+
+  const multiplicatorNumber = +multiplicationInput.value;
+
+  if (!multiplicationNumber || !multiplicatorNumber) return;
+
+  createTable(multiplicationNumber, multiplicatorNumber);
 });
