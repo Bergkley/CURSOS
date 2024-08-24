8bb2aa2dfbc11d8a12a44c95b9e2ec7c9c32fba1
Bergkley <bergkley@gmail.com>
2024-08-24 20:26:22 -0300

    feat: Seção 25 Projeto 7 Gerador de QR Code Js Função

--- Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 25 Projeto 7 Gerador de QR Code/js/scripts.js
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 25 Projeto 7 Gerador de QR Code/js/scripts.js
@@ -1,7 +1,24 @@
+// Seleção	de elementos
 const container = document.querySelector(".container");
 const qrCodeBtn = document.querySelector("#qr-form button");
 
 const qrCodeInput = document.querySelector("#qr-form input");
 
 const qrCodeImg = document.querySelector("#qr-code img");
+
+// Função
+function generateQrCode() {
+    const qrCodeInputValue = qrCodeInput.value;
+
+    if(!qrCodeInputValue) return;
+
+    qrCodeBtn.innerHTML = "Gerando código";
+
+    qrCodeImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrCodeInputValue}`;
+
+    qrCodeImg.addEventListener("load",()=>{
+        container.classList.add("active")
+        qrCodeBtn.innerHTML = "Código criado";
+    })
+}
 
