93fefa3d138cfcc11628b8d2bf068dd7434e544b
Bergkley <bergkley@gmail.com>
2024-08-27 19:13:04 -0300

    feat:Seção 28 ES6+ 1 - var,let e const

---
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 28 ES6+/js/scrits.js
@@ -0,0 +1,17 @@
+// 1 - var,let e const
+var x = 10;
+var y = 15;
+
+if(y > 10){
+    var x = 5;
+    console.log(x);
+}
+
+let a = 10;
+let b = 15;
+
+if(b > 10){
+    let a = 5;
+    console.log(a);
+}
+console.log(a)
