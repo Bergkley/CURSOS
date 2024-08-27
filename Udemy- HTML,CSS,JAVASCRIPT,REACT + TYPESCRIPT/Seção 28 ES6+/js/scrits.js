d698e2e7587daeb920c7ddc4f7c7e85bc30a18e8
Bergkley <bergkley@gmail.com>
2024-08-27 19:22:40 -0300

    feat:Seção 28 ES6+  2 - Arrow function

--- Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 28 ES6+/js/scrits.js
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 28 ES6+/js/scrits.js
@@ -15,3 +15,42 @@ if(b > 10){
     console.log(a);
 }
 console.log(a)
+
+// 2 - Arrow function
+
+const sum = function(a,b) {
+    return a + b
+}
+
+const arrowSum = (a,b) => a + b
+
+console.log(sum(1,2))
+console.log(arrowSum(1,2))
+
+const greeting = (name)=> {
+    if(name){
+        return `Hello ${name}` 
+    }else{
+        return "Hello"
+    }
+}
+console.log(greeting("João"))
+
+const user = {
+    name: "João",
+    sayUserName: () => {
+        setTimeout(function (){
+            console.log(this)
+            console.log(`Username: ${this.name}`)
+        },1000)
+    },
+    sayUserNameArroy () {
+        setTimeout(() => {
+            console.log(this)
+            console.log(`Username: ${this.name}`)
+        },1000)
+    }  
+}
+
+user.sayUserName();
+user.sayUserNameArroy();
