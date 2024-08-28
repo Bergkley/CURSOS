c616b078f15cfe9f670f017ac31655bfbbce6eab
Bergkley <bergkley@gmail.com>
2024-08-28 20:20:21 -0300

    feat: Seção 29 Axios 2- primeiro request

--- Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 29 Axios/js/scripts.js
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 29 Axios/js/scripts.js
@@ -1,2 +1,27 @@
-// 1 - instalação
-console.log(axios)
+// 1 - instalando o axios
+console.log(axios);
+
+// 2 - primeiro request
+const getData = async () => {
+  try {
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/users",
+      //   4 - definindo headers
+      //   Ver request headers
+      {
+        headers: {
+          "content-type": "application/json",
+          custom: "header",
+        },
+      }
+    );
+
+    console.log(response);
+
+    return response.data;
+  } catch (error) {
+    console.log(error.response);
+  }
+};
+
+getData();
