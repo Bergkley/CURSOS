1c279cd1c752220b8503f28044820c3ac4d917bf
Bergkley <bergkley@gmail.com>
2024-08-28 20:24:25 -0300

    feat: Seção 29 Axios 3 - imprimindo dados na tela

--- Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 29 Axios/js/scripts.js
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 29 Axios/js/scripts.js
@@ -25,3 +25,26 @@ const getData = async () => {
 };
 
 getData();
+
+// 3 - imprimindo dados na tela
+const container = document.querySelector("#user-container");
+
+const printData = async () => {
+    const data = await getData();
+
+    data.forEach((user) => {
+        const div = document.createElement("div");
+        const nameElement = document.createElement("h2");
+        nameElement.textContent = user.name;
+
+        div.appendChild(nameElement);
+
+        const emailElement = document.createElement("p");
+        emailElement.textContent = user.email;
+        
+        div.appendChild(emailElement);
+        
+        container.appendChild(div);
+    })
+}
+printData();
