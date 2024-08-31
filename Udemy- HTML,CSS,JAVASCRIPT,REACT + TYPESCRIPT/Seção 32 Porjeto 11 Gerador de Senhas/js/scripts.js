be7ef89d5fe03326abac203dd7ed09a45d913f17
Bergkley <bergkley@gmail.com>
2024-08-31 10:49:05 -0300

    feat:Seção 32 Porjeto 11 Gerador de Senhas  Css

---
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 32 Porjeto 11 Gerador de Senhas/js/scripts.js
@@ -0,0 +1,59 @@
+// Seleção de elementos
+const generatePasswordButton = document.querySelector("#generate-password");
+const generatedPasswordElement = document.querySelector("#generated-password");
+
+
+// Funções
+
+const getLetterLowerCase = () => {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+  };
+  
+  const getLetterUpperCase = () => {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+  };
+  
+  const getNumber = () => {
+    return Math.floor(Math.random() * 11).toString();
+  };
+  
+  const getSymbol = () => {
+    const symbols = "(){}[]=<>/,.!@#$%^&*";
+    return symbols[Math.floor(Math.random() * symbols.length)];
+  };
+  
+
+  const generatePassword = (getLetterLowerCase, getLetterUpperCase, getNumber, getSymbol) => {
+    let password = "";
+
+    const passwordLength = 10;
+
+    const generators = [
+      getLetterLowerCase,
+      getLetterUpperCase,
+      getNumber,
+      getSymbol
+    ];
+
+    for(let i = 0; i < passwordLength; i += 4) {
+        generators.forEach(() => {
+            const randomValue = generators[Math.floor(Math.random() * generators.length)];
+
+            password += randomValue;
+        })
+    }
+    password= password.slice(0, passwordLength);
+
+    generatedPasswordElement.style.display = "block";
+
+    generatedPasswordElement.querySelector("h4").innerText = password;
+  }
+
+
+// Eventos
+
+generatePasswordButton.addEventListener("click", () => {
+
+    generatePassword(getLetterLowerCase, getLetterUpperCase, getNumber, getSymbol);
+    
+})
