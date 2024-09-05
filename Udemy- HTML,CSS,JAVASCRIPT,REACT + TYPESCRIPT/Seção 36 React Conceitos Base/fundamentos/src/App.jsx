98af5d52aaf20432249c0aa2c1c3006d2fabdf7e
Bergkley <bergkley@gmail.com>
2024-09-05 20:40:26 -0300

    feat:comentários

--- Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 36 React Conceitos Base/fundamentos/src/App.jsx
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 36 React Conceitos Base/fundamentos/src/App.jsx
@@ -1,8 +1,9 @@
 import './App.css'
 import FirstComponent from './components/FirstComponent'
 
+// 2 - importando componente
 function App() {
-
+  // 3 - Comentários
   return (
     <div className='App'>
       <h1>Fundamentos do React</h1>
--- Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 36 React Conceitos Base/fundamentos/src/components/FirstComponent.jsx
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 36 React Conceitos Base/fundamentos/src/components/FirstComponent.jsx
@@ -1,3 +1,5 @@
+
+// 1- Criando componente
 const FirstComponent = () => {
   return (
     <div>
