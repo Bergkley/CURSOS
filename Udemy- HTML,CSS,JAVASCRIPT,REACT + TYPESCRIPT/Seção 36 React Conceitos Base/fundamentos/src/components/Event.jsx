8269144e9d39481d1cb6a289f6591514999a878c
Bergkley <bergkley@gmail.com>
2024-09-05 20:47:19 -0300

    feat: Event

---
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 36 React Conceitos Base/fundamentos/src/components/Event.jsx
@@ -0,0 +1,12 @@
+
+const Event = () => {
+  return (
+    <div>
+      <div>
+        <button onClick={() => console.log("Clicou")}></button>
+      </div>
+    </div>
+  )
+}
+
+export default Event
