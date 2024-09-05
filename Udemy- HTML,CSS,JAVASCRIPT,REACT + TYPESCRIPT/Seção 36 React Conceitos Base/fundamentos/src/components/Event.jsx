14da1615ad3c56ed4ead1e59b109627024ac00d3
Bergkley <bergkley@gmail.com>
2024-09-05 20:49:30 -0300

    feat: Event de click com função

--- Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 36 React Conceitos Base/fundamentos/src/components/Event.jsx
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 36 React Conceitos Base/fundamentos/src/components/Event.jsx
@@ -1,9 +1,15 @@
 
 const Event = () => {
+    const handleClick = () => {
+        console.log('Executou')
+    };
   return (
     <div>
       <div>
-        <button onClick={() => console.log("Clicou")}></button>
+        <button onClick={() => console.log("Clicou")}>Clique aqui</button>
+      </div>
+      <div>
+        <button onClick={handleClick}>Clique aqui função</button>
       </div>
     </div>
   )
