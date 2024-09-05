82df54284c95279df44042834044149a8e3ad04f
Bergkley <bergkley@gmail.com>
2024-09-05 20:37:35 -0300

    feat:importando primeiro componente

--- Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 36 React Conceitos Base/fundamentos/src/App.jsx
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 36 React Conceitos Base/fundamentos/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import FirstComponent from './components/FirstComponent'
 
 function App() {
 
   return (
     <div className='App'>
-
+      <h1>Fundamentos do React</h1>
+      <FirstComponent />
     </div>
   )
 }
