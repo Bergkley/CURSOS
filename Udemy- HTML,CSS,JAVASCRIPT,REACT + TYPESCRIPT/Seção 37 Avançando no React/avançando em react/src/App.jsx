69b5c80cb6523c450542c014f774a97b3a977441
Bergkley <bergkley@gmail.com>
2024-09-06 14:01:02 -0300

    feat:Seção 37 Avançando no React reaproveitamento de componente com loop

--- Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 37 Avançando no React/avançando em react/src/App.jsx
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 37 Avançando no React/avançando em react/src/App.jsx
@@ -7,6 +7,12 @@ import Data from './components/Data'
 import ListRender from './components/ListRender'
 import ShowUserName from './components/ShowUserName'
 
+// 11 - renderização de listas com componente
+const cars = [
+  { id: 1, brand: "Ferrari", color: "Amarelo", km: 0 },
+  { id: 2, brand: "KIA", color: "Branco", km: 200000 },
+  { id: 3, brand: "Renault", color: "Azul", km: 32000 },
+];
 function App() {
   return (
     <>
@@ -29,7 +35,10 @@ function App() {
         {/* 10 - reaproveitamento de componentes */}
         <CarDetails brand="Fiat" km={20000} color="Vermelho"/>
         <CarDetails brand="Audi" km={90000} color="Azul"/>
-
+        {/* 11 - renderização de listas com componente */}
+        {cars.map(car => (
+          <CarDetails key={car.id} brand={car.brand} km={car.km} color={car.color} />
+        ))}
 
       </div>
     </>
