08d2bbcd5b8841b00039a117655ba00369abb025
Bergkley <bergkley@gmail.com>
2024-09-06 13:35:25 -0300

    feat:Seção 37 Avançando no React List render com key

--- Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 37 Avançando no React/avançando em react/src/components/ListRender.jsx
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 37 Avançando no React/avançando em react/src/components/ListRender.jsx
@@ -2,12 +2,25 @@ import { useState } from "react"
 const ListRender = () => {
 const [list] = useState(["Matheus", "Pedro", "Josias"]);
 
+
+const [users,setUsers] = useState([
+    {id: 1, name: "Matheus",age: 31},
+    {id: 2, name: "Pedro",age: 20},
+    {id: 3, name: "João",age: 19},
+]);
+
   return (
     <div>
         <h1>lista</h1>
       <ul>
-        {list.map((item)=>
-            <li>{item}</li>
+        {list.map((item, i)=>
+            <li key={i}>{item}</li>
+        )}
+      </ul>
+      {/* 4 - render de lista com key */}
+      <ul>
+        {users.map((user)=>
+            <li key={user.id}>{user.name} - {user.age} anos</li>
         )}
       </ul>
     </div>
