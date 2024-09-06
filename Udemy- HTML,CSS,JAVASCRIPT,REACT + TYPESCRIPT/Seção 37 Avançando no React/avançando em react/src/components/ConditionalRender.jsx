7c93b96bb820443d7a1956c6f4ad17a4b8dea4d8
Bergkley <bergkley@gmail.com>
2024-09-06 13:47:40 -0300

    feat:Seção 37 Avançando no React ConditionalRender ternário

--- Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 37 Avançando no React/avançando em react/src/components/ConditionalRender.jsx
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 37 Avançando no React/avançando em react/src/components/ConditionalRender.jsx
@@ -1,10 +1,23 @@
 const ConditionalRender = () => {
     const x = 5;
+    const name = "Berg";
+
   return (
     <div>
       {/* 7 - render condicional */}
       <h3>Isso será exibido?</h3>
       {x>2 && <p>é maior que 2 </p>}
+      {/* 8 - ternário */}
+      <h3>Ternário</h3>
+      {name === "Berg" ? (
+        <div>
+            <p>O nome é Berg</p>
+        </div>
+      ):(
+        <div>
+            <p>O nome não é Berg</p>
+        </div>
+      )}
     </div>
   )
 }
