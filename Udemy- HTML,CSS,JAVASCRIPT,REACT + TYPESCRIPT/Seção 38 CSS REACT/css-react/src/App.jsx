200fa82dc586d12b55dc4417d138e338c70d6157
Bergkley <bergkley@gmail.com>
2024-09-07 13:44:56 -0300

    feat:Seção 38 CSS no componente

--- Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 38 CSS REACT/css-react/src/App.jsx
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 38 CSS REACT/css-react/src/App.jsx
@@ -1,10 +1,18 @@
 import './App.css'
+import MyComponent from './components/MyComponent'
+
+// 2 - css de componente
 
 function App() {
 
   return (
     <>
-    <div className='App'></div>
+    <div className='App'>
+      <h1>Hello World</h1>
+    </div>
+
+    {/* 2 - css de componente */}
+    <MyComponent />
     </>
   )
 }
---
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 38 CSS REACT/css-react/src/components/MyComponent.jsx
@@ -0,0 +1,12 @@
+import './MyComponent.css'
+
+const MyComponent = () => {
+  return (
+    <div>
+      <h1>Css de componete</h1>
+      <p>este paragrafo vai ter estilo</p>
+    </div>
+  )
+}
+
+export default MyComponent
