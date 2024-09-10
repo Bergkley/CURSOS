acb2759bd30b0864f8c7932a5e5b7c1e81cf42a1
Bergkley <bergkley@gmail.com>
2024-09-10 15:58:32 -0300

    feat: enviando dados

--- Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 41 HTTP REACT/http_react/src/App.jsx
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 41 HTTP REACT/http_react/src/App.jsx
@@ -19,6 +19,28 @@ function App() {
     fetchData();
   }, []);
 
+  // 2 - envio de dados
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+
+  const handleSubmit = async (e)=> {
+    e.preventDefault();
+
+    const product = {
+      name,
+      price
+    }
+
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(product)
+    });
+    
+  }
+
   return (
    <>
    <div className="App">
@@ -29,6 +51,20 @@ function App() {
         <li key={product.id}>{product.name}</li>
       ))}
     </ul>
+    {/* 2 - envio de dados */}
+    <div className="app-product">
+      <form onSubmit={handleSubmit}>
+        <label>
+           <span>Nome:</span>
+           <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+        </label>
+        <label>
+           <span>Preço:</span>
+           <input type="text" value={price} onChange={(e) => setPrice(e.target.value)} />
+        </label>
+        <input type="submit" value="Enviar"  />
+      </form>
+    </div>
    </div>
    </>
   )
