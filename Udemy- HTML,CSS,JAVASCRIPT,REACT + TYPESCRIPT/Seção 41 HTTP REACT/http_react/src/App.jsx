6a837d9ef366b06c3766e4c9fd904ad92b10a918
Bergkley <bergkley@gmail.com>
2024-09-10 15:49:57 -0300

    feat: resgantando dados

--- Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 41 HTTP REACT/http_react/src/App.jsx
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 41 HTTP REACT/http_react/src/App.jsx
@@ -1,11 +1,34 @@
+import { useState, useEffect } from "react";
 import './App.css'
 
+const url = "http://localhost:3000/products";
+
 function App() {
+  // 1 - resgantando dados
+  const [products, setProducts] = useState([]);
+
+   useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(url);
+
+      const data = await res.json();
+
+      setProducts(data);
+    }
+
+    fetchData();
+  }, []);
 
   return (
    <>
    <div className="App">
     <h1>HTTP em React</h1>
+    {/* 1 - resgantando dados */}
+    <ul>
+      {products.map((product) => (
+        <li key={product.id}>{product.name}</li>
+      ))}
+    </ul>
    </div>
    </>
   )
