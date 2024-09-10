de6fe76b0d4f614a03aa10147d64744f1b8bee33
Bergkley <bergkley@gmail.com>
2024-09-10 18:57:01 -0300

    feat: Seção 42 FORMULÁRIO MULTISTEP REACT estrutura do componente principal

--- Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 42 FORMULÁRIO MULTISTEP REACT/multistep_form_react/src/App.jsx
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 42 FORMULÁRIO MULTISTEP REACT/multistep_form_react/src/App.jsx
@@ -1,11 +1,34 @@
+// Components
+import {GrFormNext, GrFormPrevious} from 'react-icons/gr'
 import './App.css'
 
 function App() {
 
   return (
     <>
-     <div className="App">
-      <h1>Hello Word</h1>
+     <div className="app">
+      <div className="header">
+        <h2>Deixe sua avaliação</h2>
+        <p>
+          Ficamos felizes com a sua compra, utilize o formulário abaixo para avaliar o produto
+        </p>
+      </div>
+      <div className="form-container">
+        <p>etapas</p>
+        <form>
+          <div className="inputs-container"></div>
+          <div className="actions">
+            <button type='button'>
+              <GrFormPrevious/>
+              <span>Voltar</span>
+            </button>
+            <button type='submit'>
+              <GrFormNext/>
+              <span>Avançar</span>
+            </button>
+          </div>
+        </form>
+      </div>
      </div>
     </>
   )
