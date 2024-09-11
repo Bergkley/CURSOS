cfa630af117d0a3973f425ee1f33903abeec821d
Bergkley <bergkley@gmail.com>
2024-09-11 14:42:48 -0300

    feat: Seção 43 React Router criando componente base

--- Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 43 React Router/react_router/src/main.jsx
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 43 React Router/react_router/src/main.jsx
@@ -5,19 +5,32 @@ import "./index.css";
 
 import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
 
+
 import Contact from "./routes/Contact.jsx";
 import ErrorPage from "./routes/ErrorPage.jsx";
+import Home from "./routes/Home.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     errorElement: <ErrorPage />,
+    // 3 - componente base
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      }
+    ]
   },
-  {
-    path: "contact",
-    element: <Contact />,
-  },
+  // {
+  //   path: "contact",
+  //   element: <Contact />,
+  // },
   
 ])
 
