c2a25ff0444cb446538e7eff1ec5e4594d06aad4
Bergkley <bergkley@gmail.com>
2024-09-11 14:30:34 -0300

    feat: Seção 43 React Router configurando

--- Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 43 React Router/react_router/src/main.jsx
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 43 React Router/react_router/src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+
+import Contact from "./routes/Contact.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "contact",
+    element: <Contact />,
+  }
+])
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
---
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 43 React Router/react_router/src/routes/Contact.jsx
@@ -0,0 +1,9 @@
+const Contact = () => {
+  return (
+    <div>
+      <h1>Página de contato</h1>
+    </div>
+  )
+}
+
+export default Contact
