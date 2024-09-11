7b771b4a9b0da57dd0bb0393f21997b54f55dc28
Bergkley <bergkley@gmail.com>
2024-09-11 14:53:36 -0300

    feat: Seção 43 React Router carregando dados

--- Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 43 React Router/react_router/src/routes/Home.jsx
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 43 React Router/react_router/src/routes/Home.jsx
@@ -1,7 +1,19 @@
+import { useFetch } from "../hooks/useFetch";
+
+const url = "http://localhost:3000/products";
 const Home = () => {
+    const {data: items} = useFetch(url);
   return (
     <div>
       <h1>Home</h1>
+      <ul className="products">
+        {items && items.map((item) =>(
+            <li key={item.id}>
+                <h2>{item.name}</h2>
+                <p>R$: {item.price}</p> 
+            </li>
+        ))}
+      </ul>
     </div>
   )
 }
