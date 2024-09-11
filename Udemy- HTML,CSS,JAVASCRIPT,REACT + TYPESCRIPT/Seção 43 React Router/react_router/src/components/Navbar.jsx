3293bea167a6dc1e170204a79387cf4cb4220696
Bergkley <bergkley@gmail.com>
2024-09-11 15:43:33 -0300

    feat: link ativo

--- Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 43 React Router/react_router/src/components/Navbar.jsx
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 43 React Router/react_router/src/components/Navbar.jsx
@@ -1,10 +1,14 @@
-import { Link } from "react-router-dom"
+import { Link, NavLink } from "react-router-dom"
+
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
     <div>
-      <Link to="/">Home</Link>
-      <Link to="/contact">Contatos</Link>
+      {/* <Link to="/">Home</Link>
+      <Link to="/contact">Contatos</Link> */}
+      <NavLink to="/" className={({isActive}) => isActive ? "active" : ""}>Home</NavLink>
+      <NavLink to="/contact" className={({isActive}) => isActive ? "active" : ""}>Contatos</NavLink>
     </div>
   )
 }
