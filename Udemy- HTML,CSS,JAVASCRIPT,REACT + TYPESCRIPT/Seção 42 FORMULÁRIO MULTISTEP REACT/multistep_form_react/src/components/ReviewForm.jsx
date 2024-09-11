84510186f57d6dfef29518a4e24b128dec77a244
Bergkley <bergkley@gmail.com>
2024-09-11 09:33:10 -0300

    feat: componente ReviewForm

--- Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 42 FORMULÁRIO MULTISTEP REACT/multistep_form_react/src/components/ReviewForm.jsx
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 42 FORMULÁRIO MULTISTEP REACT/multistep_form_react/src/components/ReviewForm.jsx
@@ -1,10 +1,65 @@
+import {
+  BsFillEmojiHeartEyesFill,
+  BsFillEmojiSmileFill,
+  BsFillEmojiNeutralFill,
+  BsFillEmojiFrownFill,
+} from "react-icons/bs";
+
+import "./ReviewForm.css";
 
 const ReviewForm = () => {
   return (
-    <div>
-      <h1>Review</h1>
+    <div className="review-form">
+      <div className="form-control score-container">
+        <label className="radio-container">
+          <input
+            type="radio"
+            value="unsatisfied"
+            name="review"
+            required
+          />
+          <BsFillEmojiFrownFill />
+          <p>Insatisfeito</p>
+        </label>
+        <label className="radio-container">
+          <input
+            type="radio"
+            value="neutral"
+            name="review"
+          />
+          <BsFillEmojiNeutralFill />
+          <p>Poderia ser melhor</p>
+        </label>
+        <label className="radio-container">
+          <input
+            type="radio"
+            value="satisfied"
+            name="review"
+          />
+          <BsFillEmojiSmileFill />
+          <p>Satisfeito</p>
+        </label>
+        <label className="radio-container">
+          <input
+            type="radio"
+            value="very_satisfied"
+            name="review"
+          />
+          <BsFillEmojiHeartEyesFill />
+          <p>Muito satisfeito</p>
+        </label>
+      </div>
+      <div className="form-control">
+        <label htmlFor="comment">Comentário:</label>
+        <textarea
+          name="cooment"
+          id="comment"
+          placeholder="Conte como foi a sua experiência com o produto..."
+          required
+        ></textarea>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ReviewForm
+export default ReviewForm;
