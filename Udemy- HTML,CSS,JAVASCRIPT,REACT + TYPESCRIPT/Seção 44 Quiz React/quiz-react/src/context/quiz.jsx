05db4f715b68c9ed7be08d57ceac275caa9e1f68
Bergkley <bergkley@gmail.com>
2024-09-13 10:47:56 -0300

    feat: Seção 44 Quiz React criado contexto

---
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 44 Quiz React/quiz-react/src/context/quiz.jsx
@@ -0,0 +1,12 @@
+import { createContext } from "react";
+
+export const QuizContext = createContext();
+
+export const QuizProvider = ({ children }) => {
+    const value = {};
+    return (
+        <QuizContext.Provider value={value}>
+            {children}
+        </QuizContext.Provider>
+    )
+}
--- Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 44 Quiz React/quiz-react/src/main.jsx
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 44 Quiz React/quiz-react/src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { QuizProvider } from './context/quiz.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <QuizProvider>
+      <App />
+    </QuizProvider>
   </StrictMode>,
 )
