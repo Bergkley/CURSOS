0cf9b1f4b88b91059695515546e957bb7831a6f3
Bergkley <bergkley@gmail.com>
2024-09-14 19:35:24 -0300

    feat: inserindo dados no banco

--- Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 49 Party Time/backend/controllers/serviceController.js
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 49 Party Time/backend/controllers/serviceController.js
@@ -1,7 +1,23 @@
 const {Service: ServiceModel} = require('../models/Service');
 
 const serviceController = {
+    create: async (req, res) => {
+        try{
+            const service = {
+                name: req.body.name,
+                description: req.body.description,
+                price: req.body.price,
+                image: req.body.image
+            }
 
+            const response = await ServiceModel.create(service);
+
+            res.status(201).json({response, msg: "Serviço criado com sucesso"});
+
+        }catch (error) {
+            console.log(error)
+        }
+    }
 }
 
 module.exports = serviceController;
--- Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 49 Party Time/backend/routes/services.js
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 49 Party Time/backend/routes/services.js
@@ -3,4 +3,6 @@ const serviceController = require("../controllers/serviceController");
 
 // Funções
 
+router.route("/services").post((req, res) => serviceController.create(req, res));
+
 module.exports = router;
