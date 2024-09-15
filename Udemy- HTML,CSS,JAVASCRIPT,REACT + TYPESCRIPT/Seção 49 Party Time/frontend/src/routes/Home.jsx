c5ebe351a7bc2a423453a95f5dd46c2d6d490d68
Bergkley <bergkley@gmail.com>
2024-09-14 23:07:28 -0300

    feat: css da home

--- Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 49 Party Time/frontend/src/routes/Home.jsx
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 49 Party Time/frontend/src/routes/Home.jsx
@@ -2,7 +2,7 @@ import { useEffect,useState } from "react"
 import partyFetch from "../axios/config"
 import { Link } from "react-router-dom"
 
-
+import "./Home.css"
 
 const Home = () => {
     const [parties, setParties] = useState([]);
