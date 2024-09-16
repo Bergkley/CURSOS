350d32de85123618a94172ea760a329eece2cb53
Bergkley <bergkley@gmail.com>
2024-09-16 20:57:08 -0300

    feat: logica da contagem regressiva

--- Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 45 Projeto CountDown/countDown/src/App.jsx
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 45 Projeto CountDown/countDown/src/App.jsx
@@ -4,18 +4,21 @@ import Counter from './components/Counter'
 import Title from './components/Title'
 
 import NewYear from './assets/newyear.jpg'
+import useCountdown from './hooks/useCountdown'
+
 
 function App() {
+  const [day, hour, minute, second] = useCountdown("2025-01-01")
   return (
     <>
       <div className="App" style={{backgroundImage: `url(${NewYear})`}}>
         <div className="container">
           <Title title="Contagem regressiva para 2023"/>
           <div className="countdown-container"> 
-            <Counter title="Dias" number={2}/>
-            <Counter title="Horas" number={2}/>
-            <Counter title="Minutos" number={2}/>
-            <Counter title="Segundos" number={2}/>
+            <Counter title="Dias" number={day}/>
+            <Counter title="Horas" number={hour}/>
+            <Counter title="Minutos" number={minute}/>
+            <Counter title="Segundos" number={second}/>
           </div>
         </div>
         
