30191e8c6a38447b59e18a2ec0df26976259aedf
Bergkley <bergkley@gmail.com>
2024-09-16 21:18:23 -0300

    feat: criando o componente home

--- Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 45 Projeto CountDown/countDown/src/routes/Home.jsx
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 45 Projeto CountDown/countDown/src/routes/Home.jsx
@@ -1,10 +1,66 @@
+import "./Home.css";
 
 const Home = () => {
-  return (
-    <div>
-      Home
-    </div>
-  )
-}
+    const [title, setTitle] = useState("");
+    const [date, setDate] = useState("");
+    const [image, setImage] = useState("");
+    const [color, setColor] = useState("");
 
-export default Home
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const eventObject = {
+            title,
+            date,
+            image,
+            color,
+        };
+        
+    }
+  return (
+    <div className="home">
+      <h2>Monte a sua contagem regressiva!</h2>
+      <form className="countdown-form" onSubmit={handleSubmit}>
+        <label>
+          <span>Título:</span>
+          <input
+            type="text"
+            name="title"
+            placeholder="Digite o título do evento"
+            onChange={(e) => setTitle(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          <span>Data do evento:</span>
+          <input
+            type="date"
+            name="date"
+            onChange={(e) => setDate(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          <span>Imagem:</span>
+          <input
+            type="text"
+            placeholder="Insira a URL da imagem"
+            onChange={(e) => setImage(e.target.value)}
+          />
+        </label>
+        <label>
+          <span>Cor do tema:</span>
+          <input
+            type="color"
+            name="color"
+            onChange={(e) => setColor(e.target.value)}
+            required
+          />
+        </label>
+        <input type="submit" value="Criar" />
+      </form>
+    </div>
+  );
+};
+
+export default Home;
