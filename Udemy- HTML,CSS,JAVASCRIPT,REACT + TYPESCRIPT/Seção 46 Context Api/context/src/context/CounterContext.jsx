c0c40251c9cf82470a372fafa24a1436b3c8066e
Bergkley <bergkley@gmail.com>
2024-09-17 20:08:03 -0300

    feat: criando o contexto

---
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 46 Context Api/context/src/context/CounterContext.jsx
@@ -0,0 +1,5 @@
+// 1 - criar context
+
+import { createContext } from 'react'
+
+export const CounterContext = createContext();
--- Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 46 Context Api/context/src/main.jsx
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 46 Context Api/context/src/main.jsx
@@ -1,10 +1,43 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// 2 - criando o provider
+import { CounterContextProvider } from "./context/CounterContext";
+
+// 3 - criando uma estrutura
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Contact from "./routes/Contact";
+import Home from "./routes/Home";
+
+// 5 - contexto mais complexo
+import { TitleColorContextProvider } from "./context/TitleColorContext";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <TitleColorContextProvider>
+      <CounterContextProvider>
+        <RouterProvider router={router} />
+      </CounterContextProvider>
+    </TitleColorContextProvider>
+  </React.StrictMode>
+);
