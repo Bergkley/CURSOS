d6b36605556b25ec81dc7542482ed32f86a72e77
Bergkley <bergkley@gmail.com>
2024-09-17 20:12:09 -0300

    feat: criando o  provedor

--- Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 46 Context Api/context/src/context/CounterContext.jsx
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 46 Context Api/context/src/context/CounterContext.jsx
@@ -1,5 +1,19 @@
 // 1 - criar context
 
-import { createContext } from 'react'
+import { createContext, useState } from 'react'
 
 export const CounterContext = createContext();
+
+// 2 - criar provider
+
+export const CounterContextProvider = ({children}) => {
+
+    const [counter, setCounter] = useState(5);
+
+    return (
+        <CounterContext.Provider value={{counter,setCounter}}>
+            {children}
+        </CounterContext.Provider>
+    )
+
+}
