b6910acd0f345872baf8b48030d5a0541da179f2
Bergkley <bergkley@gmail.com>
2024-09-18 14:32:39 -0300

    feat: Seção 47 HOOKS useReducer

--- Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 47 HOOKS/hooks/src/routes/Home.jsx
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 47 HOOKS/hooks/src/routes/Home.jsx
@@ -1,3 +1,4 @@
+import HookUseReducer from "../components/HookUseReducer";
 import HookUseState from "../components/HookUseState";
 
 
@@ -7,6 +8,7 @@ const Home = () => {
     <div>
       <h1>Home</h1>
       <HookUseState />
+      <HookUseReducer />
     </div>
   );
 };
