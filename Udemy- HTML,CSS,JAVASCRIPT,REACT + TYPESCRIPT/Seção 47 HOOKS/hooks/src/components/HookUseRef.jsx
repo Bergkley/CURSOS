c58296778bacc89ac778b7a989fe1b48f557a81a
Bergkley <bergkley@gmail.com>
2024-09-18 15:55:00 -0300

    feat: Seção 47 HOOKS useRef e o DOM

--- Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 47 HOOKS/hooks/src/components/HookUseRef.jsx
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 47 HOOKS/hooks/src/components/HookUseRef.jsx
@@ -9,6 +9,17 @@ const HookUseRef = () => {
     useEffect(() => {
         numberRef.current = numberRef.current + 1
     })
+
+    // 2 - useRef e DOM
+    const inputRef = useRef();
+
+    const [text,setText]= useState("");
+
+    const handleSumbit = (e) => {
+        e.preventDefault();
+        setText("");
+        inputRef.current.focus();
+    }
   return (
     <div>
       <h2>useRef</h2>
@@ -17,6 +28,11 @@ const HookUseRef = () => {
       <button onClick={() => setCounter(counter + 1)}>Contador A</button>
       <p>Counter 2: {counterB}</p>
       <button onClick={() => setCounterB(counterB + 1)}>Contador B</button>
+      <form onSubmit={handleSumbit}>
+        <input type="text" ref={inputRef} value={text} onChange={(e) => setText(e.target.value)} />
+        <input type="submit" value="enviar" />
+      </form>
+
     </div>
   )
 }
