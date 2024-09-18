d7942b2be489ee97be26cb42a2e37b2e4b0da5dd
Bergkley <bergkley@gmail.com>
2024-09-18 14:27:02 -0300

    feat: Seção 47 HOOKS useState e inputs

--- Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 47 HOOKS/hooks/src/components/HookUseState.jsx
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 47 HOOKS/hooks/src/components/HookUseState.jsx
@@ -9,12 +9,28 @@ const HookUseState = () => {
 
         setName("Berg Brasil")
     }
+
+    // 2 - useState e input
+    
+    const [age,setAge]= useState(18);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(age)
+    }
   return (
     <div>
       <h2>UseState</h2>
       <p>Variavel: {userName}</p>
       <p>UseState: {name}</p>
       <button onClick={changeNames}>mudar o nome</button>
+      {/* 2 - useState e input */}
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={age} onChange={(e) => setAge(e.target.value)} placeholder="digite sua idade" />
+        <input type="submit" value="enviar" />
+      </form>
+      <p>Sua idade: {age}</p>
+      <hr />
     </div>
   )
 }
