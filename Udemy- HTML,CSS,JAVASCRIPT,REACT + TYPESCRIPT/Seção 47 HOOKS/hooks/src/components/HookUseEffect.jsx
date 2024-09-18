a924c04019c92c099f4a72947891d9e99ee96a4e
Bergkley <bergkley@gmail.com>
2024-09-18 14:54:03 -0300

    feat: Seção 47 HOOKS useEffect array de dependências com o valor

--- Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 47 HOOKS/hooks/src/components/HookUseEffect.jsx
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 47 HOOKS/hooks/src/components/HookUseEffect.jsx
@@ -18,11 +18,23 @@ const HookUseEffect = () => {
         console.log('Serei executado apenas uma vez')
     }, [])
 
+    // 3 - array de dependências com o valor
+
+    const [anotherNumber, setAnotherNumber] = useState(0);
+
+    useEffect(() => {
+        if(anotherNumber > 0){
+            console.log('Serei executado apenas quando muda o anotherNumber')
+        }
+    }, [anotherNumber])
+
   return (
     <div>
       <h2>UseEffect</h2>
       <p>Number: {number}</p>
       <button onClick={changeSomething}>Executar</button>
+      <p>Another number: {anotherNumber}</p>
+      <button onClick={() => setAnotherNumber(anotherNumber + 1)}>Mudar another number</button>
     </div>
   )
 }
