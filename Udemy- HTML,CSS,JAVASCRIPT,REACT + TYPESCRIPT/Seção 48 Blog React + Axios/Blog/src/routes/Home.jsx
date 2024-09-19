1ae7eaab50e6030a4259776eadee760c979ba8a7
Bergkley <bergkley@gmail.com>
2024-09-19 16:39:02 -0300

    feat:Seção 48 Blog React + Axios resgantando dados

--- Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 48 Blog React + Axios/Blog/src/routes/Home.jsx
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 48 Blog React + Axios/Blog/src/routes/Home.jsx
@@ -1,5 +1,27 @@
+import { useEffect, useState } from "react";
+import "./Home.css";
+import axios from "axios";
 
 const Home = () => {
+    const [posts, setPosts] = useState([])
+
+    const getPosts = async() =>{
+        try {
+            const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+
+            const data = response.data;
+
+            
+            
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        getPosts()
+    }, [])
+
   return (
     <div>
       Home
