3ca0748b5ae2e703eabebae0075ae5190a3ebb88
Bergkley <bergkley@gmail.com>
2024-09-19 16:28:27 -0300

    feat:Seção 48 Blog React + Axios criando navbar

--- Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 48 Blog React + Axios/Blog/src/App.jsx
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 48 Blog React + Axios/Blog/src/App.jsx
@@ -1,12 +1,14 @@
 
 import { Outlet } from 'react-router-dom'
 import './App.css'
+import NavBar from './components/NavBar'
 
 function App() {
 
   return (
     <>
      <div className="App">
+     <NavBar />
       <div className="container">
         <Outlet />
       </div>
---
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 48 Blog React + Axios/Blog/src/components/NavBar.jsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom"
+
+const NavBar = () => {
+  return (
+    <div>
+      <nav className="navbar">
+        <h2>
+           <Link to={"/"}>Blog</Link> 
+        </h2>
+        <ul>
+            <li>
+                <Link to={"/"}>Home</Link>
+            </li>
+            <li>
+                <Link to={"/new"}>Novo Post</Link>
+            </li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default NavBar
