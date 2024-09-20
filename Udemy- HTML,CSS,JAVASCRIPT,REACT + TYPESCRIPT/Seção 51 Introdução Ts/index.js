dabae4be98e0961b894eac3fb783005ac5865048
Bergkley <bergkley@gmail.com>
2024-09-20 10:42:45 -0300

    feat: Introdução ao Typescripts

---
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 51 Introdução Ts/index.js
@@ -0,0 +1,7 @@
+var firstName = "Berg";
+var anotherName = 1;
+var x = true;
+function greeting(name) {
+    return console.log('hello ' + name);
+}
+greeting(firstName);
---
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 51 Introdução Ts/index.ts
@@ -0,0 +1,9 @@
+var firstName = "Berg";
+var anotherName = 1;
+var x = true;
+
+function greeting (name:string) {
+    return console.log('hello ' + name);
+}
+
+greeting(firstName)
