dac033d3383aa00f0bd3b49578f58a61c96674d2
Bergkley <bergkley@gmail.com>
2024-09-20 18:39:34 -0300

    feat: 12 - type alias

--- Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 53 Avançando nos tipos em TypeScripts/index.ts
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 53 Avançando nos tipos em TypeScripts/index.ts
@@ -101,3 +101,15 @@ function showUserRole(role: boolean | string) {
 
 console.log(showUserRole(false));
 console.log(showUserRole("Admin"));
+
+// 12 - type alias
+
+type ID = string | number;
+
+function showId(id: ID) {
+  console.log(`O ID do usuário e ${id}`)
+}
+
+showId(1);
+showId("200");
+// showId(true);
