17f9a4608217284ce1a5718089e48d7bb244e362
Bergkley <bergkley@gmail.com>
2024-09-20 18:16:40 -0300

    feat: outra sintaxe do array

---
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 53 Avançando nos tipos em TypeScripts/index.js
@@ -0,0 +1,8 @@
+// 1 - arrays
+var numbers = [1, 2, 3, 4, 5];
+numbers.push(6);
+// numbers = "teste"
+var nomes = ["Berg", "John"];
+// outra sintaxe do array
+var nums = [1, 2, 3, 4, 5];
+nums.push(6);
--- Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 53 Avançando nos tipos em TypeScripts/index.ts
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 53 Avançando nos tipos em TypeScripts/index.ts
@@ -7,3 +7,8 @@ numbers.push(6);
 // numbers = "teste"
 
 const nomes: string[] = ["Berg","John"]
+
+// outra sintaxe do array
+const nums: Array<number> = [1, 2, 3, 4, 5];
+
+nums.push(6)
