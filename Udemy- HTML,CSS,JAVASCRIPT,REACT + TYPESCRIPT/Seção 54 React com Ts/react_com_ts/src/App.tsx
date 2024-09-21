59e1c3a9ec2a974b9366a9e043b1b1f4be154b0f
Bergkley <bergkley@gmail.com>
2024-09-21 12:37:03 -0300

    feat: Seção 54 React com Ts Type

--- Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 54 React com Ts/react_com_ts/src/App.tsx
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 54 React com Ts/react_com_ts/src/App.tsx
@@ -5,16 +5,29 @@ import FirstComponent from "./components/FirstComponent";
 import SecondComponent from "./components/SecondComponent";
 import State from "./components/State";
 
+type textOrNull = string | null;
+
+
 function App() {
   //  1 - variaveis
   const name:string = "Berg";
   const age: number = 19;
   const isWorking: boolean = true;
 
+  // type: 
+  const myText: textOrNull = "Tem algum texto aqui";
+  const mySecondText: textOrNull = null;
+
+  type fixed = "Isso" | "Ou" | "Aquilo";
+
+
   // 2 - funções
   const userGreeting = (name:string):string => {
     return `Hello, ${name}`;
   }
+
+  const testandoFixed:fixed = "Isso";
+
 
   return (
     <div className="App">
@@ -27,6 +40,7 @@ function App() {
       <SecondComponent name={name} />
       <Destructuring title="Primeiro post" content="Conteudo" commentsQty={5} tags={["tag1", "tag2"]} category={Category.TYPESCRIPT} />
       <State />
+      {myText && <p>tem texto na variavel{myText}</p>}
     </div>
   );
 }
