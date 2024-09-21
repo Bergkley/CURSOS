c1ba1cb444145d7d5103d131c7bb737cc4d249a2
Bergkley <bergkley@gmail.com>
2024-09-21 12:13:12 -0300

    feat: Seção 54 React com Ts Variavel + funções

--- Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 54 React com Ts/react_com_ts/src/App.tsx
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 54 React com Ts/react_com_ts/src/App.tsx
@@ -1,10 +1,23 @@
 
-import './App.css';
 
 function App() {
+  //  1 - variaveis
+  const name:string = "Berg";
+  const age: number = 19;
+  const isWorking: boolean = true;
+
+  // 2 - funções
+  const userGreeting = (name:string):string => {
+    return `Hello, ${name}`;
+  }
+
   return (
     <div className="App">
       <h1>TypesScript com React</h1>
+      <h2>Nome: {name}</h2>
+      <p>Idade: {age}</p>
+      {isWorking && <p>Esta trabalhando</p>}
+      <p>{userGreeting(name)}</p>
     </div>
   );
 }
