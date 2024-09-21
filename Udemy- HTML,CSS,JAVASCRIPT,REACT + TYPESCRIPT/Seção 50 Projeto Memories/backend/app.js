d0b149d1e43b6b5f0c7dc5e534e8925db9e6ef20
Bergkley <bergkley@gmail.com>
2024-09-21 16:21:21 -0300

    feat: setup inicial

---
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 50 Projeto Memories/backend/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
+
+const port =  3000;
+
+app.listen(port, () => {
+    console.log(`Servidor online na porta ${port}`);
+})
