e037af04c5e3ba42a674c65f7254350776ebb093
Bergkley <bergkley@gmail.com>
2024-09-22 15:04:47 -0300

    feat: estilos Home

--- Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 50 Projeto Memories/frontend/frontend/src/App.jsx
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 50 Projeto Memories/frontend/frontend/src/App.jsx
@@ -2,6 +2,9 @@ import { Outlet } from 'react-router-dom'
 import './App.css'
 import NavBar from './components/NavBar'
 import Footer from './components/Footer'
+import { ToastContainer } from 'react-toastify'
+
+import "react-toastify/dist/ReactToastify.css";
 
 
 
@@ -10,6 +13,7 @@ function App() {
   return (
     <>
       <div className="App">
+        <ToastContainer position='top-right' autoClose={5000} hideProgressBar={false} newestOnTop={false} closeOnClick  rtl={false} pauseOnFocusLoss draggable pauseOnHover theme="light" />
         <NavBar />
         <div className="container">
           <Outlet />
