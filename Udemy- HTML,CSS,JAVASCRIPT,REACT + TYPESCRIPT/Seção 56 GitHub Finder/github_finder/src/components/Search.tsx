e0079236b2126ffa7457d28e8af44ff688c0b8c2
Bergkley <bergkley@gmail.com>
2024-09-23 17:10:12 -0300

    feat: commit apos o enter

--- Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 56 GitHub Finder/github_finder/src/components/Search.tsx
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 56 GitHub Finder/github_finder/src/components/Search.tsx
@@ -10,12 +10,18 @@ import classes from "./Search.module.css";
 const Search = ({loadUser}:SearchProps) => {
     const [userName, setUserName] = useState('');
 
+    const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if(e.key === 'Enter') {
+            loadUser(userName);
+        }
+    }
+
   return (
     <div className={classes.search}>
         <h2>Busque por um usuário:</h2>
         <p>Conheça seus melhores repositórios</p>
         <div className={classes.search_container}>
-            <input type="text" placeholder='Digite o nome do usuário' onChange={(e) => setUserName(e.target.value)}/>
+            <input type="text" placeholder='Digite o nome do usuário' onChange={(e) => setUserName(e.target.value)} onKeyDown={(e) => handleKeyDown(e)}/>
             <button onClick={()=> loadUser(userName)}>
                 <BsSearch />
             </button>
