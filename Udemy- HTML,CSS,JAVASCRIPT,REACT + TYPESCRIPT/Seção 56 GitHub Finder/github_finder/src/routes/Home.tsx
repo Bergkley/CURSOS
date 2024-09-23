74dc2ea6335349a14c4076e8166cb415408bdaab
Bergkley <bergkley@gmail.com>
2024-09-23 17:24:48 -0300

    feat: comentario de erros

--- Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 56 GitHub Finder/github_finder/src/routes/Home.tsx
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 56 GitHub Finder/github_finder/src/routes/Home.tsx
@@ -1,36 +1,53 @@
-import  { useState } from 'react'
-import Search from '../components/Search';
+import { UserProps } from "../types/user";
 
-import {UserProps} from '../types/user';
-import User from '../components/User';
+import { useState } from "react";
 
+import Search from "../components/Search";
+import User from "../components/User";
+import Error from "../components/Error";
 
-function Home() {
-    const [user,setUser] = useState<UserProps | null>(null);
-    
-    const loadUser = async (username: string) => {
-        const res = await fetch(`https://api.github.com/users/${username}`);
+const Home = () => {
+  const [user, setUser] = useState<UserProps | null>(null);
+  const [error, setError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
-        const data = await res.json();
+  const loadUser = async function (userName: string) {
+    setUser(null);
+    setIsLoading(true);
 
-        const {avatar_url, login, location, followers, following} = data;
+    const res = await fetch(`https://api.github.com/users/${userName}`);
 
-        const userData: UserProps = {
-            avatar_url,
-            login,
-            location,
-            followers,
-            following
-        }
-        setUser(userData);
+    const data = await res.json();
+
+    setIsLoading(false);
+
+    if (res.status === 404) {
+      setError(true);
+      return;
     }
+
+    setError(false);
+
+    const { avatar_url, login, location, followers, following } = data;
+
+    const userData: UserProps = {
+      avatar_url,
+      login,
+      location,
+      followers,
+      following,
+    };
+
+    setUser(userData);
+  };
 
   return (
     <div>
-      <Search  loadUser={loadUser}/>
-       {user && <User {...user}/>  }	
-      </div>
-  )
-}
+      <Search loadUser={loadUser} />
+      {user && <User {...user} />}
+      {error && <Error />}
+    </div>
+  );
+};
 
-export default Home
+export default Home;
