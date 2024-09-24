961ae004dd3fe60b97da1ee9180532e9985f4e95
Bergkley <bergkley@gmail.com>
2024-09-24 11:31:45 -0300

    feat:Router Error

--- Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 56 GitHub Finder/github_finder/src/routes/PageError.tsx
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 56 GitHub Finder/github_finder/src/routes/PageError.tsx
@@ -1,22 +1,28 @@
-import { useRouteError, RouteError  } from "react-router-dom";
+import { useRouteError } from "react-router-dom";
 import BackBtn from './../components/BackBtn';
 import classes from "./PageError.module.css";
+
+type ErrorResponse = {
+  status?: number;
+  statusText?: string;
+};
+
 const PageError = () => {
-  const error:RouteError = useRouteError();
-  console.log({ error });
+  const error = useRouteError();
+  const errorResponse = error as ErrorResponse;
 
   return (
     <div className={classes.error}>
       <h1>Ops!</h1>
       <p>Ops! Parece que algo deu errado.</p>
-      
+
       <p>
-        {error.status === 404 ? (
+        {errorResponse?.status === 404 ? (
           <span>
-            Página não encontrada <span className={classes.errorNumber}>{error.status}</span>
+            Página não encontrada <span className={classes.errorNumber}>{errorResponse.status}</span>
           </span>
         ) : (
-          <i>Status: {error.status}</i>
+          <i>Status: {errorResponse?.status}</i>
         )}
       </p>
       <BackBtn />
