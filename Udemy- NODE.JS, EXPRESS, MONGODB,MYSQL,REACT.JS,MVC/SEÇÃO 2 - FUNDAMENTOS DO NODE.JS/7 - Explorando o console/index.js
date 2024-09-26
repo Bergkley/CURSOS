423958b98bce15dccde52863f40de902b873456c
Bergkley <bergkley@gmail.com>
2024-09-26 18:00:54 -0300

    feat: explorando console

---
+++ Udemy- NODE.JS, EXPRESS, MONGODB,MYSQL,REACT.JS,MVC/SEÇÃO 2 - FUNDAMENTOS DO NODE.JS/7 - Explorando o console/index.js
@@ -0,0 +1,24 @@
+// mais de uma valor
+
+const x =10;
+const y = 'Bergkley';
+const z = [1,2];
+
+console.log(x,y,z);
+
+// contagem de impressões
+
+console.count('o valor da contagem :');
+console.count('o valor da contagem :');
+console.count('o valor da contagem :');
+console.count('o valor da contagem :');
+console.count('o valor da contagem :');
+
+// variavel entre string
+console.log("Meu nome é %s", y)
+
+// limpar o console
+
+setTimeout(()=>{
+    console.clear()
+},2000)
