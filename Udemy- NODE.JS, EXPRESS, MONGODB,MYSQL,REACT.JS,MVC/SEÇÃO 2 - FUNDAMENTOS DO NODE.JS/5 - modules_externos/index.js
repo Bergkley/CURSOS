b1614e8f26cca7b285bb482d576c0239b13f5598
Bergkley <bergkley@gmail.com>
2024-09-26 17:49:01 -0300

    feat:Node.js Module externno

---
+++ Udemy- NODE.JS, EXPRESS, MONGODB,MYSQL,REACT.JS,MVC/SEÇÃO 2 - FUNDAMENTOS DO NODE.JS/5 - modules_externos/index.js
@@ -0,0 +1,12 @@
+// --nome=Bergkley --idade=19
+const minimist = require("minimist");
+
+const args = minimist(process.argv.slice(2));
+
+console.log(args);
+
+const nome = args["nome"];
+const idade = args["idade"];
+
+console.log(nome);
+console.log(idade);
