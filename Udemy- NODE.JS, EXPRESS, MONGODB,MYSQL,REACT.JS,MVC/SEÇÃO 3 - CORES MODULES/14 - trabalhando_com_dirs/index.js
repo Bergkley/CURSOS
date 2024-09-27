bcd748030a892e515fc33703c4ad9274d6fe5958
Bergkley <bergkley@gmail.com>
2024-09-26 23:21:48 -0300

    feat: criando pasta

---
+++ Udemy- NODE.JS, EXPRESS, MONGODB,MYSQL,REACT.JS,MVC/SEÇÃO 3 - CORES MODULES/14 - trabalhando_com_dirs/index.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+if(!fs.existsSync('./minhapasta')) {
+    console.log("Não existe")
+    fs.mkdirSync('minhapasta');
+}else if(fs.existsSync('./minhapasta')) {
+    console.log(" existe")
+}
+
+
+
