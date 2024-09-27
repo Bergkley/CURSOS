cf563494789f2024cfaeee2bd89a4a613a78ca5f
Bergkley <bergkley@gmail.com>
2024-09-26 23:12:20 -0300

    feat: detalhe de arquivos

---
+++ Udemy- NODE.JS, EXPRESS, MONGODB,MYSQL,REACT.JS,MVC/SEÇÃO 3 - CORES MODULES/11 - detalhes_arquivos/index.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+fs.stat('novoarquivo.txt', function(err, stats) {
+    if(err){
+        console.log(err);
+        return
+    }
+
+    console.log(stats.isFile());
+    console.log(stats.isDirectory());
+    console.log(stats.isSymbolicLink());
+    console.log(stats.size);
+    console.log(stats.ctime);
+})
+
