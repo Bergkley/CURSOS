db3f5b56396cc89eb54eeb07564ef22fdc1115e7
Bergkley <bergkley@gmail.com>
2024-09-28 11:29:34 -0300

    feat: criando conta

--- Udemy- NODE.JS, EXPRESS, MONGODB,MYSQL,REACT.JS,MVC/SEÇÃO 5 - ACCOUNTS/index.js
+++ Udemy- NODE.JS, EXPRESS, MONGODB,MYSQL,REACT.JS,MVC/SEÇÃO 5 - ACCOUNTS/index.js
@@ -1,7 +1,84 @@
 // modulos externos
-const inquirer = require('inquirer');
-const chalk = require('chalk');
+const inquirer = require("inquirer");
+const chalk = require("chalk");
 
 // modulos internos
-const fs = require('fs');
-console.log('Iniciamo o Accounts')
+const fs = require("fs");
+
+operation();
+
+function operation() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "action",
+        message: "O que deseja fazer?",
+        choices: [
+          "Criar conta",
+          "Consultar saldo",
+          "Depositar",
+          "Sacar",
+          "Sair",
+        ],
+      },
+    ])
+    .then((answer) => {
+      console.log(answer);
+
+      const action = answer["action"];
+
+      if (action === "Criar conta") {
+        createAccount();
+      }
+    })
+    .catch((err) => console.log(err));
+}
+
+// create an account
+
+function createAccount() {
+  console.log(chalk.bgGreen.black("Parábens por escolher o nosso  banco"));
+  console.log(chalk.green("Defina as opções da sua conta a seguir"));
+
+  buildAccount();
+}
+
+function buildAccount() {
+  inquirer
+    .prompt([
+      {
+        name: "accountName",
+        message: "Qual o nome da sua conta?",
+      },
+    ])
+    .then((answer) => {
+      const accountName = answer["accountName"];
+
+      console.info(accountName);
+
+      if (!fs.existsSync("accounts")) {
+        fs.mkdirSync("accounts");
+      }
+
+      if (fs.existsSync(`accounts/${accountName}.json`)) {
+        console.log(
+          chalk.bgRed.black("Esta conta ja existe, escolha outro nome")
+        );
+        buildAccount();
+        return;
+      }
+
+      fs.writeFileSync(
+        `accounts/${accountName}.json`,
+        `{"balance": 0}`,
+        function (err) {
+          console.log(err);
+        }
+      );
+
+      console.log(chalk.green("Conta criada com sucesso!"));
+      operation();
+    })
+    .catch((err) => console.log(err));
+}
