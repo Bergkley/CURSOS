571075f4da118982c4c1afcc73d1d683af41c48f
Bergkley <bergkley@gmail.com>
2024-09-30 20:35:20 -0300

    feat:1 - instalacao_mysql

---
+++ Udemy- NODE.JS, EXPRESS, MONGODB,MYSQL,REACT.JS,MVC/SEÇÃO 8 - MYSQL/1 - instalacao_mysql/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const mysql = require('mysql');
+
+const app = express();
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+    res.render('home')
+})
+
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'nodemysql2'
+});
+
+conn.connect(function (err) {
+    if (err) {
+        console.log(err);
+    }
+    console.log('Conectado ao MySQL!');
+
+    app.listen(3000);
+});
