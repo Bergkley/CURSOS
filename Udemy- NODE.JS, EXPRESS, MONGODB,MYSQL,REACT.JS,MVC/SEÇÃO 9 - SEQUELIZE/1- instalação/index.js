95ad4ae01096ef9ade9b50a527b441169619a261
Bergkley <bergkley@gmail.com>
2024-10-01 18:13:09 -0300

    feat: instalação

---
+++ Udemy- NODE.JS, EXPRESS, MONGODB,MYSQL,REACT.JS,MVC/SEÇÃO 9 - SEQUELIZE/1- instalação/index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+
+const conn = require('./db/conn')
+
+
+const app = express()
+
+app.engine('handlebars', exphbs())
+app.set('view engine', 'handlebars')
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+)
+
+app.use(express.json())
+
+app.use(express.static('public'))
+
+app.get('/', function (req, res) {
+  res.render('home')
+})
+
+
+
+
+
+
+app.listen(3000)
