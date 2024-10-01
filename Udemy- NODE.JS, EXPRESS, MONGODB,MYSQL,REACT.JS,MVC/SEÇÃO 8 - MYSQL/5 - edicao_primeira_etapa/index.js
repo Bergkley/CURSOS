efc0587b17a1bea01ea9896ccee90fc513fd4d9c
Bergkley <bergkley@gmail.com>
2024-09-30 21:49:16 -0300

    feat:5 - edicao_primeira_etapa

---
+++ Udemy- NODE.JS, EXPRESS, MONGODB,MYSQL,REACT.JS,MVC/SEÇÃO 8 - MYSQL/5 - edicao_primeira_etapa/index.js
@@ -0,0 +1,100 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const mysql = require('mysql')
+
+const app = express()
+
+app.engine('handlebars', exphbs())
+app.set('view engine', 'handlebars')
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+)
+
+app.use(express.json())
+
+app.use(express.static('public'))
+
+app.get('/', function (req, res) {
+  res.render('home')
+})
+
+app.post('/books/insertbook', function (req, res) {
+  const title = req.body.title
+  const pageqty = req.body.pageqty
+
+  const query = `INSERT INTO books (title, pageqty) VALUES ('${title}', ${pageqty})`
+
+  conn.query(query, function (err) {
+    if (err) {
+      console.log(err)
+    }
+
+    res.redirect('/')
+  })
+})
+
+app.get('/books', function (req, res) {
+  const query = `SELECT * FROM books`
+
+  conn.query(query, function (err, data) {
+    if (err) {
+      console.log(err)
+    }
+
+    const books = data
+
+    res.render('books', { books })
+  })
+})
+
+app.get('/books/:id', function (req, res) {
+  const id = req.params.id;
+
+  const query = `SELECT * FROM books WHERE id = ${id}`;
+
+  conn.query(query, function (err, data) {
+    if (err) {
+      console.log(err)
+    }
+
+    const book = data[0];
+
+    res.render('book', { book })
+  })
+})
+
+app.get('/books/edit/:id', function (req, res) {
+  const id = req.params.id;
+
+  const sql = `SELECT * FROM books WHERE id = ${id}`;	
+
+  conn.query(sql, function (err, data) {
+    if (err) {
+      console.log(err)
+    }
+
+    const book = data[0];
+
+    res.render('editbook', { book })
+  })
+
+})
+const conn = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'nodemysql2',
+})
+
+conn.connect(function (err) {
+  if (err) {
+    console.log(err)
+  }
+
+  console.log('Conectado ao MySQL!')
+
+  app.listen(3000)
+})
