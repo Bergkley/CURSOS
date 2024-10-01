49ff8267b1b6cb909f2637683270b6d5fb4066ff
Bergkley <bergkley@gmail.com>
2024-10-01 19:01:57 -0300

    feat: edit

---
+++ Udemy- NODE.JS, EXPRESS, MONGODB,MYSQL,REACT.JS,MVC/SEÇÃO 9 - SEQUELIZE/7 - Edit/db/conn.js
@@ -0,0 +1,15 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('nodesquelize2', 'root', '', {
+    host: 'localhost',
+    dialect: 'mysql'
+});
+
+// try {
+//     sequelize.authenticate();
+//     console.log('Connection com sucesso com sequelize!');
+// } catch (error) {
+//     console.log('Falha ao conectar: ' + error);
+// }
+
+module.exports = sequelize;
