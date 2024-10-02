b4bfbb0a722100a798486388d09dcb736a33c336
Bergkley <bergkley@gmail.com>
2024-10-02 17:05:25 -0300

    feat: edit_task

---
+++ Udemy- NODE.JS, EXPRESS, MONGODB,MYSQL,REACT.JS,MVC/SEÇÃO 10 - MVC/9 - edit_task/controllers/TaskController.js
@@ -0,0 +1,43 @@
+const Task = require('../models/Task')
+
+module.exports = class TaskController {
+  static createTask(req, res) {
+    res.render('tasks/create')
+  }
+
+  static createTaskSave(req, res) {
+    const task = {
+      title: req.body.title,
+      description: req.body.description,
+      done: false,
+    }
+
+    Task.create(task)
+      .then(res.redirect('/tasks'))
+      .catch((err) => console.log())
+  }
+
+  static  async updateTask(req, res) {
+    const id = req.params.id;
+
+    const task = await Task.findOne({ where: { id: id },raw: true})
+    
+    res.render('tasks/edit', { task })
+  }
+
+  static showTasks(req, res) {
+    Task.findAll({ raw: true })
+      .then((data) => {
+        res.render('tasks/all', { tasks: data })
+      })
+      .catch((err) => console.log(err))
+  }
+
+  static removeTask(req, res) {
+    const id = req.body.id
+
+    Task.destroy({ where: { id: id } })
+      .then(res.redirect('/tasks'))
+      .catch((err) => console.log())
+  }
+}
