9d2cddba7ec09e68c1d868ada21ffe6b4edfc0c4
Bergkley <bergkley@gmail.com>
2024-10-02 16:59:33 -0300

    feat: deletando dados

---
+++ Udemy- NODE.JS, EXPRESS, MONGODB,MYSQL,REACT.JS,MVC/SEÇÃO 10 - MVC/8  - deletando_dados/controllers/TaskController.js
@@ -0,0 +1,35 @@
+const Task = require('../models/Task')
+
+module.exports = class TaskController {
+  static createTask(req, res) {
+    res.render('tasks/create')
+  }
+
+  static createTaskSave(req, res) {
+    const task = {
+      title: req.body.title,
+      description: req.body.description,
+      done: false,
+    }
+
+    Task.create(task)
+      .then(res.redirect('/tasks'))
+      .catch((err) => console.log())
+  }
+
+  static showTasks(req, res) {
+    Task.findAll({ raw: true })
+      .then((data) => {
+        res.render('tasks/all', { tasks: data })
+      })
+      .catch((err) => console.log(err))
+  }
+
+  static removeTask(req, res) {
+    const id = req.body.id
+
+    Task.destroy({ where: { id: id } })
+      .then(res.redirect('/tasks'))
+      .catch((err) => console.log())
+  }
+}
