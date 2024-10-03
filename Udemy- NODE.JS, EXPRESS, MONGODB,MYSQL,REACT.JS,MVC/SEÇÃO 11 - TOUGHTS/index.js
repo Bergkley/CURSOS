1d0dae5c577664b356bab574326a431063e971ef
Bergkley <bergkley@gmail.com>
2024-10-03 15:36:15 -0300

    feat: criando primeira rota

--- Udemy- NODE.JS, EXPRESS, MONGODB,MYSQL,REACT.JS,MVC/SEÇÃO 11 - TOUGHTS/index.js
+++ Udemy- NODE.JS, EXPRESS, MONGODB,MYSQL,REACT.JS,MVC/SEÇÃO 11 - TOUGHTS/index.js
@@ -12,9 +12,15 @@ const app = express();
 
 const conn = require("./db/conn");
 
+const toughtRouter = require("./routes/toughtRouter");
+
+// Controllers
+const ToughtController = require("./controllers/ToughtController");
+
 // Models
 const Tought = require("./models/Tought");
 const User = require("./models/User");
+
 // template engine
 app.engine('handlebars', exphbs())
 app.set("view engine", "handlebars");
@@ -58,6 +64,10 @@ app.use((req, res, next) => {
     }
     next()
 })
+
+// Routes
+app.use("/toughts", toughtRouter);
+app.use("/", ToughtController.showToughts);
 
 conn
   .sync()
