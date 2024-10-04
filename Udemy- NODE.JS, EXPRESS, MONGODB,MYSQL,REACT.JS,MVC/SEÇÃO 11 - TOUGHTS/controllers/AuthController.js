231daf9ff180840fee7ba83dea56bff8fa5d09d1
Bergkley <bergkley@gmail.com>
2024-10-04 13:42:59 -0300

    feat: logout

--- Udemy- NODE.JS, EXPRESS, MONGODB,MYSQL,REACT.JS,MVC/SEÇÃO 11 - TOUGHTS/controllers/AuthController.js
+++ Udemy- NODE.JS, EXPRESS, MONGODB,MYSQL,REACT.JS,MVC/SEÇÃO 11 - TOUGHTS/controllers/AuthController.js
@@ -55,4 +55,9 @@ module.exports = class AuthController {
       })
       .catch((err) => console.log(err))
   }
+
+  static logout(req, res) {
+    req.session.destroy()
+    res.redirect('/login')
+  }
 };
