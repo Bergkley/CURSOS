56bc55b7e7df3a878fc43b5815bb5ccb0d468c1a
Bergkley <bergkley@gmail.com>
2024-10-04 18:46:58 -0300

    feat: busca pelos pensamentos

--- Udemy- NODE.JS, EXPRESS, MONGODB,MYSQL,REACT.JS,MVC/SEÇÃO 11 - TOUGHTS/controllers/ToughtController.js
+++ Udemy- NODE.JS, EXPRESS, MONGODB,MYSQL,REACT.JS,MVC/SEÇÃO 11 - TOUGHTS/controllers/ToughtController.js
@@ -47,10 +47,24 @@ module.exports = class ToughController {
   }
 
   static async showToughts(req, res) {
-    const toughtsData = await Tought.findAll({include: User});
+     let search = '';
+
+    if(req.query.search) {
+      search = req.query.search;
+    }
+
+
+
+    const toughtsData = await Tought.findAll({include: User, where: {title: {[Op.like]: `%${search}%`}}});
     const toughts = toughtsData.map((result) => result.get({ plain: true }));
 
-    res.render("toughts/home", { toughts });
+    let toughtsQty = toughts.length;
+
+    if(toughtsQty === 0) {
+      toughtsQty = false;
+    }
+
+    res.render("toughts/home", { toughts,search,toughtsQty });
   }
 
   static removeTought(req, res) {
