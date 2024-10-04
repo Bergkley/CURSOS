91f0b3be8b42799dcd52ee35d354122b53a42204
Bergkley <bergkley@gmail.com>
2024-10-04 14:06:42 -0300

    feat: Middleware

--- Udemy- NODE.JS, EXPRESS, MONGODB,MYSQL,REACT.JS,MVC/SEÇÃO 11 - TOUGHTS/controllers/ToughtController.js
+++ Udemy- NODE.JS, EXPRESS, MONGODB,MYSQL,REACT.JS,MVC/SEÇÃO 11 - TOUGHTS/controllers/ToughtController.js
@@ -5,4 +5,7 @@ module.exports = class ToughtController {
     static async showToughts(req, res) {
         res.render('toughts/home')
     }
+    static async dashboard(req, res) {
+        res.render('toughts/dashboard')
+    }
 }
--- Udemy- NODE.JS, EXPRESS, MONGODB,MYSQL,REACT.JS,MVC/SEÇÃO 11 - TOUGHTS/routes/toughtRouter.js
+++ Udemy- NODE.JS, EXPRESS, MONGODB,MYSQL,REACT.JS,MVC/SEÇÃO 11 - TOUGHTS/routes/toughtRouter.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
+// helpers
+const checkAuth = require("../helpers/auth").checkAuth;
+
 const ToughtController = require("../controllers/ToughtController");
 
 
 router.get("/", ToughtController.showToughts)
+router.get("/dashboard",checkAuth, ToughtController.dashboard)
 
 module.exports = router
