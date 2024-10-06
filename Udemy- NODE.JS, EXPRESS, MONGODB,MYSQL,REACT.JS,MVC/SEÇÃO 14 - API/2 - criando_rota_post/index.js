e7118a8d2f2c5ffb8974eddb14cf176fdf5fa7bf
Bergkley <bergkley@gmail.com>
2024-10-06 18:39:12 -0300

    feat: criando post

--- Udemy- NODE.JS, EXPRESS, MONGODB,MYSQL,REACT.JS,MVC/SEÇÃO 14 - API/2 - criando_rota_post/index.js
+++ Udemy- NODE.JS, EXPRESS, MONGODB,MYSQL,REACT.JS,MVC/SEÇÃO 14 - API/2 - criando_rota_post/index.js
@@ -9,5 +9,11 @@ app.get('/',(req, res) => {
     res.json({message: 'Hello World'})
 })
 
+app.post('/createproduct',(req, res) => {
+    const name = req.body.name;
+    const price = req.body.price;
 
+    res.json({message: 'Product created', name, price})
+
+})
 app.listen(3000);
