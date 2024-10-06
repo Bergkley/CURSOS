b89281f68b28003dac7cd38e593b8188824df308
Bergkley <bergkley@gmail.com>
2024-10-06 18:34:22 -0300

    feat: criando post

--- Udemy- NODE.JS, EXPRESS, MONGODB,MYSQL,REACT.JS,MVC/SEÇÃO 14 - API/1 - primeira_api/index.js
+++ Udemy- NODE.JS, EXPRESS, MONGODB,MYSQL,REACT.JS,MVC/SEÇÃO 14 - API/1 - primeira_api/index.js
@@ -8,4 +8,12 @@ app.use(express.json());
 app.get('/',(req, res) => {
     res.json({message: 'Hello World'})
 })
+
+app.post('/createproduct',(req, res) => {
+    const name = req.body.name;
+    const price = req.body.price;
+
+    res.json({message: 'Product created', name, price})
+
+})
 app.listen(3000);
