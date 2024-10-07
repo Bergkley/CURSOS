895f0555d0081d9cc162f2695e7e160596b5a12c
Bergkley <bergkley@gmail.com>
2024-10-07 19:46:53 -0300

    feat: refatoração

--- Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 44 Quiz React/quiz-react/src/App.jsx
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 44 Quiz React/quiz-react/src/App.jsx
@@ -1,18 +1,17 @@
-import Welcome from './components/Welcome'
+import Welcome from "./components/Welcome";
 
-import './App.css'
-import { useContext, useEffect } from 'react';
-import Question from './components/Question';
-import { QuizContext } from './context/quiz';
-import GamerOver from './components/GamerOver';
-
+import "./App.css";
+import { useContext, useEffect } from "react";
+import Question from "./components/Question";
+import { QuizContext } from "./context/quiz";
+import GamerOver from "./components/GamerOver";
 
 function App() {
   const [quizState, dispatch] = useContext(QuizContext);
 
   useEffect(() => {
-    dispatch({ type: "REORDER_QUESTIONS" })
-  }, [])
+    dispatch({ type: "REORDER_QUESTIONS" });
+  }, []);
 
   return (
     <>
@@ -21,10 +20,9 @@ function App() {
         {quizState.gameStage === "Start" && <Welcome />}
         {quizState.gameStage === "Playing" && <Question />}
         {quizState.gameStage === "End" && <GamerOver />}
-        
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
