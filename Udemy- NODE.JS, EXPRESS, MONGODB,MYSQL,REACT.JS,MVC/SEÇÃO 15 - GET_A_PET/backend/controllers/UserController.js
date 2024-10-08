5f972883a04b4edf32ef8368872344a67ee013a5
Bergkley <bergkley@gmail.com>
2024-10-08 09:28:47 -0300

    feat:validação

--- Udemy- NODE.JS, EXPRESS, MONGODB,MYSQL,REACT.JS,MVC/SEÇÃO 15 - GET_A_PET/backend/controllers/UserController.js
+++ Udemy- NODE.JS, EXPRESS, MONGODB,MYSQL,REACT.JS,MVC/SEÇÃO 15 - GET_A_PET/backend/controllers/UserController.js
@@ -1,7 +1,42 @@
-const User = require('../models/User');
+const User = require("../models/User");
 
 module.exports = class UserController {
-    static async register(req, res) {
-        res.json('Olá get a pet')
+  static async register(req, res) {
+    const { name, email, password, phone, confirmPassword } = req.body;
+
+    // validations
+    if (!name) {
+      res.status(422).json({ message: "O nome é obrigatório!" });
+      return;
     }
-}
+
+    if (!email) {
+      res.status(422).json({ message: "O e-mail é obrigatório!" });
+      return;
+    }
+
+    if (!phone) {
+      res.status(422).json({ message: "O telefone é obrigatório!" });
+      return;
+    }
+
+    if (!password) {
+      res.status(422).json({ message: "A senha é obrigatória!" });
+      return;
+    }
+
+    if (!confirmpassword) {
+      res
+        .status(422)
+        .json({ message: "A confirmação de senha é obrigatória!" });
+      return;
+    }
+
+    if (password != confirmpassword) {
+      res
+        .status(422)
+        .json({ message: "A senha e a confirmação precisam ser iguais!" });
+      return;
+    }
+  }
+};
