d1273e3fc0d4ee5c86d757539b5501f302de85bc
Bergkley <bergkley@gmail.com>
2024-10-08 23:19:03 -0300

    feat: criando pet

--- Udemy- NODE.JS, EXPRESS, MONGODB,MYSQL,REACT.JS,MVC/SEÇÃO 15 - GET_A_PET/backend/controllers/PetController.js
+++ Udemy- NODE.JS, EXPRESS, MONGODB,MYSQL,REACT.JS,MVC/SEÇÃO 15 - GET_A_PET/backend/controllers/PetController.js
@@ -1,7 +1,63 @@
+const getToken = require("../helpers/get-token");
+const getUserByToken = require("../helpers/get-user-by-token");
 const Pet = require("../models/Pet");
 
 module.exports = class PetController {
     static async create(req, res) {
-       res.json({ message: "Pet criado com sucesso" });
+        const { name, age, description,weight, color  } = req.body;
+
+        const available = true;
+        // images upload
+
+        // validations
+    if (!name) {
+        res.status(422).json({ message: 'O nome é obrigatório!' })
+        return
+      }
+  
+      if (!age) {
+        res.status(422).json({ message: 'A idade é obrigatória!' })
+        return
+      }
+  
+      if (!weight) {
+        res.status(422).json({ message: 'O peso é obrigatório!' })
+        return
+      }
+  
+      if (!color) {
+        res.status(422).json({ message: 'A cor é obrigatória!' })
+        return
+      }
+
+    //   get pet owner
+    const token = getToken(req);
+    const user = await getUserByToken(token);
+
+    // create a Pet
+
+    const pet = new Pet({
+        name,
+        age,
+        description,
+        weight,
+        color,
+        available,
+        images: [],
+        user: {
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            image: user.image,
+            phone: user.phone
+        }
+    })
+    try {
+        const newPet = await pet.save();
+        res.status(201).json({ message: "Pet criado com sucesso!", pet: newPet })
+        
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
     }
 };
--- Udemy- NODE.JS, EXPRESS, MONGODB,MYSQL,REACT.JS,MVC/SEÇÃO 15 - GET_A_PET/backend/routes/PetRouter.js
+++ Udemy- NODE.JS, EXPRESS, MONGODB,MYSQL,REACT.JS,MVC/SEÇÃO 15 - GET_A_PET/backend/routes/PetRouter.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 const PetController = require('../controllers/PetController');
 
-router.post('/create', PetController.create);
+// middleware
+const verifyToken = require('../helpers/verify-token');
+
+router.post('/create', verifyToken,PetController.create);
 
 module.exports = router
