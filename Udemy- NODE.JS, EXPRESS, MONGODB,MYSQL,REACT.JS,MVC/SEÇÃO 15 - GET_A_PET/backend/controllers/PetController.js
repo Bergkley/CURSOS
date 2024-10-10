7c7287691769916a526d806149d74c94a43401e7
Bergkley <bergkley@gmail.com>
2024-10-10 18:11:06 -0300

    feat: pegar pet pelo id

--- Udemy- NODE.JS, EXPRESS, MONGODB,MYSQL,REACT.JS,MVC/SEÇÃO 15 - GET_A_PET/backend/controllers/PetController.js
+++ Udemy- NODE.JS, EXPRESS, MONGODB,MYSQL,REACT.JS,MVC/SEÇÃO 15 - GET_A_PET/backend/controllers/PetController.js
@@ -2,6 +2,7 @@ const getToken = require("../helpers/get-token");
 const getUserByToken = require("../helpers/get-user-by-token");
 const Pet = require("../models/Pet");
 const {uploadImageToFirebase} = require('../helpers/image.upload')
+const ObjectId = require('mongoose').Types.ObjectId
 
 
 module.exports = class PetController {
@@ -97,5 +98,24 @@ module.exports = class PetController {
     const user = await getUserByToken(token);
     const pets = await Pet.find({ 'adopter._id': user._id }).sort('-createdAt')
     res.status(200).json({ message: "Pets encontrados com sucesso!", pets });
+  }
+
+  static async getPetById(req, res) {
+    const id = req.params.id;
+
+    if(!ObjectId.isValid(id)) {
+      res.status(422).json({ message: "ID inválido" });
+      return;
+    }
+
+    const pet = await Pet.findOne({_id: id});
+
+    if(!pet){
+      res.status(404).json({ message: "Pet não encontrado" });
+      return;
+    }
+
+    res.status(200).json({ message: "Pet encontrado com sucesso!", pet });
+
   }
 };
