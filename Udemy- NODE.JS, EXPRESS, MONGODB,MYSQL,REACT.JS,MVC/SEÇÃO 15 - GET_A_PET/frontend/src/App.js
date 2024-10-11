fe808c2c096c1b1980f8900d1973ce14e9332592
Bergkley <bergkley@gmail.com>
2024-10-11 18:29:40 -0300

    feat: criando routes

--- Udemy- NODE.JS, EXPRESS, MONGODB,MYSQL,REACT.JS,MVC/SEÇÃO 15 - GET_A_PET/frontend/src/App.js
+++ Udemy- NODE.JS, EXPRESS, MONGODB,MYSQL,REACT.JS,MVC/SEÇÃO 15 - GET_A_PET/frontend/src/App.js
@@ -1,9 +1,25 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+// pages
+import Login from "../src/components/pages/Auth/Login";
+import Register from "../src/components/pages/Auth/Register";
+import Home from "../src/components/pages/Home";
 
 function App() {
   return (
-    <div className="App">
-     <h1>Get A Pet</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
