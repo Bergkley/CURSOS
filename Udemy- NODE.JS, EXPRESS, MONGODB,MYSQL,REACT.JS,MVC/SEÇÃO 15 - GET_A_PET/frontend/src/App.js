49644879c7382c72b8eef6928340a9d57cebec45
Bergkley <bergkley@gmail.com>
2024-10-11 18:46:02 -0300

    footer && Navbar

--- Udemy- NODE.JS, EXPRESS, MONGODB,MYSQL,REACT.JS,MVC/SEÇÃO 15 - GET_A_PET/frontend/src/App.js
+++ Udemy- NODE.JS, EXPRESS, MONGODB,MYSQL,REACT.JS,MVC/SEÇÃO 15 - GET_A_PET/frontend/src/App.js
@@ -1,5 +1,9 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+// components
+import Navbar from "../src/components/layout/Navbar";
+import Footer from "../src/components/layout/Footer";
+
 // pages
 import Login from "../src/components/pages/Auth/Login";
 import Register from "../src/components/pages/Auth/Register";
@@ -8,6 +12,7 @@ import Home from "../src/components/pages/Home";
 function App() {
   return (
     <Router>
+      <Navbar />
       <Switch>
         <Route path="/login">
           <Login />
@@ -19,6 +24,7 @@ function App() {
           <Home />
         </Route>
       </Switch>
+      <Footer />
     </Router>
   );
 }
---
+++ Udemy- NODE.JS, EXPRESS, MONGODB,MYSQL,REACT.JS,MVC/SEÇÃO 15 - GET_A_PET/frontend/src/components/layout/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from "./Footer.module.css";
+
+
+const Footer = () => {
+  return (
+   <footer className={styles.footer}>
+    <span className='bold'>Get A Pet</span> &copy; 2024
+   </footer>
+  )
+}
+
+export default Footer
