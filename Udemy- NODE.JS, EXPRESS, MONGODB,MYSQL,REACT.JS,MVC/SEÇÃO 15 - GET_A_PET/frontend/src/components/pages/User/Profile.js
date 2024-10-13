4bff25da0c69fc26d1864c3b7e1da58b2d001351
Bergkley <bergkley@gmail.com>
2024-10-12 21:05:03 -0300

    feat: atualização user

--- Udemy- NODE.JS, EXPRESS, MONGODB,MYSQL,REACT.JS,MVC/SEÇÃO 15 - GET_A_PET/frontend/src/components/pages/User/Profile.js
+++ Udemy- NODE.JS, EXPRESS, MONGODB,MYSQL,REACT.JS,MVC/SEÇÃO 15 - GET_A_PET/frontend/src/components/pages/User/Profile.js
@@ -4,10 +4,15 @@ import styles from './Profile.module.css'
 
 import Input from "./../../form/input";
 import api from "../../../utils/api";
+import useFlashMessage from './../../../hooks/userFlashMessage';
+
 
 const Profile = () => {
     const [user, setUser] = useState({});
     const [token] = useState(localStorage.getItem("token") || "");
+    const {setFlashMessage} = useFlashMessage();
+    const [preview, setPreview] = useState();
+
 
     useEffect(() => {
     api
@@ -23,15 +28,49 @@ const Profile = () => {
 
     console.log('user', user)
 
-  const onFileChange = (e) => {};
-  const handleChange = (e) => {};
+  const onFileChange = (e) => {
+    setPreview(e.target.files[0]);
+    setUser({ ...user, [e.target.name]: e.target.files[0] });
+  };
+  const handleChange = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    
+    let msgType = 'success';
+
+    const formData = new FormData();
+
+    await Object.keys(user).forEach((key) => {
+      formData.append(key, user[key]);
+    });
+
+    const data = await api.patch(`/users/edit/${user._id}`, formData, {
+      headers: {
+        Authorization: `Bearer ${JSON.parse(token)}`,
+        'Content-Type': 'multipart/form-data',
+      },
+    }).then((response) => {
+      return response.data
+    }).catch((err) => {
+      msgType = 'error'
+      return err.response.data
+    })
+
+    setFlashMessage(data.message, msgType)
+
+  }
   return (
     <section>
      <div className={styles.profile_header}>
         <h1>Perfil</h1>
-        <p>Preview Imagem</p>
+        {(user.image || preview) && (
+          <img src={preview ? URL.createObjectURL(preview) : user.image} alt={user.name} />
+        )}
      </div>
-      <form className={formStyles.form_container}>
+      <form className={formStyles.form_container} onSubmit={handleSubmit}>
       <Input
           text="Imagem"
           type="file"
