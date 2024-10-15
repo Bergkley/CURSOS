bd3fa20005e492aedc35d6f15bcb1ebbbf51c0df
Bergkley <bergkley@gmail.com>
2024-10-14 22:41:21 -0300

    feat: refatoração

--- Udemy- NODE.JS, EXPRESS, MONGODB,MYSQL,REACT.JS,MVC/SEÇÃO 15 - GET_A_PET/backend/helpers/create-user-token.js
+++ Udemy- NODE.JS, EXPRESS, MONGODB,MYSQL,REACT.JS,MVC/SEÇÃO 15 - GET_A_PET/backend/helpers/create-user-token.js
@@ -4,6 +4,10 @@ const createUserToken = (user, req, res) => {
   const token = jew.sign({ name: user.name, id: user.id }, "nossosecret");
   res
     .status(200)
-    .json({ message: "Você está autenticado.", token: token, userId: user._id });
+    .json({
+      message: "Você está autenticado.",
+      token: token,
+      userId: user._id,
+    });
 };
 module.exports = createUserToken;
