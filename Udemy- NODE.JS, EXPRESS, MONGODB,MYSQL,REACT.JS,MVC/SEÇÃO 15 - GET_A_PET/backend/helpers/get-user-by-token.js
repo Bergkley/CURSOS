3227ad8d89df329070bae34b6dab58e9a494f520
Bergkley <bergkley@gmail.com>
2024-10-14 22:41:44 -0300

    feat: refatoração

--- Udemy- NODE.JS, EXPRESS, MONGODB,MYSQL,REACT.JS,MVC/SEÇÃO 15 - GET_A_PET/backend/helpers/get-user-by-token.js
+++ Udemy- NODE.JS, EXPRESS, MONGODB,MYSQL,REACT.JS,MVC/SEÇÃO 15 - GET_A_PET/backend/helpers/get-user-by-token.js
@@ -1,21 +1,18 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
-const User = require('../models/User');
-
+const User = require("../models/User");
 
 const getUserByToken = async (token) => {
+  if (!token) {
+    return res.status(401).json({ message: "Acesso Negado" });
+  }
+  const decoded = jwt.verify(token, "nossosecret");
 
-    if(!token) {
-        return res.status(401).json({ message: "Acesso Negado" });
-    }
-    const decoded = jwt.verify(token, "nossosecret");
+  const userId = decoded.id;
 
-    const userId = decoded.id;
+  const user = await User.findOne({ _id: userId });
 
-    const user = await User.findOne({_id: userId});
-
-    return user;
-
-}
+  return user;
+};
 
 module.exports = getUserByToken;
