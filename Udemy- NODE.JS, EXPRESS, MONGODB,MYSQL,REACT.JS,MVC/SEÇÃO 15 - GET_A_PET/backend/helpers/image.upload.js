d99f5274e3f7cf028b83f78b64df6e8eedba01a2
Bergkley <bergkley@gmail.com>
2024-10-14 22:41:54 -0300

    feat: refatoração

--- Udemy- NODE.JS, EXPRESS, MONGODB,MYSQL,REACT.JS,MVC/SEÇÃO 15 - GET_A_PET/backend/helpers/image.upload.js
+++ Udemy- NODE.JS, EXPRESS, MONGODB,MYSQL,REACT.JS,MVC/SEÇÃO 15 - GET_A_PET/backend/helpers/image.upload.js
@@ -2,40 +2,41 @@ const multer = require("multer");
 const path = require("path");
 const firebaseAdmin = require("firebase-admin");
 const { getStorage } = require("firebase-admin/storage");
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 
 dotenv.config();
-
 
 const firebaseConfig = JSON.parse(`${process.env.FIREBASE_CONFIG}`);
 
 firebaseAdmin.initializeApp({
   credential: firebaseAdmin.credential.cert(firebaseConfig),
-  storageBucket: `${process.env.FIREBASE_STORAGE_BUCKET}` 
+  storageBucket: `${process.env.FIREBASE_STORAGE_BUCKET}`,
 });
 
 const bucket = getStorage().bucket();
 
 const imageUpload = multer({
-  storage: multer.memoryStorage(), 
+  storage: multer.memoryStorage(),
   fileFilter(req, file, cb) {
     if (!file.originalname.match(/\.(png|jpg)$/)) {
       return cb(new Error("Por favor, envie apenas png ou jpg!"));
     }
     cb(undefined, true);
-  }
+  },
 });
 
 const uploadImageToFirebase = async (req, file) => {
-  let folder = '';
-  if (req.baseUrl.includes('users')) {
+  let folder = "";
+  if (req.baseUrl.includes("users")) {
     folder = "Users";
-  } else if (req.baseUrl.includes('pets')) {
+  } else if (req.baseUrl.includes("pets")) {
     folder = "Pets";
   }
-  const filename = `${folder}/${Date.now()}-${Math.floor(Math.random() * 10000)}${path.extname(file.originalname)}`;
+  const filename = `${folder}/${Date.now()}-${Math.floor(
+    Math.random() * 10000
+  )}${path.extname(file.originalname)}`;
 
-  console.log('filename', filename)
+  console.log("filename", filename);
   const blob = bucket.file(filename);
 
   const blobStream = blob.createWriteStream({
@@ -48,11 +49,12 @@ const uploadImageToFirebase = async (req, file) => {
 
   return new Promise((resolve, reject) => {
     blobStream.on("finish", () => {
-      const publicUrl = `https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/${encodeURIComponent(blob.name)}?alt=media`;
-      resolve(publicUrl); 
-      console.log('publicUrl', publicUrl)
+      const publicUrl = `https://firebasestorage.googleapis.com/v0/b/${
+        bucket.name
+      }/o/${encodeURIComponent(blob.name)}?alt=media`;
+      resolve(publicUrl);
+      console.log("publicUrl", publicUrl);
     });
-
 
     blobStream.on("error", (err) => {
       reject(err);
