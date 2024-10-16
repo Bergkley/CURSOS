4d245bc9d1bbbefadaa15fba6aded8b7cfe89ce1
Bergkley <bergkley@gmail.com>
2024-10-15 23:07:25 -0300

    feat: refatoração

--- Udemy- NODE.JS, EXPRESS, MONGODB,MYSQL,REACT.JS,MVC/SEÇÃO 15 - GET_A_PET/frontend/src/components/layout/Container.js
+++ Udemy- NODE.JS, EXPRESS, MONGODB,MYSQL,REACT.JS,MVC/SEÇÃO 15 - GET_A_PET/frontend/src/components/layout/Container.js
@@ -1,9 +1,7 @@
 import styles from "./Container.module.css";
 
-function Container({children}) {
-  return <main className={styles.container}>
-      {children}
-  </main>
+function Container({ children }) {
+  return <main className={styles.container}>{children}</main>;
 }
 
-export default Container
+export default Container;
