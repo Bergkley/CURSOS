f541293ed078c019a0487c973ce32467b437a616
Bergkley <bergkley@gmail.com>
2024-10-15 23:07:55 -0300

    feat: refatoração

--- Udemy- NODE.JS, EXPRESS, MONGODB,MYSQL,REACT.JS,MVC/SEÇÃO 15 - GET_A_PET/frontend/src/components/layout/Message.js
+++ Udemy- NODE.JS, EXPRESS, MONGODB,MYSQL,REACT.JS,MVC/SEÇÃO 15 - GET_A_PET/frontend/src/components/layout/Message.js
@@ -1,32 +1,28 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import styles from "./Message.module.css";
 import bus from "../../utils/bus";
 
-
 const Message = () => {
-    const [visibility, setVisibility] = useState(false)
-    const [type,setType] = useState("")
-    const [message,setMessage] = useState("")
+  const [visibility, setVisibility] = useState(false);
+  const [type, setType] = useState("");
+  const [message, setMessage] = useState("");
 
-    useEffect(() => {
+  useEffect(() => {
+    bus.addListener("flash", ({ message, type }) => {
+      setVisibility(true);
+      setType(type);
+      setMessage(message);
+      setTimeout(() => {
+        setVisibility(false);
+      }, 3000);
+    });
+  }, []);
 
-        bus.addListener('flash', ({message, type}) => {
-            setVisibility(true)
-            setType(type)
-            setMessage(message)
-            setTimeout(() => {
-                setVisibility(false)
-            }, 3000)
-        })
-        
-    },[])
-
-     
   return (
-   visibility && (
-    <div className={`${styles.message} ${styles[type]}`}>{message}</div>
-   )
-  )
-}
+    visibility && (
+      <div className={`${styles.message} ${styles[type]}`}>{message}</div>
+    )
+  );
+};
 
-export default Message
+export default Message;
