1439759547dc77633bc8f68d18cc26cb1578f49c
Bergkley <bergkley@gmail.com>
2024-10-17 23:34:02 -0300

    feat:refatoração

--- Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 50 Projeto Memories/frontend/frontend/src/axios-config.js
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 50 Projeto Memories/frontend/frontend/src/axios-config.js
@@ -1,10 +1,9 @@
-import axios from 'axios';
+import axios from "axios";
 
 axios.defaults.baseURL = "http://localhost:3000/";
 
-axios.defaults.headers.post['Content-Type'] = 'application/json';
+axios.defaults.headers.post["Content-Type"] = "application/json";
 
 axios.defaults.timeout = 10000;
 
 export default axios;
-
