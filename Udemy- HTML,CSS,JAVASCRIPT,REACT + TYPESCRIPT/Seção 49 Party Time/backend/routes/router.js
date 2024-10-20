9793c403f69e221173cc3db6c914132967a2af79
Bergkley <bergkley@gmail.com>
2024-10-19 21:35:15 -0300

    feat: refatoração

--- Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 49 Party Time/backend/routes/router.js
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 49 Party Time/backend/routes/router.js
@@ -3,11 +3,11 @@ const router = require("express").Router();
 // Services router
 const servicesRouter = require("./services");
 
-router.use("/",servicesRouter);
+router.use("/", servicesRouter);
 
 // Parties router
 const partiesRouter = require("./parties");
 
-router.use("/",partiesRouter);
+router.use("/", partiesRouter);
 
 module.exports = router;
