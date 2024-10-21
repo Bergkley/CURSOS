e5866ac53ad076861e35734995069af8919e79a2
Bergkley <bergkley@gmail.com>
2024-10-20 22:18:16 -0300

    feat: refatoração

--- Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 50 Projeto Memories/backend/db/conn.js
+++ Udemy- HTML,CSS,JAVASCRIPT,REACT + TYPESCRIPT/Seção 50 Projeto Memories/backend/db/conn.js
@@ -5,11 +5,13 @@ require("dotenv").config();
 mongoose.set("strictQuery", true);
 
 async function main() {
-    const conn = await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@memories.onm5g.mongodb.net/?retryWrites=true&w=majority&appName=Memories`);
+  const conn = await mongoose.connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@memories.onm5g.mongodb.net/?retryWrites=true&w=majority&appName=Memories`
+  );
 
-    console.log(`Banco de Dados conectado com sucesso!`);
+  console.log(`Banco de Dados conectado com sucesso!`);
 }
 
-main().catch(err => console.log(err));
+main().catch((err) => console.log(err));
 
 module.exports = main;
