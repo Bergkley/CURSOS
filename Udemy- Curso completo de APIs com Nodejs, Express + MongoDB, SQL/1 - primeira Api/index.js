261815a154318837bda038727ad201eaa0938c71
Bergkley <bergkley@gmail.com>
2024-10-22 21:51:17 -0300

    feat: crud

--- Udemy- Curso completo de APIs com Nodejs, Express + MongoDB, SQL/1 - primeira Api/index.js
+++ Udemy- Curso completo de APIs com Nodejs, Express + MongoDB, SQL/1 - primeira Api/index.js
@@ -2,12 +2,46 @@ const express = require('express');
 
 const server = express();
 
+server.use(express.json());
+
 const cursos = ['NodeJS', 'ReactJS', 'React Native'];
 
 server.get('/curso', (req,res)=>{
    const nome = req.query.nome;
 
    return res.json({curso: `Aprendendo ${nome}`})
+})
+
+server.get('/cursos', (req,res)=>{
+   return res.json(cursos)
+})
+
+server.post('/cursos', (req,res)=>{
+   const {name} = req.body;
+
+   cursos.push(name)
+
+   return res.json({cursos})
+
+})
+
+server.put('/cursos/:index', (req,res)=>{
+  const {index} = req.params;
+  const {name} = req.body;
+
+  cursos[index] = name;
+
+  return res.json({cursos})
+
+})
+
+server.delete('/cursos/:index', (req,res)=>{
+  const {index} = req.params;
+
+  cursos.splice(index, 1);
+
+  return res.json({cursos})
+
 })
 
 server.get('/curso/:id', (req,res)=>{
