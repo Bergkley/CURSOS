45c65a2a5b87b0090dac27de5b64fc35cd0c5c81
Bergkley <bergkley@gmail.com>
2024-10-22 21:41:17 -0300

    feat: query e params

--- Udemy- Curso completo de APIs com Nodejs, Express + MongoDB, SQL/1 - primeira Api/index.js
+++ Udemy- Curso completo de APIs com Nodejs, Express + MongoDB, SQL/1 - primeira Api/index.js
@@ -2,15 +2,26 @@ const express = require('express');
 
 const server = express();
 
+const cursos = ['NodeJS', 'ReactJS', 'React Native'];
+
 server.get('/curso', (req,res)=>{
    const nome = req.query.nome;
 
    return res.json({curso: `Aprendendo ${nome}`})
 })
+
 server.get('/curso/:id', (req,res)=>{
    const id = req.params.id;
 
    return res.json({Curso: `${id}`})
 })
+
+server.get('/curso/:index', (req,res)=>{
+   const {index} = req.params;
+   
+
+   return res.json(cursos[index])
+})
+
 
 server.listen(3000, () => console.log('rodando'))
