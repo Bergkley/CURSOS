be0408f3554f765e9afd73e3d8bcd7f5553ea955
Bergkley <bergkley@gmail.com>
2024-10-23 15:40:20 -0300

    feat: configurando o nodemon e sucrase

--- Udemy- Curso completo de APIs com Nodejs, Express + MongoDB, SQL/2 - Projeto DevHouse/src/routes.js
+++ Udemy- Curso completo de APIs com Nodejs, Express + MongoDB, SQL/2 - Projeto DevHouse/src/routes.js
@@ -1,9 +1,9 @@
-const { Router } = require("express");
-
+import { Router } from "express";
 const routes = new Router();
 
 routes.get('/', function (req, res) {
     return res.json({ message: "Hello World" });
 });
 
-module.exports = routes
+
+export default routes;
