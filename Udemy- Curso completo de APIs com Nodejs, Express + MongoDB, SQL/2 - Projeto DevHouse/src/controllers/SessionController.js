00c2388756946d3686da39fde27d1a514cb3d5b2
Bergkley <bergkley@gmail.com>
2024-10-23 20:11:49 -0300

    feat: validação email

--- Udemy- Curso completo de APIs com Nodejs, Express + MongoDB, SQL/2 - Projeto DevHouse/src/controllers/SessionController.js
+++ Udemy- Curso completo de APIs com Nodejs, Express + MongoDB, SQL/2 - Projeto DevHouse/src/controllers/SessionController.js
@@ -1,10 +1,18 @@
 import User from "../models/User";
+import * as Yup from "yup";
 
 class SessionController{
      async store(req, res){
+        const shema = Yup.object().shape({
+            email: Yup.string().email().required(),
+        })
         const { email } = req.body;
         
         let user = await User.findOne({ email });
+
+        if(!(await shema.isValid(req.body))){
+            return res.status(400).json({ error: 'Validation fails' });
+        }
 
         if(!user){
             user = await User.create({ email });
