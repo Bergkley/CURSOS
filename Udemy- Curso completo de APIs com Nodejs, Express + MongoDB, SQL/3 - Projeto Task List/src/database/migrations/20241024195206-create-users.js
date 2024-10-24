491ed96f2a2d3156a86650c2ca942119bb25c3ee
Bergkley <bergkley@gmail.com>
2024-10-24 16:59:40 -0300

    feat: criando a migration de user

---
+++ Udemy- Curso completo de APIs com Nodejs, Express + MongoDB, SQL/3 - Projeto Task List/src/database/migrations/20241024195206-create-users.js
@@ -0,0 +1,40 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    return queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      create_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  async down(queryInterface) {
+    return queryInterface.dropTable('users');
+  },
+};
