c2aef370157e44d188f12cd56c3196f4449db91d
Bergkley <bergkley@gmail.com>
2024-10-25 18:57:18 -0300

    feat: refatoração

--- Udemy- Curso completo de APIs com Nodejs, Express + MongoDB, SQL/2 - Projeto DevHouse/src/controllers/ReserveController.js
+++ Udemy- Curso completo de APIs com Nodejs, Express + MongoDB, SQL/2 - Projeto DevHouse/src/controllers/ReserveController.js
@@ -1,59 +1,58 @@
-import Reserve from "../models/Reserve";
-import User from "../models/User";
-import House from "../models/House";
+import Reserve from '../models/Reserve';
+import User from '../models/User';
+import House from '../models/House';
 
 class ReserveController {
+  async index(req, res) {
+    const { user_id } = req.headers;
+    const reserves = await Reserve.find({ user: user_id }).populate('house');
 
-    async index(req, res) {
-        const { user_id } = req.headers;
-        const reserves = await Reserve.find({ user: user_id }).populate('house');
+    return res.json(reserves);
+  }
 
-        return res.json(reserves);
+  async store(req, res) {
+    const { user_id } = req.headers;
+    const { house_id } = req.params;
+    const { date } = req.body;
+
+    const house = await House.findById(house_id);
+
+    if (!house) {
+      return res.status(400).json({ error: 'House not found' });
     }
 
-    async store(req, res) {
-        const { user_id } = req.headers;
-        const { house_id } = req.params;
-        const { date } = req.body;
-
-        const house = await House.findById(house_id);
-
-        if (!house) {
-            return res.status(400).json({ error: 'House not found' });
-        }
-
-        if(house.status !== "true") {
-            return res.status(400).json({ error: 'House not available' });
-        }
-
-        const user = await User.findById(user_id);
-
-        if(String(user._id) === String(house.user)) {
-            return res.status(401).json({ error: 'User cannot reserve his own house' });
-        }
-
-
-        const reserve = await Reserve.create({
-            user: user_id,
-            house: house_id,
-            date
-        });
-
-        const populatedReserve = await Reserve.findById(reserve._id)
-            .populate('house')
-            .populate('user');
-
-        return res.json(populatedReserve);
+    if (house.status !== 'true') {
+      return res.status(400).json({ error: 'House not available' });
     }
 
-    async destroy(req, res) {
-        const { reserve_id } = req.body;
+    const user = await User.findById(user_id);
 
-        const reserve = await Reserve.findByIdAndDelete({_id: reserve_id});
-
-        return res.json({mensage: "Reserve deleted",}, reserve);
+    if (String(user._id) === String(house.user)) {
+      return res
+        .status(401)
+        .json({ error: 'User cannot reserve his own house' });
     }
+
+    const reserve = await Reserve.create({
+      user: user_id,
+      house: house_id,
+      date,
+    });
+
+    const populatedReserve = await Reserve.findById(reserve._id)
+      .populate('house')
+      .populate('user');
+
+    return res.json(populatedReserve);
+  }
+
+  async destroy(req, res) {
+    const { reserve_id } = req.body;
+
+    const reserve = await Reserve.findByIdAndDelete({ _id: reserve_id });
+
+    return res.json({ mensage: 'Reserve deleted' }, reserve);
+  }
 }
-
 
 export default new ReserveController();
