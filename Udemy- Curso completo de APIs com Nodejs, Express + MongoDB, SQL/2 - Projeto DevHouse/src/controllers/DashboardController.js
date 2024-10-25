effa50f3bb6f2ca0621911baf9cf34643d0c709d
Bergkley <bergkley@gmail.com>
2024-10-25 18:56:50 -0300

    feat: refatoração

--- Udemy- Curso completo de APIs com Nodejs, Express + MongoDB, SQL/2 - Projeto DevHouse/src/controllers/DashboardController.js
+++ Udemy- Curso completo de APIs com Nodejs, Express + MongoDB, SQL/2 - Projeto DevHouse/src/controllers/DashboardController.js
@@ -1,14 +1,13 @@
-import House from "../models/House";
+import House from '../models/House';
 
-class DashboardController{
-     async show(req,res){
+class DashboardController {
+  async show(req, res) {
+    const { user_id } = req.headers;
 
-        const {user_id} = req.headers;
+    const houses = await House.find({ user: user_id });
 
-        const houses = await House.find({user: user_id});
-
-        return res.json(houses)
-    }
+    return res.json(houses);
+  }
 }
 
-export default new DashboardController()
+export default new DashboardController();
