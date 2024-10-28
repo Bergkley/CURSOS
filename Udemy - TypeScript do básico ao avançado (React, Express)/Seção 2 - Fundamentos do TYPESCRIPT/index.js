d3871f9a4dabe37a47e0fada6d7bf9c91396872b
Bergkley <bergkley@gmail.com>
2024-10-28 18:36:16 -0300

    feat: Seção 2 fundamentos Typescripts

---
+++ Udemy - TypeScript do básico ao avançado (React, Express)/Seção 2 - Fundamentos do TYPESCRIPT/index.js
@@ -0,0 +1,14 @@
+"use strict";
+// 1 - number
+let x = 10;
+console.log("Number", x);
+// 2 - string
+let firstName = "Bergkley";
+console.log(firstName.toUpperCase());
+// 3 - boolean
+let isProgrammer = true;
+console.log(isProgrammer);
+console.log(typeof isProgrammer);
+// 4 - inference e annotation
+const ann = "teste";
+let inf = "teste";
---
+++ Udemy - TypeScript do básico ao avançado (React, Express)/Seção 2 - Fundamentos do TYPESCRIPT/index.ts
@@ -0,0 +1,25 @@
+// 1 - number
+
+let x: number = 10;
+
+console.log("Number", x);
+
+// 2 - string
+
+let firstName: string = "Bergkley";
+
+console.log(firstName.toUpperCase())
+
+// 3 - boolean
+
+let isProgrammer: boolean = true;
+
+console.log(isProgrammer);
+console.log(typeof isProgrammer);
+
+// 4 - inference e annotation
+
+const ann: string ="teste";
+
+let inf = "teste";
+
