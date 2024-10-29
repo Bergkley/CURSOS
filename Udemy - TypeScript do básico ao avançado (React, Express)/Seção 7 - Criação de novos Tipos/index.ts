ef41faa1dae8c1b9b30265fe6fb208b8e12e3678
Bergkley <bergkley@gmail.com>
2024-10-29 20:32:01 -0300

    feat: constraint em generic

--- Udemy - TypeScript do básico ao avançado (React, Express)/Seção 7 - Criação de novos Tipos/index.ts
+++ Udemy - TypeScript do básico ao avançado (React, Express)/Seção 7 - Criação de novos Tipos/index.ts
@@ -4,3 +4,11 @@ function showData<T>(arg: T): string {
 }
 
 console.log(showData(5));
+
+// 2 - constraint em generic
+
+function showProductName<T extends { name: string }>(obj: T) {
+  return `O nome do produto e ${obj.name}`;
+}
+
+console.log(showProductName({ name: "Boneco", category: "Brinquedo" }))
