4e93837c7f1d2d3e84197516b6bc697f7e90c74c
Bergkley <bergkley@gmail.com>
2024-10-29 20:34:22 -0300

    feat: generic com interface

--- Udemy - TypeScript do básico ao avançado (React, Express)/Seção 7 - Criação de novos Tipos/index.ts
+++ Udemy - TypeScript do básico ao avançado (React, Express)/Seção 7 - Criação de novos Tipos/index.ts
@@ -12,3 +12,18 @@ function showProductName<T extends { name: string }>(obj: T) {
 }
 
 console.log(showProductName({ name: "Boneco", category: "Brinquedo" }))
+
+// 3 - generic com interface
+interface MyObject<T, U> {
+  name: string;
+  wheels: T;
+  engine:U;
+}
+
+type Car = MyObject<number, number>;
+type Pen = MyObject<boolean, boolean>;
+
+const myCar: Car = { name: "Fusca", wheels: 4, engine: 1.0 };
+const myPen: Pen = { name: "Caneta", wheels: true, engine: false }
+
+console.log(myCar, myPen)
