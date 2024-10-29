1cb111cce71aa3f0a196486436e525de436fb6e8
Bergkley <bergkley@gmail.com>
2024-10-29 20:40:57 -0300

    feat: keyof type operator

--- Udemy - TypeScript do básico ao avançado (React, Express)/Seção 7 - Criação de novos Tipos/index.ts
+++ Udemy - TypeScript do básico ao avançado (React, Express)/Seção 7 - Criação de novos Tipos/index.ts
@@ -27,3 +27,37 @@ const myCar: Car = { name: "Fusca", wheels: 4, engine: 1.0 };
 const myPen: Pen = { name: "Caneta", wheels: true, engine: false }
 
 console.log(myCar, myPen)
+
+//4 - type parameters
+function getSomeKey<T, K extends keyof T>(obj: T, key: K) {
+  return `O valor da chave ${key} é ${obj[key]}`
+}
+
+const server2 = {
+  hd: "2TB",
+  ram: "32GB"
+}
+
+console.log(getSomeKey(server2, "ram")) 
+
+// 5 - keyof type operator
+
+type Character = {
+  name: string;
+  age: number;
+  hasDriveLicense: boolean;
+}
+
+type C = keyof Character;
+
+function showCharName(obj: Character, name: C):string {
+  return `${obj[name]}`
+}
+
+const myChar: Character = {
+  name: "berg",
+  age: 19,
+  hasDriveLicense: true
+}
+
+console.log(showCharName(myChar, "name"))
