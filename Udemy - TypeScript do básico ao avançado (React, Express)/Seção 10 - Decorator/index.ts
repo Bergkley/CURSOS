e1927e2c9ba81891b18dc820de825b6c48b909d1
Bergkley <bergkley@gmail.com>
2024-10-30 14:14:42 -0300

    feat: Decorators

--- Udemy - TypeScript do básico ao avançado (React, Express)/Seção 10 - Decorator/index.ts
+++ Udemy - TypeScript do básico ao avançado (React, Express)/Seção 10 - Decorator/index.ts
@@ -46,3 +46,65 @@ class MultipleDecorators{
 
 const myObj2 = new MultipleDecorators();
 myObj2.testing()
+
+// 3 - class decorator
+
+function classDec(constructor: Function){
+  console.log(constructor)
+  if(constructor.name === "User"){
+    console.log("Criando usuário")
+  }
+}
+
+@classDec
+class User {
+  name
+
+  constructor(name:string){
+    this.name = name
+  }
+}
+
+const berg = new User("Berg")
+console.log(berg)
+
+// 4 - Decorator de método
+
+function enumerable(value: boolean){
+  return function(target: any, propertyKey: string, descriptor: PropertyDescriptor){
+    descriptor.enumerable = value
+  }
+}
+class Machine {
+  name
+
+  constructor(name: string) {
+    this.name = name
+  }
+
+
+  @enumerable(false)
+  showName() {
+    console.log(`The name is: ${this.name}`)
+  }
+}
+
+const trator = new Machine("Trator")
+trator.showName()
+
+// 5 - acessor decorator
+class Monster {
+  name?
+  age?
+
+  constructor(name: string, age: number) {
+    this.name = name
+    this.age = age
+  }
+  @enumerable(true)
+  get showName() { return this.name }
+  @enumerable(true)
+  get showAge() { return this.age }
+}
+
+const charmander = new Monster("Charmander", 10)
