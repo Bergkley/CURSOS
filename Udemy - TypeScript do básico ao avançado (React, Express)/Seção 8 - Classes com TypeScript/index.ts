8c74590258a52d2f170bef009c5933b970c11852
Bergkley <bergkley@gmail.com>
2024-10-30 09:10:51 -0300

    feat: 2 - constructor

--- Udemy - TypeScript do básico ao avançado (React, Express)/Seção 8 - Classes com TypeScript/index.ts
+++ Udemy - TypeScript do básico ao avançado (React, Express)/Seção 8 - Classes com TypeScript/index.ts
@@ -7,3 +7,17 @@ class User {
 
 const berg = new User();
 berg.name = "Berg";
+
+// 2 - constructor
+class NewUser {
+  name
+  age
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+const joao = new NewUser("Joao", 22);
+console.log(joao);
+
