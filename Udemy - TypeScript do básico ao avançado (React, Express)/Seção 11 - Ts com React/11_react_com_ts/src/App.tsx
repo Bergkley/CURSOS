4304328b16dd2c320455174ada46067031b43ec7
Bergkley <bergkley@gmail.com>
2024-10-31 15:46:38 -0300

    feat: criando contexto

--- Udemy - TypeScript do básico ao avançado (React, Express)/Seção 11 - Ts com React/11_react_com_ts/src/App.tsx
+++ Udemy - TypeScript do básico ao avançado (React, Express)/Seção 11 - Ts com React/11_react_com_ts/src/App.tsx
@@ -4,6 +4,15 @@ import FirstComponent from "./components/FirstComponent";
 import SecondComponent from "./components/SecondComponent";
 import Destructuring, {Category} from "./components/Destructuring";
 import State from "./components/State";
+import { createContext } from "react";
+
+interface IAppContext{
+  language: string,
+  framework: string,
+  projects: number
+}
+
+ export const AppContext = createContext<IAppContext | null>(null);
 function App() {
   // 1 - variaveis
   const name: string = "Berg";
@@ -18,28 +27,39 @@ function App() {
   // 8 - type
   type textOrNull = string | null;
 
+  // 9 - context
+
+
+  const contextValue: IAppContext = {
+    language: "TypeScript",
+    framework: "React",
+    projects: 5
+  }
+
   const myText: textOrNull = "Tem algum texto aqui";
   const mySecondText: textOrNull = null;
 
   return (
-    <div className="App">
-      <h1>TypeScript com React</h1>
-      <h2>Ola, {name} tenho {age} anos</h2>
-      {isWorking && <p>Estou trabalhando</p>}
+    <AppContext.Provider value={contextValue}>
+      <div className="App">
+        <h1>TypeScript com React</h1>
+        <h2>Ola, {name} tenho {age} anos</h2>
+        {isWorking && <p>Estou trabalhando</p>}
 
-      <p>{userGreeting(name)}</p>
-      <FirstComponent />
-      <SecondComponent name="Berg" />
-      <Destructuring title="Post 1"
-        content="Algum conteudo"
-        commentsQty={10}
-        tags={["ts", "js"]}
-        category={Category.TS}
-        />
-      <State />
-      {myText && <p>{myText}</p>}
-      {mySecondText && <p>{myText}</p>}
-    </div>
+        <p>{userGreeting(name)}</p>
+        <FirstComponent />
+        <SecondComponent name="Berg" />
+        <Destructuring title="Post 1"
+          content="Algum conteudo"
+          commentsQty={10}
+          tags={["ts", "js"]}
+          category={Category.TS}
+          />
+        <State />
+        {myText && <p>{myText}</p>}
+        {mySecondText && <p>{myText}</p>}
+      </div>
+    </AppContext.Provider>
   );
 }
 
