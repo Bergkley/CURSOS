14f71a795bc1a5fed5a8319248fc7a0f25d4046c
Bergkley <bergkley@gmail.com>
2024-10-31 10:42:03 -0300

    feat: 2 - funcoes

--- Udemy - TypeScript do básico ao avançado (React, Express)/Seção 11 - Ts com React/11_react_com_ts/src/App.tsx
+++ Udemy - TypeScript do básico ao avançado (React, Express)/Seção 11 - Ts com React/11_react_com_ts/src/App.tsx
@@ -5,11 +5,18 @@ function App() {
   const name: string = "Berg";
   const age: number = 19;
   const isWorking: boolean = true;
+
+  // 2 - funcoes
+  const userGreeting = (name: string): string => {
+    return `Ola, ${name}`;
+  }
   return (
     <div className="App">
       <h1>TypeScript com React</h1>
       <h2>Ola, {name} tenho {age} anos</h2>
       {isWorking && <p>Estou trabalhando</p>}
+
+      <p>{userGreeting(name)}</p>
     </div>
   );
 }
