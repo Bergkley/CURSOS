2010e24c5f9fa28fdc2d6d12088919c5c952567d
Bergkley <bergkley@gmail.com>
2024-10-31 10:38:06 -0300

    feat: criando variaveis

--- Udemy - TypeScript do básico ao avançado (React, Express)/Seção 11 - Ts com React/11_react_com_ts/src/App.tsx
+++ Udemy - TypeScript do básico ao avançado (React, Express)/Seção 11 - Ts com React/11_react_com_ts/src/App.tsx
@@ -1,9 +1,15 @@
 
 
 function App() {
+  // 1 - variaveis
+  const name: string = "Berg";
+  const age: number = 19;
+  const isWorking: boolean = true;
   return (
     <div className="App">
       <h1>TypeScript com React</h1>
+      <h2>Ola, {name} tenho {age} anos</h2>
+      {isWorking && <p>Estou trabalhando</p>}
     </div>
   );
 }
