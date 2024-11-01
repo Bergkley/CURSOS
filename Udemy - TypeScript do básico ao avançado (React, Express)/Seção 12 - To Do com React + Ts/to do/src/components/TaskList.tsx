41eae01626ba8d3ec2f03c3faa085834658be843
Bergkley <bergkley@gmail.com>
2024-11-01 15:57:55 -0300

    feat: taskList

--- Udemy - TypeScript do básico ao avançado (React, Express)/Seção 12 - To Do com React + Ts/to do/src/components/TaskList.tsx
+++ Udemy - TypeScript do básico ao avançado (React, Express)/Seção 12 - To Do com React + Ts/to do/src/components/TaskList.tsx
@@ -1,7 +1,43 @@
-type Props = {};
+import { ITask } from "../interfaces/Task";
+import styles from "./TaskList.module.css";
 
-const TaskList = (props: Props) => {
-  return <div>Lista de tarefa</div>;
+
+type Props = {
+  taskList: ITask[];
+};
+
+const handleEdit = () => {
+  
+}
+
+const handleDelete = () => {
+  
+}
+
+const TaskList = ({ taskList }: Props) => {
+  return (
+    <>
+      {taskList.length > 0 ? (
+        taskList.map((task, index) => (
+          <div key={index} className={styles.task}>
+            <div className={styles.details}>
+              <h4>{task.title}</h4>
+              <p>Dificuldade: {task.difficulty}</p>
+            </div>
+            <div className={styles.actions}>
+              <i className="bi bi-pencil" onClick={() => handleEdit()}></i>
+              <i
+                className="bi bi-trash"
+                onClick={() => handleDelete()}
+              ></i>
+            </div>
+          </div>
+        ))
+      ) : (
+        <p>Não há tarefas cadastradas</p>
+      )}
+    </>
+  );
 };
 
 export default TaskList;
