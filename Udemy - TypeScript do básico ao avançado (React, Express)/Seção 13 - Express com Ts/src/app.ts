21772954aba6556bd75f49141045618edadef691
Bergkley <bergkley@gmail.com>
2024-11-01 21:13:58 -0300

    feat: post

--- Udemy - TypeScript do básico ao avançado (React, Express)/Seção 13 - Express com Ts/src/app.ts
+++ Udemy - TypeScript do básico ao avançado (React, Express)/Seção 13 - Express com Ts/src/app.ts
@@ -1,2 +1,17 @@
-// 1 - express
-console.log('Express com TypeScript')
+import express from "express"
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+})
+
+app.post("/api/product", (req, res) => {
+    console.log(req.body)
+    return res.send('Produto adicionado');
+});
+
+app.listen(3000, () => {
+    console.log("Server is running");
+})
