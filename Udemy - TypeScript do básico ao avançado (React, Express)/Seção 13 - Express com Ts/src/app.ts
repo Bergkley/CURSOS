83bdb1a6e42953822432baf532d2af3a75a33b7b
Bergkley <bergkley@gmail.com>
2024-11-02 22:03:07 -0300

    feat: refatoração

--- Udemy - TypeScript do básico ao avançado (React, Express)/Seção 13 - Express com Ts/src/app.ts
+++ Udemy - TypeScript do básico ao avançado (React, Express)/Seção 13 - Express com Ts/src/app.ts
@@ -1,86 +1,131 @@
-import express, { NextFunction, Request, Response } from "express"
+// 1 - iniciar projeto
+// console.log("Hello Express + TS");
+
+// 2 - init express
+import express, { NextFunction, Request, Response } from "express";
+
 const app = express();
 
+// 3 - rota com POST
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("Hello World");
-})
-
-function showPath(req: Request, res: Response, next:NextFunction) {
-    console.log(req.path)
-    next()
+// 11 - middleware para todas as rotas
+function showPath(req: Request, res: Response, next: NextFunction) {
+  console.log(req.path);
+  next();
 }
 
-app.use(showPath)
+app.use(showPath);
 
+app.get("/", (req, res) => {
+  return res.send("Hello Express!");
+});
+
+// 3 - rota com POST
 app.post("/api/product", (req, res) => {
-    console.log(req.body)
-    return res.send('Produto adicionado');
+  console.log(req.body);
+
+  return res.send("Produto cadastrado!");
 });
 
+// 4 - rota para todos os verbos
 app.all("/api/product/check", (req, res) => {
-    if(req.method === "POST"){
-        return res.send("Inseriu alguma coisa");
-    }else if(req.method === "GET"){ 
-        return res.send("Leu alguma coisa");
-    }else {
-        return res.send("Algo deu errado");
-    }
+  if (req.method === "POST") {
+    return res.send("Inseriu algo!");
+  } else if (req.method === "GET") {
+    return res.send("Leu algo!");
+  }
 });
 
+// 5 - interfaces do express
 app.get("/api/interfaces", (req: Request, res: Response) => {
-    return res.send("utilizando as interfaces")
-})
+  return res.send("Utilizando as interfaces!");
+});
 
+// 6 - enviando JSON
+app.get("/api/json", (req: Request, res: Response) => {
+  return res.json({
+    name: "Shirt",
+    price: 19.99,
+    inStock: true,
+  });
+});
+
+// 7 - router parameters
 app.get("/api/product/:id", (req: Request, res: Response) => {
-    console.log(req.params)
-    return res.send("Id: " + req.params.id)
-})
+  console.log(req.params);
 
-app.get ("/api/product/:id/review/:reviewId", (req: Request, res: Response) => {
-    console.log(req.params)
-    return res.send("Id: " + req.params.id + " Review: " + req.params.reviewId)
-})
+  const product = {
+    id: 1,
+    name: "Boné",
+    price: 40.49,
+  };
+
+  return res.json(product);
+});
+
+// 8 - rotas complexas
+app.get("/api/product/:id/review/:reviewId", (req: Request, res: Response) => {
+  console.log(req.params);
+
+  const productId = req.params.id;
+  const reviewId = req.params.reviewId;
+
+  return res.send(`Acessando a review ${reviewId} do produto ${productId}!`);
+});
+
+// 9 - router handle
+app.get("/api/user/:id", getUser);
 
 function getUser(req: Request, res: Response) {
-    console.log('Resgatando o usuário: ' + req.params.id)
-    return res.send("Id: " + req.params.id)
-}
-app.get("/api/user/:id", getUser)
+  console.log(`Resgatando usuário com id: ${req.params.id}`);
 
-function checkUser(req: Request, res: Response, next:NextFunction) {
-    if(req.params.id === "1"){
-        console.log("Pode seguir!")
-        next()
-    }else {
-        console.log("Não pode seguir!")
-        
-    }
+  return res.send("O usuário foi encontrado!");
 }
 
-app.get("/api/user/:id/acess",checkUser, (req: Request, res: Response) => {
-    return res.json({msg: "Acesso concedido"})
-})
+// 10 - middleware
+function someMiddleware(req: Request, res: Response, next: NextFunction) {
+  if (req.params.id === "1") {
+    console.log("Pode seguir!");
+    next();
+  } else {
+    console.log("Não pode seguir");
+  }
+}
 
+app.get(
+  "/api/user/:id/access",
+  someMiddleware,
+  (req: Request, res: Response, next: NextFunction) => {
+    return res.json({ msg: "Bem-vindo a área administrativa!" });
+  }
+);
 
-app.get("/api/json", (req: Request, res: Response) => {
-    return res.json({name: "shirts", price: 100})
-})
+// 12 - req res generics
+app.get(
+  "/api/user/:id/details/:name",
+  (
+    req: Request<{ id: string; name: "string" }>,
+    res: Response<{ status: boolean }>
+  ) => {
+    console.log(`ID: ${req.params.id}`);
+    console.log(`Name: ${req.params.name}`);
 
-app.get("/api/user/:id/details/:name", (req: Request<{id: string, name: string}>, res: Response<{status: boolean}>) => {
-    console.log("id: " + req.params.id + " name: " + req.params.name)
-    return res.json({status: true})
-})
+    return res.json({ status: true });
+  }
+);
 
+// 13 - tratando erros
 app.get("/api/error", (req: Request, res: Response) => {
-    try {
-        throw new Error("Ocorreu um erro")
-    } catch (e:any) {
-         res.status(500).json({error: e.message})
-    }
-})
+  try {
+    throw new Error("Algo deu errado!");
+  } catch (e: any) {
+    res.statusCode = 500;
+
+    res.status(500).json({ msg: e.message });
+  }
+});
 
 app.listen(3000, () => {
-    console.log("Server is running");
-})
+  console.log("Aplicação TS + Express funcionando!");
+});
