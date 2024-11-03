72fe39800b67ff406f1fc0bef971f9e61ebe16cd
Bergkley <bergkley@gmail.com>
2024-11-03 13:48:23 -0300

    feat: integrando express

--- Udemy - TypeScript do básico ao avançado (React, Express)/Seção 14 - Api RestFull Ts/src/app.ts
+++ Udemy - TypeScript do básico ao avançado (React, Express)/Seção 14 - Api RestFull Ts/src/app.ts
@@ -1,1 +1,11 @@
-console.log('deu boa')
+import express from "express";
+import config from "config";
+
+const app = express();
+
+// JSON middleware
+app.use(express.json());
+
+app.listen(config.get<number>("port"), async ()=>{
+    console.log(`Servidor online na porta ${config.get<number>("port")}`)
+})
