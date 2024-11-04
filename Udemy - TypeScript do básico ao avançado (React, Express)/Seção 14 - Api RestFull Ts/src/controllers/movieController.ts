247da84e714bc333634712d7e50e245e6ce201f6
Bergkley <bergkley@gmail.com>
2024-11-03 22:24:04 -0300

    feat: removeMovie

--- Udemy - TypeScript do básico ao avançado (React, Express)/Seção 14 - Api RestFull Ts/src/controllers/movieController.ts
+++ Udemy - TypeScript do básico ao avançado (React, Express)/Seção 14 - Api RestFull Ts/src/controllers/movieController.ts
@@ -13,6 +13,7 @@ export async function createMovie(req: Request, res: Response) {
         return res.status(201).json(movie)
     } catch (e:any) {
         Logger.error(`Erro no sistema: ${e.message}`)
+        return res.status(500).json({error: "Por favor,tente mais tarde !"})
     }
 }
 
@@ -36,5 +37,21 @@ export async function getAllMovies(req: Request, res: Response) {
         const movies = await MovieModel.find();
     } catch (e:any) {
         Logger.error(`Erro no sistema: ${e.message}`)
+    }
+}
+
+export async function removeMovie(req: Request, res: Response) {
+    try {
+        const id=req.params.id
+        const movie = await MovieModel.findByIdAndDelete(id)
+
+        if(!movie) {
+            return res.status(404).json({message: "Filme não encontrado"})
+        }
+
+        return res.status(200).json({message: "Filme removido com sucesso"})
+    } catch (e:any) {
+        Logger.error(`Erro no sistema: ${e.message}`)
+        return res.status(500).json({error: "Por favor,tente mais tarde !"})
     }
 }
