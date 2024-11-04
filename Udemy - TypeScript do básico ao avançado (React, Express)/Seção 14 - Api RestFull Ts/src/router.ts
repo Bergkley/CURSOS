548972fb2a5c683b8949547aed368240f75d5ce2
Bergkley <bergkley@gmail.com>
2024-11-03 21:45:21 -0300

    feat: middleware movie validation

--- Udemy - TypeScript do básico ao avançado (React, Express)/Seção 14 - Api RestFull Ts/src/router.ts
+++ Udemy - TypeScript do básico ao avançado (React, Express)/Seção 14 - Api RestFull Ts/src/router.ts
@@ -3,10 +3,11 @@ import { createMovie } from './controllers/movieController'
 
 // validations
 import { validate} from './middleware/handleValidation'
+import { movieCreateValidation } from './middleware/movieValidation'
 
 const router = Router()
 
 export default router.get('/test', (req: Request, res: Response) => {
     res.status(200).send('Teste')
 })
-.post("/movie",validate, createMovie)
+.post("/movie",movieCreateValidation,validate, createMovie)
