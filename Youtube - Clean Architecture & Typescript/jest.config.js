4a776ff112de2e7ab9c31e9ec3ec5d25aad0b967
Bergkley <bergkley@gmail.com>
2024-12-02 19:58:29 -0300

    feat: config jest

---
+++ Youtube - Clean Architecture & Typescript/jest.config.js
@@ -0,0 +1,10 @@
+modules.exports = {
+    roots: ['<rootDir>/src'],
+    testEnvironment: 'node',
+    transform: {
+        '.+\\.ts$' : 'ts-jest'
+    },
+    moduleNameMapper:{
+        '@/(.*)': '<rootDir>/src/$1'
+    }             
+}
