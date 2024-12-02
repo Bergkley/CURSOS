e153dfede688041ccaa945a77f488b7d1ddc4fd8
Bergkley <bergkley@gmail.com>
2024-12-01 21:08:24 -0300

    feat: refatoração

--- Udemy -  WebSockets com NodeJS, Socket.IO e Bootstrap 5/Introdução/src/index.js
+++ Udemy -  WebSockets com NodeJS, Socket.IO e Bootstrap 5/Introdução/src/index.js
@@ -1,31 +1,68 @@
 import express from 'express';
 import http from 'http';
-import sockeio from 'socket.io';
+import socketio from 'socket.io';
 
 const app = express();
-const server = http.Server(app)
+const server = http.Server(app);
 app.use(express.static(__dirname + '/public'));
 
-const io = sockeio(server);
+const io = socketio(server);
+let users = [];  
 
-io.on('connect',(socket)=> {
+io.on('connect', (socket) => {
+    console.log('Novo cliente conectado', socket.id);
 
-    io.to(socket.id).emit("teste", { status: true, message: "Conexão estabelecida!" });
+    socket.on('login', (username) => {
+        users.push({ id: socket.id, username: username });
+        console.log(`${username} entrou no chat.`);
 
-    socket.on('teste', (res) => {
-        console.log('Mensagem recebida!', res);
+        socket.broadcast.emit('chat-message', { message: `${username} entrou no chat.`, systemMessage: true });
+    
+        io.emit('updateUsers', users);
 
-        socket.broadcast.emit('teste', res);
+    });
+
+    socket.on('disconnect', () => {
+        const userExit = users.find(user => user.id === socket.id);
+        users = users.filter(user => user.id !== socket.id);
+        console.log('User exit', userExit);
+        console.log(`Cliente desconectado: ${socket.id}`);
+        
+        io.emit('updateUsers', users);
+
+        socket.broadcast.emit('chat-message', { message: `${userExit?.username} saiu no chat.`, systemMessage: true });
+
+    });
+
+    socket.on('logout',(username) => {
+        users = users.filter(user => user.username !== username);
+        console.log('User logout', users);
+        socket.broadcast.emit('chat-message', { message: `${username} saiu do chat.`, systemMessage: true });
+        io.emit('updateUsers', users);
     })
-})
+
+    socket.on('infoUser',(username) => {
+        users = users.filter(user => user.username === username);
+        console.log('chegou aqui')
+        console.log('socket.id')
+        socket.emit('infoUser', {socketId: socket.id, user: users});
+    })
+    
 
 
+    socket.on('send-message', (res) => {
+        console.log('Mensagem recebida!', res);
+        socket.broadcast.emit('send-message', res); 
+    });
 
+    
+    
+});
 
 app.get('/', (req, res) => {
-    res.render('index.html');
-})
+    res.sendFile(__dirname + '/public/index.html');
+});
 
 server.listen(3333, () => {
     console.log('Server running on port 3333');
-})
+});
