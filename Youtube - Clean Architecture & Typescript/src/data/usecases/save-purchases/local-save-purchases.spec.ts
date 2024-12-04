988942e7176b65e034b7f16e38da2637f50f8bf2
Bergkley <bergkley@gmail.com>
2024-12-04 19:26:34 -0300

    feat:refatorando

--- Youtube - Clean Architecture & Typescript/src/data/usecases/save-purchases/local-save-purchases.spec.ts
+++ Youtube - Clean Architecture & Typescript/src/data/usecases/save-purchases/local-save-purchases.spec.ts
@@ -32,12 +32,12 @@ describe('LocalSavePurchases', () => {
          expect(cacheStore.deleteKey).toBe('purchases');
     });
 
-    test('Should not insert new Cache if delete fails',  () => {
+    test('Should not insert new Cache if delete fails',async  () => {
         const { cacheStore, sut } = makeSut();
         cacheStore.simulateDeleteError()
         const promise = sut.save(mockPurchases());
         expect(cacheStore.messages).toEqual([CacheStoreSpy.Message.delete]);
-        expect(promise).rejects.toThrow()
+        await expect(promise).rejects.toThrow()
     });
 
     test('Should  insert new Cache if delete succeeds', async () => {
@@ -50,12 +50,12 @@ describe('LocalSavePurchases', () => {
 
     });
 
-    test('Should thory if insert thorws',  () => {
+    test('Should thory if insert thorws', async () => {
         const { cacheStore, sut } = makeSut();
         cacheStore.simulateInsertError()
         const promise = sut.save(mockPurchases());
         expect(cacheStore.messages).toEqual([CacheStoreSpy.Message.delete, CacheStoreSpy.Message.insert]);
-        expect(promise).rejects.toThrow()
+        await expect(promise).rejects.toThrow()
     });
 
     // novo
