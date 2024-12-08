47e4bdbcb7e5f759bb8fc9104fd349850e25ad15
Bergkley <bergkley@gmail.com>
2024-12-08 18:38:16 -0300

    feat:local-validate-purchases

---
+++ Youtube - Clean Architecture & Typescript/src/data/usecases/load-purchases/local-validate-purchases.spec.ts
@@ -0,0 +1,29 @@
+import { LocalLoadPurchases } from "@/data/usecases";
+import { CacheStoreSpy } from "@/data/testes";
+
+
+
+type SutTypes = {
+    sut: LocalLoadPurchases,
+    cacheStore: CacheStoreSpy
+}
+
+const makeSut = (timeStamp = new Date()): SutTypes => {
+    
+    const cacheStore = new CacheStoreSpy();
+    const sut = new LocalLoadPurchases(cacheStore, timeStamp);
+
+    return {
+        sut,
+        cacheStore
+    }
+}
+
+describe('LocalLoadPurchases', () => {
+
+    test('Should not delete or insert cache on sut.init', () => {
+        const { cacheStore } = makeSut()
+        expect(cacheStore.actions).toEqual([]);
+    });
+  });
+  
