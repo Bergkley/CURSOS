f960b7bd269b64cddf3d427acfd563a56178e6b4
Bergkley <bergkley@gmail.com>
2024-12-12 20:46:24 -0300

    feat:refatoração

--- Youtube - Clean Architecture & Typescript/src/data/testes/mock-purchases.ts
+++ Youtube - Clean Architecture & Typescript/src/data/testes/mock-purchases.ts
@@ -1,11 +1,14 @@
 import { SavePurchases } from "@/domain/usecases";
-import { faker } from '@faker-js/faker';
-export const mockPurchases = (): Array<SavePurchases.Params> => [{
+import { faker } from "@faker-js/faker";
+export const mockPurchases = (): Array<SavePurchases.Params> => [
+  {
     id: faker.string.uuid(),
     date: faker.date.recent(),
-    value: faker.number.int(100)
-},{
+    value: faker.number.int(100),
+  },
+  {
     id: faker.string.uuid(),
     date: faker.date.recent(),
-    value: faker.number.int(100)
-}]
+    value: faker.number.int(100),
+  },
+];
