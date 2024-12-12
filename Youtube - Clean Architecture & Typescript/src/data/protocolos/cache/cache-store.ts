3602a8e70ae28787f587c5236dc546ce54eaea37
Bergkley <bergkley@gmail.com>
2024-12-11 21:04:16 -0300

    refatoração

--- Youtube - Clean Architecture & Typescript/src/data/protocolos/cache/cache-store.ts
+++ Youtube - Clean Architecture & Typescript/src/data/protocolos/cache/cache-store.ts
@@ -1,6 +1,6 @@
-export interface  CacheStore{ 
-    fetch: (key:string) => any;
-    delete: (key:string) => void;
-    insert: (key:string, value:any) => void;
-    replace: (key:string, value:any) => void;
+export interface CacheStore {
+  fetch: (key: string) => any;
+  delete: (key: string) => void;
+  insert: (key: string, value: any) => void;
+  replace: (key: string, value: any) => void;
 }
