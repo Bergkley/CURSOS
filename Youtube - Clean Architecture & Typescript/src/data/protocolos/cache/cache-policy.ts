4491f8b8e1b0e746f198baa9eae87fd7f08d4ef2
Bergkley <bergkley@gmail.com>
2024-12-11 21:04:02 -0300

    refatoração

--- Youtube - Clean Architecture & Typescript/src/data/protocolos/cache/cache-policy.ts
+++ Youtube - Clean Architecture & Typescript/src/data/protocolos/cache/cache-policy.ts
@@ -1,9 +1,9 @@
 export class CachePolicy {
-    private static maxAgeInDays = 3
-    private constructor () {}
-    static validade (timestamp: Date, date: Date): boolean {
-        const maxAge = new Date(timestamp)
-        maxAge.setDate(maxAge.getDate() + CachePolicy.maxAgeInDays)
-        return maxAge > date
-    }
+  private static maxAgeInDays = 3;
+  private constructor() {}
+  static validade(timestamp: Date, date: Date): boolean {
+    const maxAge = new Date(timestamp);
+    maxAge.setDate(maxAge.getDate() + CachePolicy.maxAgeInDays);
+    return maxAge > date;
+  }
 }
