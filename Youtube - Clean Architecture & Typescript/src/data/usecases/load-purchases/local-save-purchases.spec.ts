e4d48e6c631500a948441b86ee10dc926a140af6
Bergkley <bergkley@gmail.com>
2024-12-13 22:18:25 -0300

    feat:refatoração

--- Youtube - Clean Architecture & Typescript/src/data/usecases/load-purchases/local-save-purchases.spec.ts
+++ Youtube - Clean Architecture & Typescript/src/data/usecases/load-purchases/local-save-purchases.spec.ts
@@ -1,67 +1,60 @@
 import { LocalLoadPurchases } from "@/data/usecases";
 import { mockPurchases, CacheStoreSpy } from "@/data/testes";
 
-
-
 type SutTypes = {
-    sut: LocalLoadPurchases,
-    cacheStore: CacheStoreSpy
-}
+  sut: LocalLoadPurchases;
+  cacheStore: CacheStoreSpy;
+};
 
 const makeSut = (timeStamp = new Date()): SutTypes => {
-    
-    const cacheStore = new CacheStoreSpy();
-    const sut = new LocalLoadPurchases(cacheStore, timeStamp);
+  const cacheStore = new CacheStoreSpy();
+  const sut = new LocalLoadPurchases(cacheStore, timeStamp);
 
-    return {
-        sut,
-        cacheStore
-    }
-}
+  return {
+    sut,
+    cacheStore,
+  };
+};
 
-describe('LocalSavePurchases', () => {
-    test('Should not delete or insert cache on sut.init', () => {
-        const { cacheStore } = makeSut()
-        expect(cacheStore.actions).toEqual([]);
-    });
-
-    
-
-    test('Should not insert new Cache if delete fails',async  () => {
-        const { cacheStore, sut } = makeSut();
-        cacheStore.simulateDeleteError()
-        const promise = sut.save(mockPurchases());
-        expect(cacheStore.actions).toEqual([CacheStoreSpy.Action.delete]);
-        await expect(promise).rejects.toThrow()
-    });
-
-    test('Should  insert new Cache if delete succeeds', async () => {
-        const timestamp = new Date();
-        const { cacheStore, sut } = makeSut(timestamp);
-        const purchases = mockPurchases()
-        const promise = sut.save(purchases);
-        expect(cacheStore.actions).toEqual([CacheStoreSpy.Action.delete, CacheStoreSpy.Action.insert]);
-        expect(cacheStore.insertKey).toBe('purchases');
-        expect(cacheStore.insertValues).toEqual({timestamp, value: purchases});
-        expect(cacheStore.deleteKey).toBe('purchases');
-        await expect(promise).resolves.toBeFalsy()
-
-
-    });
-
-    test('Should thory if insert thorws', async () => {
-        const { cacheStore, sut } = makeSut();
-        cacheStore.simulateInsertError()
-        const promise = sut.save(mockPurchases());
-        expect(cacheStore.actions).toEqual([CacheStoreSpy.Action.delete, CacheStoreSpy.Action.insert]);
-        await expect(promise).rejects.toThrow()
-    });
-
-    // novo
-
-
-    
-
-
+describe("LocalSavePurchases", () => {
+  test("Should not delete or insert cache on sut.init", () => {
+    const { cacheStore } = makeSut();
+    expect(cacheStore.actions).toEqual([]);
   });
-  
+
+  test("Should not insert new Cache if delete fails", async () => {
+    const { cacheStore, sut } = makeSut();
+    cacheStore.simulateDeleteError();
+    const promise = sut.save(mockPurchases());
+    expect(cacheStore.actions).toEqual([CacheStoreSpy.Action.delete]);
+    await expect(promise).rejects.toThrow();
+  });
+
+  test("Should  insert new Cache if delete succeeds", async () => {
+    const timestamp = new Date();
+    const { cacheStore, sut } = makeSut(timestamp);
+    const purchases = mockPurchases();
+    const promise = sut.save(purchases);
+    expect(cacheStore.actions).toEqual([
+      CacheStoreSpy.Action.delete,
+      CacheStoreSpy.Action.insert,
+    ]);
+    expect(cacheStore.insertKey).toBe("purchases");
+    expect(cacheStore.insertValues).toEqual({ timestamp, value: purchases });
+    expect(cacheStore.deleteKey).toBe("purchases");
+    await expect(promise).resolves.toBeFalsy();
+  });
+
+  test("Should thory if insert thorws", async () => {
+    const { cacheStore, sut } = makeSut();
+    cacheStore.simulateInsertError();
+    const promise = sut.save(mockPurchases());
+    expect(cacheStore.actions).toEqual([
+      CacheStoreSpy.Action.delete,
+      CacheStoreSpy.Action.insert,
+    ]);
+    await expect(promise).rejects.toThrow();
+  });
+
+  // novo
+});
