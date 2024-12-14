7899ff30f3a2b03f38d36dde7716f7f8f9b477d0
Bergkley <bergkley@gmail.com>
2024-12-13 22:18:31 -0300

    feat:refatoração

--- Youtube - Clean Architecture & Typescript/src/data/usecases/load-purchases/local-validate-purchases.spec.ts
+++ Youtube - Clean Architecture & Typescript/src/data/usecases/load-purchases/local-validate-purchases.spec.ts
@@ -1,75 +1,76 @@
 import { LocalLoadPurchases } from "@/data/usecases";
 import { CacheStoreSpy, getCacheExpirationDate } from "@/data/testes";
 
-
-
 type SutTypes = {
-    sut: LocalLoadPurchases,
-    cacheStore: CacheStoreSpy
-}
+  sut: LocalLoadPurchases;
+  cacheStore: CacheStoreSpy;
+};
 
 const makeSut = (timeStamp = new Date()): SutTypes => {
-    
-    const cacheStore = new CacheStoreSpy();
-    const sut = new LocalLoadPurchases(cacheStore, timeStamp);
+  const cacheStore = new CacheStoreSpy();
+  const sut = new LocalLoadPurchases(cacheStore, timeStamp);
 
-    return {
-        sut,
-        cacheStore
-    }
-}
+  return {
+    sut,
+    cacheStore,
+  };
+};
 
-describe('LocalLoadPurchases', () => {
-
-    test('Should not delete or insert cache on sut.init', () => {
-        const { cacheStore } = makeSut()
-        expect(cacheStore.actions).toEqual([]);
-    });
-
-    test('Should delete cache  if load fails ', () => {
-        const { cacheStore,sut } = makeSut()
-        cacheStore.simulateFetchError()
-         sut.validate()
-        expect(cacheStore.actions).toEqual([CacheStoreSpy.Action.fetch, CacheStoreSpy.Action.delete]);
-        expect(cacheStore.deleteKey).toBe('purchases');
-    });
-
-    test('Should has no side effect if load succeds', () => {
-        const currentDate = new Date()
-        const timestamp = getCacheExpirationDate(currentDate)
-        timestamp.setSeconds(timestamp.getSeconds() + 1)
-        const { cacheStore,sut } = makeSut(currentDate)
-        cacheStore.fetchResult = { timestamp }
-        sut.validate()
-        expect(cacheStore.actions).toEqual([CacheStoreSpy.Action.fetch]);
-        expect(cacheStore.fetchKey).toBe('purchases');
-    });
-
-    test('Should delete cache if its expired', () => {
-        const currentDate = new Date()
-        const timestamp = getCacheExpirationDate(currentDate)
-        timestamp.setSeconds(timestamp.getSeconds() - 1)
-        const { cacheStore,sut } = makeSut(currentDate)
-        cacheStore.fetchResult = { timestamp}
-        sut.validate()
-        expect(cacheStore.actions).toEqual([CacheStoreSpy.Action.fetch, CacheStoreSpy.Action.delete]);
-        expect(cacheStore.fetchKey).toBe('purchases');
-        expect(cacheStore.deleteKey).toBe('purchases');
-    });
-
-
-    test('Should delete cache if its on expired', () => {
-        const currentDate = new Date()
-        const timestamp = getCacheExpirationDate(currentDate)
-
-        const { cacheStore,sut } = makeSut(currentDate)
-        cacheStore.fetchResult = { timestamp }
-        sut.validate()
-        expect(cacheStore.actions).toEqual([CacheStoreSpy.Action.fetch, CacheStoreSpy.Action.delete]);
-        expect(cacheStore.fetchKey).toBe('purchases');
-        expect(cacheStore.deleteKey).toBe('purchases');
-    });
-
-    
+describe("LocalLoadPurchases", () => {
+  test("Should not delete or insert cache on sut.init", () => {
+    const { cacheStore } = makeSut();
+    expect(cacheStore.actions).toEqual([]);
   });
-  
+
+  test("Should delete cache  if load fails ", () => {
+    const { cacheStore, sut } = makeSut();
+    cacheStore.simulateFetchError();
+    sut.validate();
+    expect(cacheStore.actions).toEqual([
+      CacheStoreSpy.Action.fetch,
+      CacheStoreSpy.Action.delete,
+    ]);
+    expect(cacheStore.deleteKey).toBe("purchases");
+  });
+
+  test("Should has no side effect if load succeds", () => {
+    const currentDate = new Date();
+    const timestamp = getCacheExpirationDate(currentDate);
+    timestamp.setSeconds(timestamp.getSeconds() + 1);
+    const { cacheStore, sut } = makeSut(currentDate);
+    cacheStore.fetchResult = { timestamp };
+    sut.validate();
+    expect(cacheStore.actions).toEqual([CacheStoreSpy.Action.fetch]);
+    expect(cacheStore.fetchKey).toBe("purchases");
+  });
+
+  test("Should delete cache if its expired", () => {
+    const currentDate = new Date();
+    const timestamp = getCacheExpirationDate(currentDate);
+    timestamp.setSeconds(timestamp.getSeconds() - 1);
+    const { cacheStore, sut } = makeSut(currentDate);
+    cacheStore.fetchResult = { timestamp };
+    sut.validate();
+    expect(cacheStore.actions).toEqual([
+      CacheStoreSpy.Action.fetch,
+      CacheStoreSpy.Action.delete,
+    ]);
+    expect(cacheStore.fetchKey).toBe("purchases");
+    expect(cacheStore.deleteKey).toBe("purchases");
+  });
+
+  test("Should delete cache if its on expired", () => {
+    const currentDate = new Date();
+    const timestamp = getCacheExpirationDate(currentDate);
+
+    const { cacheStore, sut } = makeSut(currentDate);
+    cacheStore.fetchResult = { timestamp };
+    sut.validate();
+    expect(cacheStore.actions).toEqual([
+      CacheStoreSpy.Action.fetch,
+      CacheStoreSpy.Action.delete,
+    ]);
+    expect(cacheStore.fetchKey).toBe("purchases");
+    expect(cacheStore.deleteKey).toBe("purchases");
+  });
+});
