b69b54e0a56a0c7a462629eca9c56c33f9c1c8f1
Bergkley <bergkley@gmail.com>
2024-12-13 22:18:17 -0300

    feat:refatoração

--- Youtube - Clean Architecture & Typescript/src/data/usecases/load-purchases/local-load-purchases.ts
+++ Youtube - Clean Architecture & Typescript/src/data/usecases/load-purchases/local-load-purchases.ts
@@ -1,34 +1,38 @@
-import { CachePolicy, CacheStore } from "@/data/protocolos/cache"
-import { SavePurchases , LoadPurchases} from "@/domain/usecases"
-export class LocalLoadPurchases implements SavePurchases, LoadPurchases { 
-    private readonly key = 'purchases'
-    constructor(
-        private readonly cacheStore: CacheStore,
-         private readonly currentDate: Date) {}
+import { CachePolicy, CacheStore } from "@/data/protocolos/cache";
+import { SavePurchases, LoadPurchases } from "@/domain/usecases";
+export class LocalLoadPurchases implements SavePurchases, LoadPurchases {
+  private readonly key = "purchases";
+  constructor(
+    private readonly cacheStore: CacheStore,
+    private readonly currentDate: Date
+  ) {}
 
-     async save (purchases: Array<SavePurchases.Params>): Promise<void> {
-        this.cacheStore.replace(this.key, {
-            timestamp: this.currentDate,
-            value: purchases})
-    }
+  async save(purchases: Array<SavePurchases.Params>): Promise<void> {
+    this.cacheStore.replace(this.key, {
+      timestamp: this.currentDate,
+      value: purchases,
+    });
+  }
 
-    async loadAll (): Promise<Array<LoadPurchases.Result>> {
-        try {
-            const cache = this.cacheStore.fetch(this.key)
-            return CachePolicy.validade(cache.timestamp, this.currentDate) ? cache.value : []             
-        } catch (err) {
-            return [];
-        }
+  async loadAll(): Promise<Array<LoadPurchases.Result>> {
+    try {
+      const cache = this.cacheStore.fetch(this.key);
+      return CachePolicy.validade(cache.timestamp, this.currentDate)
+        ? cache.value
+        : [];
+    } catch (err) {
+      return [];
     }
+  }
 
-    validate (): void {
-        try {
-            const cache = this.cacheStore.fetch(this.key)  
-            if(!CachePolicy.validade(cache.timestamp, this.currentDate)) {
-                throw new Error()
-            }   
-        } catch (err) {
-            this.cacheStore.delete(this.key)
-        }
+  validate(): void {
+    try {
+      const cache = this.cacheStore.fetch(this.key);
+      if (!CachePolicy.validade(cache.timestamp, this.currentDate)) {
+        throw new Error();
+      }
+    } catch (err) {
+      this.cacheStore.delete(this.key);
     }
+  }
 }
