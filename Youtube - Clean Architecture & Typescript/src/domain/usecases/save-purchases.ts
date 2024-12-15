16d970af7908916f2163ca8ed83146259fd3fad3
Bergkley <bergkley@gmail.com>
2024-12-15 19:30:08 -0300

    feat:refatoração

--- Youtube - Clean Architecture & Typescript/src/domain/usecases/save-purchases.ts
+++ Youtube - Clean Architecture & Typescript/src/domain/usecases/save-purchases.ts
@@ -1,10 +1,9 @@
 import { PurchaseModel } from "@/domain/models";
 
-
 export interface SavePurchases {
   save(purchases: Array<SavePurchases.Params>): Promise<void>;
 }
 
 export namespace SavePurchases {
-  export type Params = PurchaseModel
+  export type Params = PurchaseModel;
 }
