b70e33eac9a7b2752a829ecaeac58902fc96c679
Bergkley <bergkley@gmail.com>
2024-12-19 21:32:18 -0300

    refatoração

--- Youtube - Clean Architecture & Typescript/src/data/testes/mock-cache.ts
+++ Youtube - Clean Architecture & Typescript/src/data/testes/mock-cache.ts
@@ -3,6 +3,7 @@ import { CacheStore } from "@/data/protocolos/cache";
 
 const maxAgeInDays = 3;
 export const getCacheExpirationDate = (timestamp: Date): Date => {
+  
   const maxCacheAge = new Date(timestamp);
 
   maxCacheAge.setDate(maxCacheAge.getDate() - maxAgeInDays);
