e6bf8edb707001e5b00820fc0466d6fedd7a57e0
Bergkley <bergkley@gmail.com>
2024-12-29 20:23:24 -0300

    refatoração

--- Youtube - Clean Architecture & Typescript/src/data/testes/mock-cache.ts
+++ Youtube - Clean Architecture & Typescript/src/data/testes/mock-cache.ts
@@ -54,6 +54,7 @@ export class CacheStoreSpy implements CacheStore {
   simulateDeleteError(): void {
     
     jest.spyOn(CacheStoreSpy.prototype, "delete").mockImplementationOnce(() => {
+      
       this.actions.push(CacheStoreSpy.Action.delete);
 
       throw new Error();
