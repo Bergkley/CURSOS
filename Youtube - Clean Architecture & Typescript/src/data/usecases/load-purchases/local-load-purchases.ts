69999a50245a1b0842b2a60ef3999d8f074a1273
Bergkley <bergkley@gmail.com>
2025-01-11 19:25:25 -0300

    refatoração

--- Youtube - Clean Architecture & Typescript/src/data/usecases/load-purchases/local-load-purchases.ts
+++ Youtube - Clean Architecture & Typescript/src/data/usecases/load-purchases/local-load-purchases.ts
@@ -1,14 +1,12 @@
 import { CachePolicy, CacheStore } from "@/data/protocolos/cache";
 import { SavePurchases, LoadPurchases } from "@/domain/usecases";
 export class LocalLoadPurchases implements SavePurchases, LoadPurchases {
-
   private readonly key = "purchases";
-  
+
   constructor(
     private readonly cacheStore: CacheStore,
 
     private readonly currentDate: Date
-    
   ) {}
 
   async save(purchases: Array<SavePurchases.Params>): Promise<void> {
