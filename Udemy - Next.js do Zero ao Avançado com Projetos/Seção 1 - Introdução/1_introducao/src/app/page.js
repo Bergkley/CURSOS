9e939f08f1d53872652d63e6a2b3f290509f3a2a
Bergkley <bergkley@gmail.com>
2025-02-17 20:55:50 -0300

    feat: Iniciando o projeto em next.js

---
+++ Udemy - Next.js do Zero ao Avançado com Projetos/Seção 1 - Introdução/1_introducao/src/app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <h1>Hello Word Next.js</h1>
+        
+      </main>
+     
+  );
+}
