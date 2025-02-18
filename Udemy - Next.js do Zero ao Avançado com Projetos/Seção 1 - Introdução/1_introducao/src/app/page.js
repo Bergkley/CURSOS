3598a0537cf42b73859f0a2f477ab1e9872f3c4b
Bergkley <bergkley@gmail.com>
2025-02-17 21:12:22 -0300

    chore: eslint

--- Udemy - Next.js do Zero ao Avançado com Projetos/Seção 1 - Introdução/1_introducao/src/app/page.js
+++ Udemy - Next.js do Zero ao Avançado com Projetos/Seção 1 - Introdução/1_introducao/src/app/page.js
@@ -2,10 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1>Hello Word Next.js</h1>
-      </main>
-     
+    <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      <h1>Hello Word Next.js</h1>
+    </main>
   );
 }
