f00d08b4b52a8e1ea3c18790b925bcfe94136dc7
Bergkley <bergkley@gmail.com>
2025-02-19 14:42:12 -0300

    feat:css global

--- Udemy - Next.js do Zero ao Avançado com Projetos/Seção 3 - Estilização/src/app/page.js
+++ Udemy - Next.js do Zero ao Avançado com Projetos/Seção 3 - Estilização/src/app/page.js
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      {/* 1 - CSS Global */}
       <h1>Hello Word Next.js</h1>
+
     </main>
   );
 }
