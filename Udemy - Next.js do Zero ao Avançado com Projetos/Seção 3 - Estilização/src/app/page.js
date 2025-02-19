24ad8bb014a4541c80d864a28ab09ce9c1307ba0
Bergkley <bergkley@gmail.com>
2025-02-19 14:47:17 -0300

    feat: CSS Modules

--- Udemy - Next.js do Zero ao Avançado com Projetos/Seção 3 - Estilização/src/app/page.js
+++ Udemy - Next.js do Zero ao Avançado com Projetos/Seção 3 - Estilização/src/app/page.js
@@ -1,9 +1,15 @@
+import styles from './page.module.css';
 export default function Home() {
   return (
     <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
       {/* 1 - CSS Global */}
       <h1>Hello Word Next.js</h1>
 
+      {/* 2 - CSS Modules */}
+      <h2 className={styles.heading_module}>Meu Css Modules</h2>
+      <div className={styles.container}>
+        <p>Testando Css modules</p>
+      </div>
     </main>
   );
 }
