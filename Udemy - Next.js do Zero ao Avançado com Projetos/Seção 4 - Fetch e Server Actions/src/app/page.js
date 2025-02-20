4bdebafe2335b5227fea07d83b25c71f4e48bd06
Bergkley <bergkley@gmail.com>
2025-02-20 15:31:17 -0300

    feat:estrutura base

--- Udemy - Next.js do Zero ao Avançado com Projetos/Seção 4 - Fetch e Server Actions/src/app/page.js
+++ Udemy - Next.js do Zero ao Avançado com Projetos/Seção 4 - Fetch e Server Actions/src/app/page.js
@@ -1,7 +1,9 @@
+import Link  from 'next/link';
+
 export default function Home() {
   return (
-    <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-      <h1>Hello Word Next.js</h1>
+    <main>
+      <Link href={'/todos/create'}>Ir para criar de todo</Link>
     </main>
   );
 }
---
+++ Udemy - Next.js do Zero ao Avançado com Projetos/Seção 4 - Fetch e Server Actions/src/app/todos/create/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TodoPage = () => {
+  return (
+    <div>Criação de Todo</div>
+  )
+}
+
+export default TodoPage
