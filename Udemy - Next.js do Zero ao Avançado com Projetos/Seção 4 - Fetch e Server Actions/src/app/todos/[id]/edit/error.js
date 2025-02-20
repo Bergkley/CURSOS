bb0d2be80952fc7c01f84d4d198745b227d064fc
Bergkley <bergkley@gmail.com>
2025-02-20 17:40:25 -0300

    feat: Tratando erros do back end

---
+++ Udemy - Next.js do Zero ao Avançado com Projetos/Seção 4 - Fetch e Server Actions/src/app/todos/[id]/edit/error.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'next/link';
+
+const TodoEditErrorPage = () => {
+  return (
+    <div>
+        <h1>Ocorreu um problmea, tente novamente mais tarde</h1>
+        <Link href={"/"}>Voltar para home</Link>
+    </div>
+  )
+}
+
+export default TodoEditErrorPage
