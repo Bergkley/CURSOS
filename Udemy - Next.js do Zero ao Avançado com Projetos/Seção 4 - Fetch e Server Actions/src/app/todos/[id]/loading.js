40a7c83cf881db30596372f725d66051ac2f081e
Bergkley <bergkley@gmail.com>
2025-02-20 17:02:15 -0300

    feat: loading

---
+++ Udemy - Next.js do Zero ao Avançado com Projetos/Seção 4 - Fetch e Server Actions/src/app/todos/[id]/loading.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LoadingTodo = () => {
+  return (
+    <div><p>Carregando...</p></div>
+  )
+}
+
+export default LoadingTodo
--- Udemy - Next.js do Zero ao Avançado com Projetos/Seção 4 - Fetch e Server Actions/src/app/todos/[id]/page.js
+++ Udemy - Next.js do Zero ao Avançado com Projetos/Seção 4 - Fetch e Server Actions/src/app/todos/[id]/page.js
@@ -3,6 +3,7 @@ import { db } from '@/db'
 import { notFound } from 'next/navigation'
 
 const TodoShow = async ({params}) => {
+    await new Promise((a)=> setTimeout(a,2000))
     const {id} = await params
 
     const todo= await db.todo.findFirst({where: {id: Number(id)}})
