bf107f04d02bf805531abee394fc558acafd17d0
Bergkley <bergkley@gmail.com>
2025-02-20 16:57:39 -0300

    feat: pagina notFound

--- Udemy - Next.js do Zero ao Avançado com Projetos/Seção 4 - Fetch e Server Actions/src/app/todos/[id]/page.js
+++ Udemy - Next.js do Zero ao Avançado com Projetos/Seção 4 - Fetch e Server Actions/src/app/todos/[id]/page.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { db } from '@/db'
+import { notFound } from 'next/navigation'
 
 const TodoShow = async ({params}) => {
     const {id} = await params
 
     const todo= await db.todo.findFirst({where: {id: Number(id)}})
+
+    if(!todo) return notFound()
   return (
     <div>{todo.titulo}</div>
   )
