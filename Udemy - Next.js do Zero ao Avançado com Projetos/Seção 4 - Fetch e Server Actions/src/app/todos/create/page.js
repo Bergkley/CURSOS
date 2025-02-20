2c6d5a7e0ad39678166cd00d1eb03b8b5b9288f1
Bergkley <bergkley@gmail.com>
2025-02-20 15:47:06 -0300

    feat: inserindo dados no banco

--- Udemy - Next.js do Zero ao Avançado com Projetos/Seção 4 - Fetch e Server Actions/src/app/todos/create/page.js
+++ Udemy - Next.js do Zero ao Avançado com Projetos/Seção 4 - Fetch e Server Actions/src/app/todos/create/page.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import { db } from '@/db'
+import { redirect } from 'next/navigation'
 
 const TodoPage = () => {
     const addTodo = async (formData) => {
         "use server"
-        console.log(formData)
+
+        const titulo = formData.get('titulo')
+        const descricao = formData.get('descricao')
+        const status = "pendente"
+
+        const todo = await db.todo.create({data:{titulo,descricao, status}})
+
+        console.log(todo)
+
+        redirect('/')
     }
     return (
         <div className="max-w-md mx-auto mt-10">
