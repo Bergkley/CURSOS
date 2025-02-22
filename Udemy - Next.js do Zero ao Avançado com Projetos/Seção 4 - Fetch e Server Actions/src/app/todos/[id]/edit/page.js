60dc52d2b080a7d40149e193fd7816cda3ade74a
Bergkley <bergkley@gmail.com>
2025-02-21 23:42:19 -0300

    refatoração

--- Udemy - Next.js do Zero ao Avançado com Projetos/Seção 4 - Fetch e Server Actions/src/app/todos/[id]/edit/page.js
+++ Udemy - Next.js do Zero ao Avançado com Projetos/Seção 4 - Fetch e Server Actions/src/app/todos/[id]/edit/page.js
@@ -1,21 +1,21 @@
-import { findTodoById } from '@/actions';
-import TodoForm from '@/components/TodoForm';
-import React from 'react'
+import { findTodoById } from "@/actions";
+import TodoForm from "@/components/TodoForm";
+import React from "react";
 
-const TodoEdit = async ({params}) => {
-    const {id} = await params
+const TodoEdit = async ({ params }) => {
+  const { id } = await params;
 
-    const todo = await findTodoById(Number(id));
+  const todo = await findTodoById(Number(id));
 
-    if (!todo) return notFound();
-    return (
-        <div className="max-w-md mx-auto mt-10">
-          <h1 className="text-2xl font-bold text-center mb-6">
-            Editando: {todo.titulo}
-          </h1>
-          <TodoForm todo={todo} />
-        </div>
-      );
-}
+  if (!todo) return notFound();
+  return (
+    <div className="max-w-md mx-auto mt-10">
+      <h1 className="text-2xl font-bold text-center mb-6">
+        Editando: {todo.titulo}
+      </h1>
+      <TodoForm todo={todo} />
+    </div>
+  );
+};
 
-export default TodoEdit
+export default TodoEdit;
