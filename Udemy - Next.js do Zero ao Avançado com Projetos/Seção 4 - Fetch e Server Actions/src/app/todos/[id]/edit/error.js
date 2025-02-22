463a7c23d4a1325244e513dd28b85614b0aace69
Bergkley <bergkley@gmail.com>
2025-02-21 23:41:51 -0300

    refatoração

--- Udemy - Next.js do Zero ao Avançado com Projetos/Seção 4 - Fetch e Server Actions/src/app/todos/[id]/edit/error.js
+++ Udemy - Next.js do Zero ao Avançado com Projetos/Seção 4 - Fetch e Server Actions/src/app/todos/[id]/edit/error.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import Link from 'next/link';
+import React from "react";
+import Link from "next/link";
 
 const TodoEditErrorPage = () => {
   return (
     <div>
-        <h1>Ocorreu um problmea, tente novamente mais tarde</h1>
-        <Link href={"/"}>Voltar para home</Link>
+      <h1>Ocorreu um problmea, tente novamente mais tarde</h1>
+      <Link href={"/"}>Voltar para home</Link>
     </div>
-  )
-}
+  );
+};
 
-export default TodoEditErrorPage
+export default TodoEditErrorPage;
