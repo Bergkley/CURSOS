9ea007b52158fd641d23a80eb96d89bcb8f40e89
Bergkley <bergkley@gmail.com>
2025-02-23 10:10:21 -0300

    feat: config arquivo do auth

---
+++ Udemy - Next.js do Zero ao Avançado com Projetos/Seçao 6 - Auth/src/app/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      <h1>Hello Word Next.js</h1>
+    </main>
+  );
+}
