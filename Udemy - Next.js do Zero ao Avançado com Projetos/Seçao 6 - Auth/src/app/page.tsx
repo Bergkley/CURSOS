ed880c38fb8d136e826ab447da16cf03b8dccd6e
Bergkley <bergkley@gmail.com>
2025-02-24 14:45:51 -0300

    feat: protegendo componentes do server

--- Udemy - Next.js do Zero ao Avançado com Projetos/Seçao 6 - Auth/src/app/page.tsx
+++ Udemy - Next.js do Zero ao Avançado com Projetos/Seçao 6 - Auth/src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
       <Link href={'/client'}>Página com cliente component</Link>
+      <Link href={'/server'}>Página com server component</Link>
     </main>
   );
 }
