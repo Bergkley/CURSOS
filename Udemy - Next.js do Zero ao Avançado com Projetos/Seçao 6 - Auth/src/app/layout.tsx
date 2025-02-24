618e973eb168703cdffa659c53b2e0ac0f7df879
Bergkley <bergkley@gmail.com>
2025-02-24 14:40:14 -0300

    feat: protegendo componentes do cliente

--- Udemy - Next.js do Zero ao Avançado com Projetos/Seçao 6 - Auth/src/app/layout.tsx
+++ Udemy - Next.js do Zero ao Avançado com Projetos/Seçao 6 - Auth/src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
-
+import { SessionProvider } from "next-auth/react";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -18,8 +18,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <SessionProvider>
           <Navbar />
           {children}
+        </SessionProvider>
       </body>
     </html>
   );
--- Udemy - Next.js do Zero ao Avançado com Projetos/Seçao 6 - Auth/src/app/page.tsx
+++ Udemy - Next.js do Zero ao Avançado com Projetos/Seçao 6 - Auth/src/app/page.tsx
@@ -1,7 +1,9 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
     <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-      <h1>Hello Word Next.js</h1>
+      <Link href={'/client'}>Página com cliente component</Link>
     </main>
   );
 }
