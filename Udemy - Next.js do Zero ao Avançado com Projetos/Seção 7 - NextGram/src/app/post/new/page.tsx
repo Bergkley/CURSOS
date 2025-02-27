0ae74c7c8917dc2ea486f1f47e984c4b0e531c9d
Bergkley <bergkley@gmail.com>
2025-02-27 15:13:30 -0300

    feat:Seção 7 - Iniciando a página de nova publicação

--- Udemy - Next.js do Zero ao Avançado com Projetos/Seção 7 - NextGram/src/app/post/new/page.tsx
+++ Udemy - Next.js do Zero ao Avançado com Projetos/Seção 7 - NextGram/src/app/post/new/page.tsx
@@ -1,7 +1,23 @@
-const NewPostPage = () => {
-  return (
-    <div>NewPostPage</div>
-  )
-}
+import React from "react";
+import CreatePostForm from "@/components/CreatePostForm";
+import { auth } from "auth";
+import { redirect } from "next/navigation";
 
-export default NewPostPage
+const CreatePostPage: React.FC = async () => {
+  const session = await auth();
+
+  if (!session || !session.user?.email) return redirect(`/`);
+
+  return (
+    <div className="w-[35rem] mx-auto p-4 my-10">
+      <h1 className="text-[2rem] leading-10 text-center font-semibold">
+        Criar novo post
+      </h1>
+      <div className="border border-zinc-300 p-4 rounded mt-8">
+        <CreatePostForm />
+      </div>
+    </div>
+  );
+};
+
+export default CreatePostPage;
---
+++ Udemy - Next.js do Zero ao Avançado com Projetos/Seção 7 - NextGram/src/components/CreatePostForm.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CreatePostForm = () => {
+  return (
+    <div>CreatePostForm</div>
+  )
+}
+
+export default CreatePostForm
