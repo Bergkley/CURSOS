ced1029052f0f73ad612e438e86470ba2f4d153d
Bergkley <bergkley@gmail.com>
2025-03-05 14:38:28 -0300

    feat:criando module manualmente

--- Udemy - NestJS do Zero com Banco de dados, Prisma, Testes, Swagger/2- Seção Entendendo conceitos/test/app.e2e-spec.ts
+++ Udemy - NestJS do Zero com Banco de dados, Prisma, Testes, Swagger/2- Seção Entendendo conceitos/test/app.e2e-spec.ts
@@ -2,7 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { App } from 'supertest/types';
-import { AppModule } from './../src/app.module';
+import { AppModule } from './../src/app/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication<App>;
