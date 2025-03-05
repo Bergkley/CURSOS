05ea35228e962566c85aafcb0e792ca0261a9d58
Bergkley <bergkley@gmail.com>
2025-03-05 14:53:03 -0300

    feat:lidando com controller

--- Udemy - NestJS do Zero com Banco de dados, Prisma, Testes, Swagger/2- Seção Entendendo conceitos/src/app/app.controller.ts
+++ Udemy - NestJS do Zero com Banco de dados, Prisma, Testes, Swagger/2- Seção Entendendo conceitos/src/app/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -10,8 +10,13 @@ export class AppController {
     return this.appService.getHello();
   }
 
-  @Get()
+  @Get('/teste')
   getTeste(): string {
     return this.appService.getTeste();
+  }
+
+  @Post('/teste')
+  createTest(): string {
+    return 'ROTA POST FUNCIONANDO';
   }
 }
