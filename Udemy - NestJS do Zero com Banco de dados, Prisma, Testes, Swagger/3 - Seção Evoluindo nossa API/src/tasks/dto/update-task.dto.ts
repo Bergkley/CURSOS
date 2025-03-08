f1b78d2c23396fb99836fed6b0debb93c3e1d0bf
Bergkley <bergkley@gmail.com>
2025-03-07 21:55:26 -0300

    feat:conhecendo pipes

--- Udemy - NestJS do Zero com Banco de dados, Prisma, Testes, Swagger/3 - Seção Evoluindo nossa API/src/tasks/dto/update-task.dto.ts
+++ Udemy - NestJS do Zero com Banco de dados, Prisma, Testes, Swagger/3 - Seção Evoluindo nossa API/src/tasks/dto/update-task.dto.ts
@@ -1,5 +1,13 @@
+import { IsBoolean, IsOptional, IsString } from "class-validator";
+
 export class UpdateTaskDto {
+    @IsString()
+    @IsOptional()
     readonly name?: string;
+    @IsString()
+    @IsOptional()
     readonly description?: string;
+    @IsBoolean()
+    @IsOptional()
     readonly completed?: boolean
 }
