d6dff0c33f0d7727650d5cf3926b7993f6ec1c6c
Bergkley <bergkley@gmail.com>
2025-03-10 19:20:15 -0300

    feat:add relação com user

--- Udemy - NestJS do Zero com Banco de dados, Prisma, Testes, Swagger/5 - Seção melhorando controller e services/src/tasks/tasks.service.ts
+++ Udemy - NestJS do Zero com Banco de dados, Prisma, Testes, Swagger/5 - Seção melhorando controller e services/src/tasks/tasks.service.ts
@@ -69,7 +69,15 @@ export class TasksService {
         where: {
           id: findTask.id,
         },
-        data: updateTaskDto,
+        data: {
+          name: updateTaskDto.name ? updateTaskDto.name : findTask.name,
+          description: updateTaskDto.description
+            ? updateTaskDto.description
+            : findTask.description,
+          completed: updateTaskDto.completed
+            ? updateTaskDto.completed
+            : findTask.completed,
+        },
       });
 
       return task;
