f5619cd08528efd3e8775e48abf3650e3db9fb65
Bergkley <bergkley@gmail.com>
2025-03-10 19:10:42 -0300

    feat:atualizando e deletando user

--- Udemy - NestJS do Zero com Banco de dados, Prisma, Testes, Swagger/5 - Seção melhorando controller e services/src/users/users.controller.ts
+++ Udemy - NestJS do Zero com Banco de dados, Prisma, Testes, Swagger/5 - Seção melhorando controller e services/src/users/users.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Get, Param, ParseIntPipe, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
@@ -13,5 +14,15 @@ export class UsersController {
   @Post()
   create(@Body() createUserDto:CreateUserDto ) {
     return this.userService.create(createUserDto)
+  }
+
+  @Patch(':id')
+  update(@Param('id',ParseIntPipe) id: number, @Body() updateUserDto:UpdateUserDto) {
+    return this.userService.update(id,updateUserDto)
+  }
+
+  @Delete(':id')
+  delete(@Param('id',ParseIntPipe) id: number) {
+    return this.userService.delete(id)
   }
 }
