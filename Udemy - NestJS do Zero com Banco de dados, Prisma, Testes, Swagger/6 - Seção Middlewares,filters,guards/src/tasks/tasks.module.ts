b87cded97cffc95cc98c76cc496d055c4e5fee69
Bergkley <bergkley@gmail.com>
2025-03-11 15:23:59 -0300

    feat:revisão de injelçao de dependencia

--- Udemy - NestJS do Zero com Banco de dados, Prisma, Testes, Swagger/6 - Seção Middlewares,filters,guards/src/tasks/tasks.module.ts
+++ Udemy - NestJS do Zero com Banco de dados, Prisma, Testes, Swagger/6 - Seção Middlewares,filters,guards/src/tasks/tasks.module.ts
@@ -4,10 +4,11 @@ import { TasksService } from './tasks.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { APP_FILTER } from '@nestjs/core';
 import { ApiExceptionFilter } from 'src/common/filters/exception.filter';
+import { TaskUtils } from './tasks.utils';
 
 @Module({
   controllers: [TasksController],
-  providers: [TasksService, {provide: APP_FILTER, useClass: ApiExceptionFilter}],
+  providers: [TasksService,TaskUtils, {provide: APP_FILTER, useClass: ApiExceptionFilter}],
   imports:[PrismaModule]
 })
 export class TasksModule {}
