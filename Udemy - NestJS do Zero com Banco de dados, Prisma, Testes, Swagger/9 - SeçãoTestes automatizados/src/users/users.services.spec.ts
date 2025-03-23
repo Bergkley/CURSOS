072626219a2102752d6d2bd587b7bee493e608fb
Bergkley <bergkley@gmail.com>
2025-03-23 12:44:44 -0300

    feat: primeiro teste automatizado

--- Udemy - NestJS do Zero com Banco de dados, Prisma, Testes, Swagger/9 - SeçãoTestes automatizados/src/users/users.services.spec.ts
+++ Udemy - NestJS do Zero com Banco de dados, Prisma, Testes, Swagger/9 - SeçãoTestes automatizados/src/users/users.services.spec.ts
@@ -1,5 +1,14 @@
+// Testes unitários
+
+
 describe('UsersService', () => {
-    it('dveria testar o modulo userService', () => {
-        expect(true).toBe(true);
+    it('should be define users service', () => {
+        const numero1 = 150;
+        const numero2 = 100;
+
+        const conta = numero1 - numero2
+
+        expect(conta).toBe(50);
+        expect(conta).toBeGreaterThan(10);
     })
 })
