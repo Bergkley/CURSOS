d47e1e7ecbbb34174eaa5b6b6fd5b6bd2a722c00
Bergkley <bergkley@gmail.com>
2025-03-23 13:16:47 -0300

    feat: testando  o metodo create

--- Udemy - NestJS do Zero com Banco de dados, Prisma, Testes, Swagger/9 - SeçãoTestes automatizados/src/users/user.service.spec.ts
+++ Udemy - NestJS do Zero com Banco de dados, Prisma, Testes, Swagger/9 - SeçãoTestes automatizados/src/users/user.service.spec.ts
@@ -4,6 +4,7 @@ import { PrismaService } from "../prisma/prisma.service";
 import { UsersService } from "./users.service";
 import { HashingServiceProtocol } from 'src/auth/hash/hashing.service';
 import { Test, TestingModule } from "@nestjs/testing";
+import { CreateUserDto } from "./dto/create-user.dto";
 
 
 describe('UsersService', () => {
@@ -15,11 +16,17 @@ describe('UsersService', () => {
         const module: TestingModule = await Test.createTestingModule({
             providers: [UsersService , {
                 provide: PrismaService,
-                useValue: {},
+                useValue: {
+                    user:{
+                        create: jest.fn(),
+                    }
+                },
             },
             {
                 provide: HashingServiceProtocol,
-                useValue: {},
+                useValue: {
+                    hash: jest.fn(),
+                },
             }],
             
         }).compile();
@@ -30,5 +37,31 @@ describe('UsersService', () => {
     })
     it('should be define users service', () => {
        expect(userService).toBeDefined();
+    })
+
+    it('should create a new user',async () => {
+        const createUserDto: CreateUserDto = {
+            name: 'Teste',
+            email: 'q0Jd5@example.com',
+            password: '123456',
+        }
+        jest.spyOn(hashingService, 'hash').mockResolvedValue("HASH_MOCK_EXEMPLO")
+        
+        await userService.create(createUserDto);
+
+        expect(hashingService.hash).toHaveBeenCalled();
+        expect(prismaService.user.create).toHaveBeenCalledWith({
+            data: {
+                name: createUserDto.name,
+                email: createUserDto.email,
+                passwordHash: "HASH_MOCK_EXEMPLO",
+            },
+            select: {
+                id: true,
+                name: true,
+                email: true,
+            },
+        });
+
     })
 })
