44833b4b4a5d923a5db94da2b034711f29e3e740
Bergkley <bergkley@gmail.com>
2025-03-25 10:30:27 -0300

    feat: create And Update user controller

--- Udemy - NestJS do Zero com Banco de dados, Prisma, Testes, Swagger/9 - SeçãoTestes automatizados/src/users/users.controller.spec.ts
+++ Udemy - NestJS do Zero com Banco de dados, Prisma, Testes, Swagger/9 - SeçãoTestes automatizados/src/users/users.controller.spec.ts
@@ -1,3 +1,6 @@
+import { PayloadTokenDto } from "src/auth/dto/payload-token.dto";
+import { CreateUserDto } from "./dto/create-user.dto";
+import { UpdateUserDto } from "./dto/update-user.dto";
 import { UsersController } from "./users.controller";
 
 describe('Users Controllers', () => {
@@ -5,8 +8,8 @@ describe('Users Controllers', () => {
 
     const usersServiceMock = {
         findOne: jest.fn(),
-        createUser: jest.fn(),
-        updateUser: jest.fn(),
+        create: jest.fn(),
+        update: jest.fn(),
         delete: jest.fn(),
         uploadAvatarImage: jest.fn(),
     }
@@ -20,4 +23,60 @@ describe('Users Controllers', () => {
         await controller.findOneUser(userId);
         expect(usersServiceMock.findOne).toHaveBeenCalledWith(userId);
     })
+
+    it('should create a new user', async () => {
+        const createUserDto: CreateUserDto = {
+            name: 'Berg',
+            email: 'berg@gmail.com',
+            password: '123456',
+        }
+        const mockUser = {
+            id: 1,
+            name: 'Berg',
+            email: 'berg@gmail.com',
+        }
+
+        jest.spyOn(usersServiceMock, 'create').mockResolvedValue(mockUser)
+
+        const result = await controller.create(createUserDto)
+
+
+
+        await controller.create(createUserDto);
+        expect(usersServiceMock.create).toHaveBeenCalledWith(createUserDto);
+
+        expect(result).toEqual(mockUser);
+    })
+
+    it('should update user', async () => {
+        const userId = 1;
+        const updateUserDto: UpdateUserDto = {
+            name: 'Berg Novo',
+        }
+
+        const tokenPayload: PayloadTokenDto = {
+            sub: userId,
+            aud: '',
+            email: '',
+            exp: 1,
+            iat: 1,
+            iss: ''
+          }
+
+        const updatedUser = {
+            id: userId,
+            name: 'Berg',
+            email: 'berg@gmail.com',
+        }
+
+        await controller.update(updatedUser.id, updateUserDto, tokenPayload);
+
+        jest.spyOn(usersServiceMock, 'update').mockResolvedValue(updatedUser)
+
+        const result = await controller.update(userId, updateUserDto, tokenPayload)
+
+        expect(usersServiceMock.update).toHaveBeenCalledWith(userId, updateUserDto, tokenPayload);
+
+        expect(result).toEqual(updatedUser);
+    } )
 })
