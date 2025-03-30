cdb6759a998e6c5f548f613295edfa50f3d6542d
Bergkley <bergkley@gmail.com>
2025-03-30 10:10:27 -0300

    feat: ajustando DTO na docs

--- Udemy - NestJS do Zero com Banco de dados, Prisma, Testes, Swagger/10 - Swagger/src/tasks/tasks.controller.ts
+++ Udemy - NestJS do Zero com Banco de dados, Prisma, Testes, Swagger/10 - Swagger/src/tasks/tasks.controller.ts
@@ -23,23 +23,45 @@ import { AuthTokenGuard } from 'src/auth/guard/auth-token.guard';
 import { TokenPayloadParam } from 'src/auth/param/token-payload.param';
 import { PayloadTokenDto } from 'src/auth/dto/payload-token.dto';
 import { ResponseTaskDto } from './dto/response-task.dto';
-import { ApiBearerAuth, ApiOperation, ApiParam, ApiQuery } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiParam,
+  ApiQuery,
+} from '@nestjs/swagger';
 
 @Controller('tasks')
 export class TasksController {
-  constructor(private readonly tasksService: TasksService,
-  ) {}
+  constructor(private readonly tasksService: TasksService) {}
   @Get()
   @ApiOperation({ summary: 'Buscar todas as tarefas' })
-  @ApiQuery({name: 'limit', description: 'Quantidade de tarefas por pagina', type: Number, required: false,example: 10})
-  @ApiQuery({name: 'offset', description: 'Quantidade de tarefas a serem puladas', type: Number, required: false,example: 0})
+  @ApiQuery({
+    name: 'limit',
+    description: 'Quantidade de tarefas por pagina',
+    type: Number,
+    required: false,
+    example: 10,
+  })
+  @ApiQuery({
+    name: 'offset',
+    description: 'Quantidade de tarefas a serem puladas',
+    type: Number,
+    required: false,
+    example: 0,
+  })
   // @UseGuards(AuthAdminGuard)
   findAllTasks(@Query() paginationDto: PaginationDto) {
     return this.tasksService.findAll(paginationDto);
   }
   @Get(':id')
   @ApiOperation({ summary: 'Buscar por uma unica tarefa' })
-  @ApiParam({name: 'id', description: 'Id da tarefa', type: Number, required: true,example: 1})
+  @ApiParam({
+    name: 'id',
+    description: 'Id da tarefa',
+    type: Number,
+    required: true,
+    example: 1,
+  })
   findOneTask(@Param('id', ParseIntPipe) id: number) {
     return this.tasksService.findOne(id);
   }
@@ -47,27 +69,37 @@ export class TasksController {
   @ApiBearerAuth()
   @ApiOperation({ summary: 'Criar uma nova tarefa' })
   @Post()
-  createTask(@Body() createTaskDto: CreateTaskDto, @TokenPayloadParam() tokenPayload:PayloadTokenDto ) {
-    return this.tasksService.create(createTaskDto,tokenPayload);
+  createTask(
+    @Body() createTaskDto: CreateTaskDto,
+    @TokenPayloadParam() tokenPayload: PayloadTokenDto,
+  ) {
+    return this.tasksService.create(createTaskDto, tokenPayload);
   }
   @UseGuards(AuthTokenGuard)
   @ApiBearerAuth()
   @ApiOperation({ summary: 'Atualizar uma tarefa' })
-  @ApiParam({name: 'id', description: 'Id da tarefa', type: Number, required: true})
+  @ApiParam({
+    name: 'id',
+    description: 'Id da tarefa',
+    type: Number,
+    required: true,
+  })
   @Patch(':id')
   updateTask(
     @Param('id', ParseIntPipe) id: number,
     @Body() updateTaskDto: UpdateTaskDto,
-    @TokenPayloadParam() tokenPayload:PayloadTokenDto
+    @TokenPayloadParam() tokenPayload: PayloadTokenDto,
   ) {
-    return this.tasksService.update(id, updateTaskDto,tokenPayload);
+    return this.tasksService.update(id, updateTaskDto, tokenPayload);
   }
   @UseGuards(AuthTokenGuard)
   @ApiBearerAuth()
   @ApiOperation({ summary: 'Deletar uma tarefa' })
   @Delete(':id')
-  deleteTask(@Param('id', ParseIntPipe) id: number,@TokenPayloadParam() tokenPayload:PayloadTokenDto
-) {
-    return this.tasksService.delete(id,tokenPayload);
+  deleteTask(
+    @Param('id', ParseIntPipe) id: number,
+    @TokenPayloadParam() tokenPayload: PayloadTokenDto,
+  ) {
+    return this.tasksService.delete(id, tokenPayload);
   }
 }
